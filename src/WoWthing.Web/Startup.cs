1a7fa61bfca6bb8b573cd6b41ebc7498d3aa875a
Freddie <freddie@wafflemonster.org>
2020-10-29 19:44:36 -0700

    Basic solution

--- src/WoWthing.Web/Startup.cs
+++ src/WoWthing.Web/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace WoWthing.Web
+namespace Wowthing.Web
 {
     public class Startup
     {
