a8ca6077de3da877a95c2035c98796977e5fa50d
Freddie <freddie@wafflemonster.org>
2020-11-11 00:00:41 -0800

    Add JS garbage for Rollup/Svelte/Typescript

---
+++ src/Wowthing.Web/rollup.config.js
@@ -0,0 +1,88 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import livereload from 'rollup-plugin-livereload';
+import svelte from 'rollup-plugin-svelte';
+import { terser } from 'rollup-plugin-terser';
+import rimraf from 'rimraf';
+import sveltePreprocess from 'svelte-preprocess';
+
+
+const production = !process.env.ROLLUP_WATCH
+console.log('production', production)
+
+rimraf.sync(__dirname + '/wwwroot/assets')
+
+function serve() {
+    let server;
+    
+    function toExit() {
+        if (server) server.kill(0);
+    }
+
+    return {
+        writeBundle() {
+            if (server) return;
+            server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+                stdio: ['ignore', 'inherit', 'inherit'],
+                shell: true
+            });
+
+            process.on('SIGTERM', toExit);
+            process.on('exit', toExit);
+        }
+    };
+}
+
+export default {
+    input: 'frontend/ts/main.ts',
+    output: {
+        sourcemap: !production,
+        format: 'iife',
+        name: 'app',
+        dir: 'wwwroot/assets',
+        entryFileNames: '[name].[hash].js',
+    },
+    plugins: [
+        svelte({
+            // enable run-time checks when not in production
+            dev: !production,
+            // we'll extract any component CSS out into
+            // a separate file - better for performance
+            css: css => {
+                css.write('bundle.[hash].css', !production);
+            },
+            preprocess: sveltePreprocess(),
+        }),
+
+        // If you have external dependencies installed from
+        // npm, you'll most likely need these plugins. In
+        // some cases you'll need additional configuration -
+        // consult the documentation for details:
+        // https://github.com/rollup/plugins/tree/master/packages/commonjs
+        resolve({
+            browser: true,
+            dedupe: ['svelte']
+        }),
+        commonjs(),
+        typescript({
+            sourceMap: !production,
+            inlineSources: !production
+        }),
+
+        // In dev mode, call `npm run start` once
+        // the bundle has been generated
+        !production && serve(),
+
+        // Watch the `assets` directory and refresh the
+        // browser on changes when not in production
+        !production && livereload('wwwroot/assets'),
+
+        // If we're building for production (npm run build
+        // instead of npm run dev), minify
+        production && terser()
+    ],
+    watch: {
+        clearScreen: false
+    }
+};
