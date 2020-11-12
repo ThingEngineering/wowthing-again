0de0343840366b95ab2d62c1b9238579e042467b
Freddie <freddie@wafflemonster.org>
2020-11-11 19:14:08 -0800

    More fighting with Rollup/Svelte

--- src/Wowthing.Web/frontend/ts/main.ts
+++ src/Wowthing.Web/frontend/ts/main.ts
@@ -1,1 +1,7 @@
-// hi
+import App from './App.svelte'
+
+const app = new App({
+  target: document.body,
+})
+
+export default app
--- src/Wowthing.Web/rollup.config.js
+++ src/Wowthing.Web/rollup.config.js
@@ -1,37 +1,39 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import livereload from 'rollup-plugin-livereload';
-import svelte from 'rollup-plugin-svelte';
-import { terser } from 'rollup-plugin-terser';
-import rimraf from 'rimraf';
-import sveltePreprocess from 'svelte-preprocess';
+import crypto from 'crypto'
+
+import rimraf from 'rimraf'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import livereload from 'rollup-plugin-livereload'
+import svelte from 'rollup-plugin-svelte'
+import { terser } from 'rollup-plugin-terser'
+import sveltePreprocess from 'svelte-preprocess'
 
 
 const production = !process.env.ROLLUP_WATCH
 console.log('production', production)
 
-rimraf.sync(__dirname + '/wwwroot/assets')
+rimraf.sync('wwwroot/dist')
 
 function serve() {
-    let server;
+    let server
     
     function toExit() {
-        if (server) server.kill(0);
+        if (server) server.kill(0)
     }
 
     return {
         writeBundle() {
-            if (server) return;
+            if (server) return
             server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
                 stdio: ['ignore', 'inherit', 'inherit'],
                 shell: true
-            });
+            })
 
-            process.on('SIGTERM', toExit);
-            process.on('exit', toExit);
+            process.on('SIGTERM', toExit)
+            process.on('exit', toExit)
         }
-    };
+    }
 }
 
 export default {
@@ -40,26 +42,20 @@ export default {
         sourcemap: !production,
         format: 'iife',
         name: 'app',
-        dir: 'wwwroot/assets',
+        dir: 'wwwroot/dist',
         entryFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash][extname]',
     },
     plugins: [
         svelte({
             // enable run-time checks when not in production
             dev: !production,
-            // we'll extract any component CSS out into
-            // a separate file - better for performance
-            css: css => {
-                css.write('bundle.[hash].css', !production);
+            css: (css) => {
+                const hash = crypto.createHash('md5').update(css.code).digest('hex').substr(0, 8)
+                css.write(`main.${hash}.css`, !production)
             },
             preprocess: sveltePreprocess(),
         }),
-
-        // If you have external dependencies installed from
-        // npm, you'll most likely need these plugins. In
-        // some cases you'll need additional configuration -
-        // consult the documentation for details:
-        // https://github.com/rollup/plugins/tree/master/packages/commonjs
         resolve({
             browser: true,
             dedupe: ['svelte']
@@ -74,15 +70,15 @@ export default {
         // the bundle has been generated
         !production && serve(),
 
-        // Watch the `assets` directory and refresh the
+        // Watch the `dist` directory and refresh the
         // browser on changes when not in production
-        !production && livereload('wwwroot/assets'),
+        !production && livereload('wwwroot/dist'),
 
         // If we're building for production (npm run build
         // instead of npm run dev), minify
-        production && terser()
+        production && terser(),
     ],
     watch: {
         clearScreen: false
     }
-};
+}
