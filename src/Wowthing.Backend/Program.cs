e822b1750d2cf5dbcc45f619d2d8e8b4b2d7a2e6
Freddie <freddie@wafflemonster.org>
2020-11-12 21:30:33 -0800

    Recreate Backend with "worker" template

--- src/Wowthing.Backend/Program.cs
+++ src/Wowthing.Backend/Program.cs
@@ -2,10 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Wowthing.Backend
 {
@@ -18,9 +16,9 @@ namespace Wowthing.Backend
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+                .ConfigureServices((hostContext, services) =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    services.AddHostedService<Worker>();
                 });
     }
 }
