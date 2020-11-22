ee04c7172dbdfd6102be58cd8a9ff79217b4d3ce
Freddie <freddie@wafflemonster.org>
2020-11-22 15:17:23 -0800

    Frontend: use components for race/class icon instead of util function

---
+++ src/frontend/ts/components/images/BaseImage.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    export let size
+    export let name: string
+</script>
+
+<img src="https://img.wowthing.org/{size}/{name}.png" width="{size}" height="{size}">
---
+++ src/frontend/ts/components/images/ClassIcon.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import {data} from '../../stores/static-store'
+  import Character from '../../models/character'
+
+  import BaseImage from './BaseImage.svelte'
+
+  export let character: Character
+  export let size: number
+
+  $: name = $data.Classes[character.classId].Icon
+</script>
+
+<BaseImage name={name} size={size} />
