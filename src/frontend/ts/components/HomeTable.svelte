e3e8936c6d76d66afd370c6dad380ff870f52520
Freddie <freddie@wafflemonster.org>
2020-11-22 22:36:47 -0800

    Frontend: fix icons in HomeTable

--- src/frontend/ts/components/HomeTable.svelte
+++ src/frontend/ts/components/HomeTable.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
     import { data } from '../stores/user-store'
+
+    import ClassIcon from './images/ClassIcon.svelte'
+    import RaceIcon from './images/RaceIcon.svelte'
 </script>
 
 <style lang="scss">
@@ -17,8 +20,8 @@
     <table>
         {#each $data.characters as character}
             <tr class="{character.faction === 0 ? 'faction0' : 'faction1'}">
-                <td>{@html character.getRaceIcon(20)}</td>
-                <td>{@html character.getClassIcon(20)}</td>
+                <td><RaceIcon character={character} size=20 /></td>
+                <td><ClassIcon character={character} size=20 /></td>
                 <td>{character.name}</td>
                 <td>&mdash; {character.getRealmName()}</td>
                 <td>{character.level}</td>
