863435efde406393db4562beb90acdd267d52c63
Freddie <freddie@wafflemonster.org>
2020-11-24 10:06:45 -0800

    Even more collection fun times
    - Add more mounts

--- src/frontend/ts/components/collections/Collection.svelte
+++ src/frontend/ts/components/collections/Collection.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+    import {afterUpdate} from 'svelte'
+
     import CollectionSection from './CollectionSection.svelte'
 
     export let thingType: string
     export let thingMap
     export let userHas
     export let sets
+
+    afterUpdate(() => {
+        if (window.__tip) {
+            window.__tip.watchElligibleElements();
+        }
+    })
 </script>
 
 <div>
