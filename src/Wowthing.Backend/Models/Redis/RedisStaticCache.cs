977427aa97f34b70691267926b1b8b7d7d61576f
Freddie <freddie@wafflemonster.org>
2020-11-27 11:26:27 -0800

    Backend: add WowReputation and WowReputationTier to static data

--- src/Wowthing.Backend/Models/Redis/RedisStaticCache.cs
+++ src/Wowthing.Backend/Models/Redis/RedisStaticCache.cs
@@ -15,6 +15,10 @@ namespace Wowthing.Backend.Models.Redis
         public SortedDictionary<int, WowRace> Races { get; set; }
         [JsonProperty(Order = 2)]
         public SortedDictionary<int, WowRealm> Realms { get; set; }
+        [JsonProperty(Order = 3)]
+        public SortedDictionary<int, WowReputation> Reputations { get; set; }
+        [JsonProperty(Order = 4)]
+        public SortedDictionary<int, WowReputationTier> ReputationTiers { get; set; }
 
         [JsonProperty(Order = 10)]
         public SortedDictionary<int, int> MountToSpell { get; set; }
@@ -24,6 +28,6 @@ namespace Wowthing.Backend.Models.Redis
         [JsonProperty(Order = 20)]
         public List<RedisSetCategory> MountSets { get; set; }
         [JsonProperty(Order = 23)]
-        public List<DataReputationCategory> Reputations { get; set; }
+        public List<DataReputationCategory> ReputationSets { get; set; }
     }
 }
