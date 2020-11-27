c7506984816ba60d5459bfaca54519046516ba06
Freddie <freddie@wafflemonster.org>
2020-11-26 23:26:00 -0800

    Add WowReputationTier table and jobs to populate it

---
+++ src/Wowthing.Backend/Jobs/Data/DataReputationTiersIndexJob.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wowthing.Backend.Models.API;
+using Wowthing.Backend.Models.API.Data;
+using Wowthing.Lib.Enums;
+using Wowthing.Lib.Jobs;
+
+namespace Wowthing.Backend.Jobs.Data
+{
+    public class DataReputationTiersIndexJob : JobBase, IScheduledJob
+    {
+        public static ScheduledJob Schedule = new ScheduledJob
+        {
+            Type = JobType.DataReputationTiersIndex,
+            Priority = JobPriority.High,
+            Interval = TimeSpan.FromDays(1),
+        };
+
+        private const string API_PATH = "data/wow/reputation-tiers/index";
+
+        public override async Task Run(params string[] data)
+        {
+            // Fetch API data
+            var uri = GenerateUri(WowRegion.US, ApiNamespace.Static, API_PATH);
+            var result = await GetJson<ApiDataReputationTiersIndex>(uri);
+            if (result.NotModified)
+            {
+                return;
+            }
+
+            // Absolute garbage API design on Blizzard's part, cool
+            var datas = result.Data.Tiers.Select(x => x.Id.ToString());
+            await _jobRepository.AddJobsAsync(JobPriority.High, JobType.DataReputationTiers, datas);
+        }
+    }
+}
---
+++ src/Wowthing.Lib/Models/Wow/WowReputationTier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Wowthing.Lib.Models
+{
+    public class WowReputationTier
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        
+        public int[] MinValues { get; set; }
+        public int[] MaxValues { get; set; }
+        public string[] Names { get; set; }
+    }
+}
