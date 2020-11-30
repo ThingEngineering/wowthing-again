768fb36d829c4073d9c417af66134568f19ddfd8
Freddie <freddie@wafflemonster.org>
2020-11-29 19:15:13 -0800

    Backend: add DataReputationCategory.Note

--- src/Wowthing.Backend/Models/Data/DataReputationCategory.cs
+++ src/Wowthing.Backend/Models/Data/DataReputationCategory.cs
@@ -33,5 +33,6 @@ namespace Wowthing.Backend.Models.Data
         public int Id { get; set; }
         public string Name { get; set; }
         public string Icon { get; set; }
+        public string Note { get; set; }
     }
 }
