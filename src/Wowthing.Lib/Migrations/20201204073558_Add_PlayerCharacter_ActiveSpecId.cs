ca1d06652c89e857ee4a62b90fca9e21a7a418f2
Freddie <freddie@wafflemonster.org>
2020-12-03 23:38:45 -0800

    Add PlayerCharacter.ActiveSpecId
    - Update CharacterJob to set ActiveSpecId

---
+++ src/Wowthing.Lib/Migrations/20201204073558_Add_PlayerCharacter_ActiveSpecId.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Wowthing.Lib.Migrations
+{
+    public partial class Add_PlayerCharacter_ActiveSpecId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "active_spec_id",
+                table: "player_character",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "active_spec_id",
+                table: "player_character");
+        }
+    }
+}
