07144dd386628cdae240886ba3fa883ebbb8cc56
Freddie <freddie@wafflemonster.org>
2020-12-10 15:51:06 -0800

    Frontend: automatically route to initial or saved section in collections

--- src/frontend/ts/components/collections/Collection.svelte
+++ src/frontend/ts/components/collections/Collection.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import {afterUpdate, setContext} from 'svelte'
+    import {replace} from 'svelte-spa-router'
 
     import CollectionSection from './CollectionSection.svelte'
     import CollectionSidebar from './CollectionSidebar.svelte'
@@ -23,6 +24,21 @@
     afterUpdate(() => {
         if (window.__tip) {
             window.__tip.watchElligibleElements()
+        }
+
+        const key = `route-${route}`
+        if (slug === null) {
+            const saved = localStorage.getItem(key)
+            if (saved !== null) {
+                replace(`/${route}/${saved}`)
+            }
+            else {
+                const first = document.getElementById('sub-sidebar').querySelector('li a')
+                replace(first.getAttribute('href').replace('#', ''))
+            }
+        }
+        else {
+            localStorage.setItem(key, slug)
         }
     })
 </script>
