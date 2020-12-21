36c290d65ad99ba5c2a35284087b52351506180b
Freddie <freddie@wafflemonster.org>
2020-12-20 16:34:10 -0800

    Frontend: add TableCharacterName component for DRY

--- src/frontend/ts/components/home/HomeTable.svelte
+++ src/frontend/ts/components/home/HomeTable.svelte
@@ -1,35 +1,27 @@
 <script lang="ts">
-    import { data } from '../../stores/user-store'
-    import getRealmName from '../../utils/get-realm-name'
+    import {data} from '../../stores/user-store'
 
-    import ClassIcon from '../images/ClassIcon.svelte'
-    import RaceIcon from '../images/RaceIcon.svelte'
+    import TableCharacterName from '../common/TableCharacterName.svelte'
+    import TableItemLevel from '../common/TableItemLevel.svelte'
 </script>
 
 <style lang="scss">
     @import "../../../scss/variables.scss";
 
     div {
-        background: $thing-background;
-        border: 1px solid $border-color;
-        padding: 0.5rem 0.75rem;
-    }
-    table {
+        padding: 0 0.5rem;
     }
 </style>
 
-<div>
-    <table>
-        {#each $data.characters as character}
-            <tr class="{character.faction === 0 ? 'faction0' : 'faction1'}">
-                <td><RaceIcon character={character} size=20 /></td>
-                <td><ClassIcon character={character} size=20 /></td>
-                <td>{character.name}</td>
-                <td>&mdash; {getRealmName(character.realmId)}</td>
-                <td>{character.level}</td>
-                <td>⚔</td>
-                <td style="text-align: right">{character.equippedItemLevel}</td>
-            </tr>
-        {/each}
+<div class="thing-container">
+    <table class="table-striped">
+        <tbody>
+            {#each $data.characters as character}
+                <tr class="faction{character.faction}">
+                    <TableCharacterName {character} />
+                    <TableItemLevel {character} />
+                </tr>
+            {/each}
+        </tbody>
     </table>
 </div>
