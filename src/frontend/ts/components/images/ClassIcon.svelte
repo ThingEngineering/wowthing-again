0bbf2f7e87ad8286afbdef9608e13d12501ce1e7
Freddie <freddie@wafflemonster.org>
2021-02-13 16:14:37 -0800

    Add missing bits of specialization icons

--- src/frontend/ts/components/images/ClassIcon.svelte
+++ src/frontend/ts/components/images/ClassIcon.svelte
@@ -3,9 +3,12 @@
   import {Character} from '../../types'
 
   import WowthingImage from './sources/WowthingImage.svelte'
+  import {specializationMap} from '../../data/specialization'
 
   export let character: Character
   export let size: number
+
+  $: tooltip = character.activeSpecId > 0 ? `${specializationMap[character.activeSpecId].Name} ${$data.Classes[character.classId].Name}` : $data.Classes[character.classId].Name;
 </script>
 
-<WowthingImage name={$data.Classes[character.classId].Icon} {size} tooltip={$data.Classes[character.classId].Name} border=1 />
+<WowthingImage name={$data.Classes[character.classId].Icon} {size} tooltip={tooltip} border=1 />
