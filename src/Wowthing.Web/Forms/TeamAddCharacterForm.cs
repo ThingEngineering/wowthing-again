116d80dbc97e1819d8406af3126985d28b8525d6
Freddie <freddie@wafflemonster.org>
2021-05-25 07:14:13 -0700

    web: add partially working team functionality

---
+++ src/Wowthing.Web/Forms/TeamAddCharacterForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Wowthing.Lib.Enums;
+
+namespace Wowthing.Web.Forms
+{
+    public class TeamAddCharacterForm
+    {
+        [HiddenInput]
+        public Guid TeamId { get; set; }
+
+        public int RealmId { get; set; }
+        public string CharacterName { get; set; }
+        public WowRole PrimaryRole { get; set; }
+        public WowRole SecondaryRole { get; set; }
+
+        public TeamAddCharacterForm(Guid guid)
+        {
+            TeamId = guid;
+        }
+    }
+}
--- src/Wowthing.Web/Models/Team/TeamApiCharacter.cs
+++ src/Wowthing.Web/Models/Team/TeamApiCharacter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Wowthing.Lib.Contexts;
+using Wowthing.Lib.Enums;
 using Wowthing.Lib.Models;
 
 namespace Wowthing.Web.Models
@@ -10,10 +11,16 @@ namespace Wowthing.Web.Models
     public class TeamApiCharacter
     {
         public UserApiCharacter Character { get; set; }
+        public string Note { get; set; }
+        public WowRole PrimaryRole { get; set; }
+        public WowRole SecondaryRole { get; set; }
 
         public TeamApiCharacter(WowDbContext context, TeamCharacter character)
         {
             Character = new UserApiCharacter(context, character.Character, pub: true);
+            Note = character.Note;
+            PrimaryRole = character.PrimaryRole;
+            SecondaryRole = character.SecondaryRole;
         }
     }
 }
