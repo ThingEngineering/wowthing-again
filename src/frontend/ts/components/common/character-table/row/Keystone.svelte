356aca84c9542ba49b6624ea78ddb910574a02a2
Freddie <freddie@wafflemonster.org>
2021-05-25 21:26:05 -0700

    frontend: add keystone to mythic+ this week/current season
    - only show keystone/vault for max level characters

--- src/frontend/ts/components/common/character-table/row/Keystone.svelte
+++ src/frontend/ts/components/common/character-table/row/Keystone.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
     import {getContext} from 'svelte'
 
-    import {dungeonMap} from '../../../data/dungeon'
-    import type {Character, Dungeon} from '../../../types'
-    import getMythicPlusRunQuality from '../../../utils/get-mythic-plus-run-quality'
+    import {dungeonMap} from '../../../../data/dungeon'
+    import type {Character, Dungeon} from '../../../../types'
+    import getMythicPlusRunQuality from '../../../../utils/get-mythic-plus-run-quality'
 
-    import TableIcon from '../../common/TableIcon.svelte'
-    import WowthingImage from '../../images/sources/WowthingImage.svelte'
-    import TableRowVault from './TableRowVault.svelte'
+    import TableIcon from '../../TableIcon.svelte'
+    import WowthingImage from '../../../images/sources/WowthingImage.svelte'
 
     const character: Character = getContext('character')
 
@@ -20,26 +19,28 @@
 </script>
 
 <style lang="scss">
+    @import '../../../../../scss/variables';
+
     .level {
-        text-align: right;
+        min-width: $table-width-key-level;
+        width: $table-width-key-level;
         padding-left: 0.1rem;
         padding-right: 0.3rem;
-        width: 1.6rem;
+        text-align: right;
     }
     .dungeon {
+        min-width: $table-width-key-dungeon;
+        width: $table-width-key-dungeon;
         padding-right: 0.7rem;
     }
 </style>
 
-{#if dungeon}
+{#if character.level >= 60 && dungeon}
     <TableIcon>
         <WowthingImage name="{dungeon.Icon}" size={20} border={1} />
     </TableIcon>
     <td class="level { getMythicPlusRunQuality(character.weekly.keystoneLevel) }">{ character.weekly.keystoneLevel }</td>
     <td class="dungeon">{ dungeon.Abbreviation }</td>
-    {#key character}
-        <TableRowVault />
-    {/key}
 {:else}
     <td colspan="3"></td>
 {/if}
