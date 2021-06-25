b15fc4ba1e83c744b8731d75b97b60354dffe9b3
Freddie <freddie@wafflemonster.org>
2021-06-25 07:37:40 -0700

    Add currencies display

---
+++ frontend/components/currencies/Sidebar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { link } from 'svelte-spa-router'
+    import active from 'svelte-spa-router/active'
+
+    import { data as staticData } from '@/stores/static'
+    import { categoryOrder } from '@/data/currencies'
+
+    import Sidebar from '@/components/common/Sidebar.svelte'
+
+    const categories = categoryOrder.map((id) => $staticData.currencyCategories[id])
+</script>
+
+<Sidebar width="12rem">
+    {#each categories as category}
+        <li use:active={'/currencies/' + category.slug}>
+            <a href="/currencies/{category.slug}" use:link
+            >{category.name}</a
+            >
+        </li>
+    {/each}
+</Sidebar>
---
+++ frontend/components/currencies/TableHeadCurrency.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import type {StaticDataCurrency} from '@/types'
+
+    import WowthingImage from '@/components/images/sources/WowthingImage.svelte'
+
+    export let currency: StaticDataCurrency
+</script>
+
+<style lang="scss">
+    th {
+        background: $thing-background;
+        border: 1px solid $border-color;
+        border-right-width: 0;
+        border-top-width: 0;
+        padding: 0.3rem 0;
+        text-align: center;
+        width: 4em;
+
+        & :global(img) {
+            border: 1px solid $border-color;
+            border-radius: $border-radius;
+        }
+    }
+</style>
+
+<th>
+    <a href="currencies/{currency.id}">
+        <WowthingImage name="currency/{currency.id}" size={48} border={1} tooltip={currency.name} />
+    </a>
+</th>
