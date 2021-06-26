852041cc9f890c89e610dc11a703e6f95d2a6380
Freddie <freddie@wafflemonster.org>
2021-06-26 12:44:06 -0700

    frontend: add tippyComponent util function to render a Svelte component as the tooltip content

--- frontend/utils/tippy.ts
+++ frontend/utils/tippy.ts
@@ -1,4 +1,5 @@
 import tippy, { SingleTarget } from 'tippy.js'
+import type {Instance, Props} from 'tippy.js'
 
 import type {TippyProps} from '@/types'
 
@@ -27,4 +28,43 @@ export default function (node: SingleTarget, props: TippyProps | string): any {
             tp.destroy()
         }
     }
+}
+
+// TODO: fix typing of this mess
+export function tippyComponent(node: SingleTarget, {component, props}: TippyComponentProps): any {
+    let cmp: any
+
+    const tp = tippy(node, {
+        allowHTML: true,
+        onShow(instance: Instance<Props>) {
+            cmp = new component({
+                target: instance.popper.querySelector('.tippy-content'),
+                props
+            })
+        },
+        onHide() {
+            if (cmp) {
+                cmp.$destroy()
+            }
+        }
+    })
+
+    return {
+        update(params: {props: any}) {
+            if (cmp) {
+                cmp.$set(params.props)
+            }
+        },
+        destroy() {
+            tp.destroy()
+            if (cmp) {
+                cmp.$destroy()
+            }
+        }
+    }
+}
+
+interface TippyComponentProps {
+    component: any
+    props: any
 }
