ec061efaaa9b2490a1fac4b0576338858ed0941a
Freddie <freddie@wafflemonster.org>
2021-06-29 17:58:20 -0700

    web: change a file to force deploy

--- src/Wowthing.Web/Models/UserApiCharacter.cs
+++ src/Wowthing.Web/Models/UserApiCharacter.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using Wowthing.Lib.Contexts;
 using Wowthing.Lib.Enums;
 using Wowthing.Lib.Models;
 
