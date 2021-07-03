7eed65731f556744e398eded1bfc333968be9303
Freddie <freddie@wafflemonster.org>
2021-07-03 12:22:05 -0700

    frontend: only show Settings in sidebar if not public

--- frontend/apps/home/Sidebar.svelte
+++ frontend/apps/home/Sidebar.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import active from 'svelte-spa-router/active'
 
+    import {data as userData, loading as userLoading} from '@/stores/user'
+
     import Sidebar from '@/components/common/Sidebar.svelte'
 </script>
 
@@ -14,7 +16,9 @@
     <li use:active={'/cards'}>
         <a href="#/cards">🚧 Home (Cards)</a>
     </li>
-    <li class="separator" />
+
+    <li class="separator"></li>
+
     <li use:active={'/currencies/*'}>
         <a href="#/currencies/shadowlands">Currencies</a>
     </li>
@@ -30,22 +34,29 @@
     <li use:active={'/reputations/*'}>
         <a href="#/reputations/shadowlands">Reputations</a>
     </li>
-    <li class="separator" />
+
+    <li class="separator"></li>
+
     <li use:active={'/mounts/*'}>
         <a href="#/mounts/">Mounts</a>
     </li>
     <li use:active={'/toys/*'}>
         <a href="#/toys/">Toys</a>
     </li>
-    <li class="separator" />
+
+    <li class="separator"></li>
+
     <li use:active={'/pets/*'}>
         <a href="#/pets/">🚧 Pets</a>
     </li>
     <li use:active={'/transmog/*'}>
         <a href="#/transmog/shadowlands">🚧 Transmog</a>
     </li>
-    <li class="separator" />
-    <li use:active={'/settings/*'}>
-        <a href="#/settings/">🚧 Settings</a>
-    </li>
+
+    {#if !$userLoading && !$userData.public}
+        <li class="separator"></li>
+        <li use:active={'/settings/*'}>
+            <a href="#/settings/">🚧 Settings</a>
+        </li>
+    {/if}
 </Sidebar>
