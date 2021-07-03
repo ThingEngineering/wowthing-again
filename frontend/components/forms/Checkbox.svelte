df4c45c1a3324f7cfae69a6a04ef33f890574f03
Freddie <freddie@wafflemonster.org>
2021-07-03 12:21:35 -0700

    frontend: initial version of new Settings

---
+++ frontend/components/forms/Checkbox.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let label: string
+    export let name: string
+    export let value: boolean
+</script>
+
+<fieldset>
+    <label for="input-{name}">
+        <input id="input-{name}" name={name} type="checkbox" bind:checked={value}>
+        {label}
+    </label>
+</fieldset>
---
+++ frontend/components/settings/SettingsGeneral.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import {data as settingsData} from '@/stores/settings'
+    import Checkbox from '@/components/forms/Checkbox.svelte'
+</script>
+
+<style lang="scss">
+    .thing-container {
+        border: 1px solid $border-color;
+    }
+</style>
+
+<div class="thing-container settings-container">
+    <h2>General</h2>
+
+    <ul>
+        <li>
+            <Checkbox name="showClassIcon" label="Show class icon" bind:value={$settingsData.general.showClassIcon} />
+        </li>
+        <li>
+            <Checkbox name="showRaceIcon" label="Show race icon" bind:value={$settingsData.general.showRaceIcon} />
+        </li>
+        <li>
+            <Checkbox name="showSpecIcon" label="Show specialization icon" bind:value={$settingsData.general.showSpecIcon} />
+        </li>
+        <li>
+            <Checkbox name="showItemLevel" label="Show item level" bind:value={$settingsData.general.showItemLevel} />
+        </li>
+        <li>
+            <Checkbox name="showRealm" label="Show realm name" bind:value={$settingsData.general.showRealm} />
+        </li>
+    </ul>
+</div>
