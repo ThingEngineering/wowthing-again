42b39037b14025908cab19fb273c0e28051da0cf
Freddie <freddie@wafflemonster.org>
2021-07-05 22:45:18 -0700

    Change job queueing to use pub/sub

--- src/Wowthing.Lib/Utilities/RedisUtilities.cs
+++ src/Wowthing.Lib/Utilities/RedisUtilities.cs
@@ -8,6 +8,7 @@ namespace Wowthing.Lib.Utilities
         public static ConnectionMultiplexer GetConnection(string connectionString)
         {
             var options = ConfigurationOptions.Parse(connectionString);
+            options.ChannelPrefix = "wowthing_";
             options.ClientName = Assembly.GetCallingAssembly().GetName().Name;
 
             return ConnectionMultiplexer.Connect(options);
