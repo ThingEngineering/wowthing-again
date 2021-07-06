d6d4af79c075a736011c719760205e4a2ae60f99
Freddie <freddie@wafflemonster.org>
2021-07-05 22:45:18 -0700

    Change job queueing to use pub/sub

--- src/Wowthing.Lib/Repositories/JobRepository.cs
+++ src/Wowthing.Lib/Repositories/JobRepository.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using StackExchange.Redis;
-using Wowthing.Lib.Extensions;
 using Wowthing.Lib.Jobs;
 
 namespace Wowthing.Lib.Repositories
@@ -12,59 +11,39 @@ namespace Wowthing.Lib.Repositories
     public class JobRepository
     {
         private readonly IConnectionMultiplexer _redis;
-        private readonly string[] _queues;
 
         public JobRepository(IConnectionMultiplexer redis)
         {
             _redis = redis;
-
-            _queues = ((JobPriority[])Enum.GetValues(typeof(JobPriority)))
-                .Select(p => p.GetQueueName())
-                .ToArray();
         }
 
         public async Task AddJobAsync(JobPriority priority, JobType type, params string[] data)
         {
-            var database = _redis.GetDatabase();
-
             var job = new WorkerJob
             {
                 Type = type,
                 Data = data,
             };
-            await database.ListRightPushAsync(priority.GetQueueName(), JsonConvert.SerializeObject(job));
+            await _redis.GetSubscriber().PublishAsync("jobs", JsonConvert.SerializeObject(job));
         }
 
         public async Task AddJobsAsync(JobPriority priority, JobType type, IEnumerable<string[]> datas)
         {
-            var db = _redis.GetDatabase();
-            var jobs = datas.Select(data => new RedisValue(JsonConvert.SerializeObject(new WorkerJob
+            var sub = _redis.GetSubscriber();
+            foreach (var data in datas)
             {
-                Type = type,
-                Data = data,
-            }))).ToArray();
-            await db.ListRightPushAsync(priority.GetQueueName(), jobs);
+                var job = new WorkerJob
+                {
+                    Type = type,
+                    Data = data,
+                };
+                await sub.PublishAsync("jobs", JsonConvert.SerializeObject(job));
+            }
         }
 
         public async Task AddJobsAsync(JobPriority priority, JobType type, IEnumerable<string> datas)
         {
             await AddJobsAsync(priority, type, datas.Select(d => new[] { d }));
-        }
-
-        public async Task<WorkerJob> GetJobAsync()
-        {
-            var database = _redis.GetDatabase();
-
-            foreach (var queueName in _queues)
-            {
-                var result = await database.ListLeftPopAsync(queueName);
-                if (!result.IsNullOrEmpty)
-                {
-                    return JsonConvert.DeserializeObject<WorkerJob>(result);
-                }
-            }
-
-            return null;
         }
 
         public async Task<bool> CheckLastTime(string prefix, string suffix, TimeSpan maximumAge)
