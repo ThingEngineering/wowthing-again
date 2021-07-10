9b8f517870ccddfb0da5f72978e822f77189ecf2
Freddie <freddie@wafflemonster.org>
2021-07-09 20:05:35 -0700

    Add better reputation tooltips
    - Add toNiceNumber utility function, anything >=1000 becomes "12.3k"

--- frontend/components/reputations/TableRowReputation.svelte
+++ frontend/components/reputations/TableRowReputation.svelte
@@ -6,33 +6,33 @@
         Character,
         ReputationTier,
         StaticDataReputation,
+        StaticDataReputationReputation,
         StaticDataReputationSet,
         StaticDataReputationTier,
         TippyProps,
     } from '@/types'
     import findReputationTier from '@/utils/find-reputation-tier'
-    import tippy from '@/utils/tippy'
+    import tippy, {tippyComponent} from '@/utils/tippy'
+
+    import ReputationTooltip from '@/tooltips/reputation/Tooltip.svelte'
 
     const character: Character = getContext('character')
 
     export let reputation: StaticDataReputationSet
 
     let characterRep: number | undefined
+    let repInfo: StaticDataReputationReputation
     let repTier: ReputationTier
     let tooltip: TippyProps
 
     $: {
         if (character !== undefined && reputation !== undefined) {
-            const repInfo =
-                reputation.both ||
-                (character.faction === 0
-                    ? reputation.alliance
-                    : reputation.horde)
+            repInfo = reputation.both || (character.faction === 0 ? reputation.alliance : reputation.horde)
             const dataRep: StaticDataReputation = $data.reputations[repInfo.id]
             characterRep = character.reputations[repInfo.id]
 
             if (characterRep !== undefined && dataRep !== undefined) {
-                const tiers: StaticDataReputationTier = $data.reputationTiers[dataRep.tierId] ?? $data.reputationTiers[0]
+                const tiers: StaticDataReputationTier = $data.reputationTiers[dataRep.tierId] || $data.reputationTiers[0]
                 repTier = findReputationTier(tiers, characterRep)
                 const valueRank = repTier.MaxValue
                     ? `${repTier.Value} / ${repTier.MaxValue} ${repTier.Name}`
@@ -60,7 +60,7 @@
 </style>
 
 {#if characterRep !== undefined && tooltip !== undefined}
-    <td class="reputation{repTier.Tier}" use:tippy={tooltip}
+    <td class="reputation{repTier.Tier}" use:tippyComponent={{component: ReputationTooltip, props: {characterRep, reputation: repInfo}}}
         >{repTier.Percent}%</td
     >
 {:else}
---
+++ frontend/tooltips/reputation/Tooltip.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+    import { data as staticData } from '@/stores/static'
+    import toNiceNumber from '@/utils/to-nice-number'
+
+    import type {StaticDataReputation, StaticDataReputationReputation, StaticDataReputationTier} from '@/types'
+
+    export let characterRep: number
+    export let reputation: StaticDataReputationReputation
+
+    const reps = []
+
+    $: {
+        const dataRep: StaticDataReputation = $staticData.reputations[reputation.id]
+        const tiers: StaticDataReputationTier = $staticData.reputationTiers[dataRep.tierId] || $staticData.reputationTiers[0]
+
+        for (let i = 0; i < tiers.names.length; i++) {
+            const thisOne = (characterRep >= tiers.minValues[i] && characterRep < tiers.maxValues[i])
+            reps.push({
+                cls: 'quality0',
+                maxValue: tiers.maxValues[i],
+                minValue: tiers.minValues[i],
+                name: tiers.names[i],
+                thisOne,
+            })
+        }
+        reps.reverse()
+
+        // Apply quality colours to the top 5 tiers
+        for (let i = 0; i < Math.min(5, reps.length); i++) {
+            reps[i].cls = `quality${Math.abs(i - 5)}`
+            if (reps[i].thisOne && reps[i + 1]) {
+                reps[i + 1].thisOne = false
+            }
+        }
+    }
+</script>
+
+<div class="wowthing-tooltip">
+    <h4>{reputation.name}</h4>
+    <table class="tooltip-reputation table-striped">
+        <tbody>
+            {#each reps as rep}
+                <tr class="{rep.cls}">
+                    {#if characterRep >= rep.maxValue}
+                        <td class="rep-number1" colspan="3">✔</td>
+                    {:else}
+                        <td class="rep-number1">
+                            {#if rep.thisOne}
+                                {toNiceNumber(characterRep - rep.minValue)}
+                            {:else}
+                                0
+                            {/if}
+                        </td>
+                        <td class="rep-separator">/</td>
+                        <td class="rep-number2">{toNiceNumber(rep.maxValue - rep.minValue)}</td>
+                    {/if}
+                    <td class="rep-name">{rep.name}</td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
