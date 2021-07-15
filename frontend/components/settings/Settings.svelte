4edd4774085447e3590b809a02fe52632c769ea4
Freddie <freddie@wafflemonster.org>
2021-07-14 23:56:06 -0700

    Add Group By/Sort By user settings

--- frontend/components/settings/Settings.svelte
+++ frontend/components/settings/Settings.svelte
@@ -1,21 +1,37 @@
 <script lang="ts">
     import SettingsApiKey from './SettingsApiKey.svelte'
     import SettingsGeneral from './SettingsGeneral.svelte'
+    import SettingsGroupBy from './SettingsGroupBy.svelte'
     import SettingsHome from './SettingsHome.svelte'
     import SettingsPrivacy from './SettingsPrivacy.svelte'
     import SettingsSave from './SettingsSave.svelte'
+    import SettingsSortBy from './SettingsSortBy.svelte'
 </script>
 
 <style lang="scss">
     form {
+        display: flex;
         width: 100%;
+    }
+    div {
+        & + div {
+            margin-left: 1rem;
+        }
     }
 </style>
 
 <form>
-    <SettingsSave />
-    <SettingsGeneral />
-    <SettingsHome />
-    <SettingsPrivacy />
-    <SettingsApiKey />
+    <div>
+        <SettingsSave />
+        <SettingsGeneral />
+        <SettingsPrivacy />
+        <SettingsApiKey />
+    </div>
+    <div>
+        <SettingsHome />
+        <SettingsGroupBy />
+    </div>
+    <div>
+        <SettingsSortBy />
+    </div>
 </form>
--- frontend/utils/get-character-sort-func.ts
+++ frontend/utils/get-character-sort-func.ts
@@ -1,30 +1,42 @@
 import { get } from 'svelte/store'
 
+import {Constants} from '@/data/constants'
+import {data as settingsData} from '@/stores/settings'
 import { data as userData } from '@/stores/user'
 import type { Character} from '@/types'
 import {Faction} from '@/types/enums'
 
 export default function getCharacterSortFunc(): (char: Character) => string {
-    // TODO hook this up to settings
-    const groupBy = ['enabled', 'faction']
+    const sortBy = get(settingsData).general.sortBy ?? ['level', 'name']
 
     return (char: Character) => {
         const out: string[] = []
 
-        for (const thing of groupBy) {
+        for (const thing of sortBy) {
             if (thing === 'account') {
-                out.push(get(userData).accounts?.[char.accountId]?.name ?? 'account')
+                out.push(get(userData).accounts?.[char.accountId]?.name ?? `account${char.accountId}`)
             }
             else if (thing === 'enabled') {
                 const enabled = get(userData).accounts?.[char.accountId]?.enabled ?? true
                 out.push(enabled ? 'a' : 'z')
             }
-            else if (thing === 'faction') {
+            else if (thing === 'faction' || thing === '-faction') {
                 out.push(Faction[char.faction])
+            }
+            else if (thing === 'level') {
+                // this will sort by level in descending order
+                out.push(toDigits(Constants.characterMaxLevel - char.level, 2))
+            }
+            else if (thing === 'name') {
+                out.push(char.name)
             }
         }
 
-        console.log(out, char)
+        console.log(out, char.name)
         return out.join('|')
     }
+}
+
+function toDigits(n: number, digits: number): string {
+    return n.toLocaleString(undefined, { minimumIntegerDigits: digits})
 }
