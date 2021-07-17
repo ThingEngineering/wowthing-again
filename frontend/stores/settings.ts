435dd1fedbbcc8f9fc396a0f722634c8905cf5e1
Freddie <freddie@wafflemonster.org>
2021-07-17 14:04:51 -0700

    Clean up handling of auto-refresh

--- frontend/stores/settings.ts
+++ frontend/stores/settings.ts
@@ -1,11 +1,32 @@
 import { writable } from 'svelte/store'
+import type { Writable } from 'svelte/store'
 
+import { fetch as userFetch } from './user'
 import type { Settings } from '@/types'
 
-export const data = writable<Settings>(
-    JSON.parse(document.getElementById('app').getAttribute('data-settings')),
-)
 
-export default {
-    data,
+let interval: number | null = null
+
+function createData(): Writable<Settings> {
+    const initial = JSON.parse(document.getElementById('app').getAttribute('data-settings')) as Settings
+    const { set, subscribe, update } = writable(initial)
+
+    return {
+        set: (settings) => {
+            set(settings)
+
+            if (interval !== null) {
+                clearInterval(interval)
+                interval = null
+            }
+
+            if (settings.general.refreshInterval >= 10) {
+                interval = setInterval(async () => await userFetch(), settings.general.refreshInterval * 1000 * 60)
+            }
+        },
+        subscribe,
+        update,
+    }
 }
+
+export const data = createData()
