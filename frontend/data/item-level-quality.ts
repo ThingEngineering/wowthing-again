4db07197103e2f8055674a0b0e42c345008370a2
Freddie <freddie@wafflemonster.org>
2021-08-09 19:56:54 -0700

    Change item level tiers to be -3 from raid drops

--- frontend/data/item-level-quality.ts
+++ frontend/data/item-level-quality.ts
@@ -1,7 +1,7 @@
 export const itemLevelQuality: number[][] = [
-    [252, 5], // Mythic raid = 252/259, +15 vault = 252
-    [239, 4], // Heroic raid = 239/246, +10 vault = 239
-    [226, 3], // Normal raid = 226/233, + 2 vault = 226
-    [213, 2], // LFR raid    = 213/220
-    [200, 1], //
+    [249, 5], // Mythic raid = 252/259, +15 vault = 252
+    [236, 4], // Heroic raid = 239/246, +10 vault = 239
+    [223, 3], // Normal raid = 226/233, + 2 vault = 226
+    [210, 2], // LFR raid    = 213/220
+    [197, 1], //
 ]
