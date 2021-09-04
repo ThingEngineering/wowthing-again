6f24c7de69339c2f6be14bdd902a79c431412823
Freddie <freddie@wafflemonster.org>
2021-09-03 19:49:51 -0700

    Add saving of uncategorized set info for easier categorization

--- src/Wowthing.Backend/Models/Data/DumpCreature.cs
+++ src/Wowthing.Backend/Models/Data/DumpCreature.cs
@@ -3,11 +3,10 @@
 // ReSharper disable InconsistentNaming
 namespace Wowthing.Backend.Models.Data
 {
-    public class DumpMount
+    public class DumpCreature
     {
         public int ID { get; set; }
-        public int SourceSpellID { get; set; }
-        
+
         [Name("Name_lang")]
         public string Name { get; set; }
     }
