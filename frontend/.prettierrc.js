18958005926acc4fef6326a25868cb593c66a286
Freddie <freddie@wafflemonster.org>
2021-09-04 17:09:47 -0700

    Change TypeScript target to ES2019
    - Misc config updates. Prettier still sucks.

--- frontend/.prettierrc.js
+++ frontend/.prettierrc.js
@@ -1,7 +1,12 @@
 module.exports = {
+    arrowParens: 'always',
+    bracketSpacing: true,
+    printWidth: 100,
+    quoteProps: 'consistent',
     semi: false,
     singleQuote: true,
     tabWidth: 4,
+    trailingComma: 'es5',
 
     svelteSortOrder: 'options-scripts-styles-markup',
 }
