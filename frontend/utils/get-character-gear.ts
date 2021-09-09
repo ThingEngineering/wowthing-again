cbd72b5b151eb2878c5155fc066e6011e7ca6cf0
Freddie <freddie@wafflemonster.org>
2021-09-09 16:10:16 -0700

    Handle missing enchant/gem visibility in getCharacterGear function

--- frontend/utils/get-character-gear.ts
+++ frontend/utils/get-character-gear.ts
@@ -4,15 +4,15 @@ import type {Character, CharacterGear} from '@/types'
 import {Constants} from '@/data/constants'
 import {gemBonusIds, slotOrder, specialValidEnchants, validEnchants} from '@/data/inventory-slot'
 
-export default function getCharacterGear(character: Character): CharacterGear[] {
+export default function getCharacterGear(character: Character, options: GetCharacterGearOptions): CharacterGear[] {
     const ret: CharacterGear[] = []
 
     for (const inventorySlot of slotOrder) {
         const gear: CharacterGear = {
             equipped: character.equippedItems[inventorySlot],
+            highlight: false,
             missingEnchant: false,
             missingGem: false,
-            upgradeable: false,
         }
         ret.push(gear)
 
@@ -20,24 +20,35 @@ export default function getCharacterGear(character: Character): CharacterGear[] 
             continue
         }
 
-        const enchants = validEnchants[inventorySlot]
-        if (enchants) {
-            if (!some(gear.equipped.enchantmentIds, (e) => enchants.indexOf(e) >= 0)) {
-                gear.missingEnchant = true
+        if (options.highlightMissingEnchants === true) {
+            const enchants = validEnchants[inventorySlot]
+            if (enchants) {
+                if (!some(gear.equipped.enchantmentIds, (e) => enchants.indexOf(e) >= 0)) {
+                    gear.missingEnchant = true
+                }
+            }
+
+            const specialEnchants = specialValidEnchants[inventorySlot]
+            if (specialEnchants?.checkFunc(character)) {
+                if (!some(gear.equipped.enchantmentIds, (e) => specialEnchants.enchants.indexOf(e) >= 0)) {
+                    gear.missingEnchant = true
+                }
             }
         }
 
-        const specialEnchants = specialValidEnchants[inventorySlot]
-        if (specialEnchants?.checkFunc(character)) {
-            if (!some(gear.equipped.enchantmentIds, (e) => specialEnchants.enchants.indexOf(e) >= 0)) {
-                gear.missingEnchant = true
+        if (options.highlightMissingGems === true) {
+            if (some(gear.equipped.bonusIds, (b) => gemBonusIds.indexOf(b) >= 0)) {
+                gear.missingGem = gear.equipped.gemIds.length === 0
             }
         }
 
-        if (some(gear.equipped.bonusIds, (b) => gemBonusIds.indexOf(b) >= 0)) {
-            gear.missingGem = gear.equipped.gemIds.length === 0
-        }
+        gear.highlight = gear.missingEnchant || gear.missingGem
     }
 
     return ret
+}
+
+interface GetCharacterGearOptions {
+    highlightMissingEnchants: boolean
+    highlightMissingGems: boolean
 }
