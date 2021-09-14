cf49ddd1f06e31e0d5362280ea2ce1fd803160cd
Freddie <freddie@wafflemonster.org>
2021-09-14 16:20:19 -0700

    Add toNiceTime function

--- frontend/utils/to-nice.ts
+++ frontend/utils/to-nice.ts
@@ -1,4 +1,7 @@
-export default function toNiceNumber(n: number): string {
+import {DateTime} from 'luxon'
+
+
+export function toNiceNumber(n: number): string {
     if (n >= 1000) {
         // 25976 -> 259.76 (divide by 100) -> 259 (floor) -> 25.9 (divide by 10) -> 25.9k
         return `${(Math.floor(n / 100) / 10).toFixed(1).toLocaleString()}k`
@@ -6,4 +9,8 @@ export default function toNiceNumber(n: number): string {
     else {
         return n.toString()
     }
+}
+
+export function toNiceTime(time: DateTime): string {
+    return time.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)
 }
