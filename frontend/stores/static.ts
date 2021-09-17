a74a9cfd1175a7b4033ef9de9bfba5e32082d48e
Freddie <freddie@wafflemonster.org>
2021-09-17 16:43:58 -0700

    Add the fake Honkstrasza realm to StaticDataStore

--- frontend/stores/static.ts
+++ frontend/stores/static.ts
@@ -6,6 +6,15 @@ export class StaticDataStore extends WritableFancyStore<StaticData> {
     get dataUrl(): string {
         return document.getElementById('app').getAttribute('data-static')
     }
+
+    initialize(data: StaticData): void {
+        data.realms[0] = {
+            id: 0,
+            region: 1,
+            name: 'Honkstrasza',
+            slug: 'honkstrasza',
+        }
+    }
 }
 
 export const staticStore = new StaticDataStore()
