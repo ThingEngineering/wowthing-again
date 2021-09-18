0f62a8c45d5da3bf11fe356b7ab24ad266e5b76c
Freddie <freddie@wafflemonster.org>
2021-09-18 14:01:39 -0700

    Handle using item IDs in farms instead of appearance IDs, appearance IDs are annoying

---
+++ src/Wowthing.Backend/Models/Data/DumpItemModifiedAppearance.cs
@@ -0,0 +1,9 @@
+﻿// ReSharper disable InconsistentNaming
+namespace Wowthing.Backend.Models.Data
+{
+    public class DumpItemModifiedAppearance
+    {
+       public int ItemID { get; set; }
+        public int ItemAppearanceID { get; set; }
+    }
+}
