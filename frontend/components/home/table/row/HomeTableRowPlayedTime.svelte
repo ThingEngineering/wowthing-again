b6bf46990b13d6ccc5bdc60ca767c89ffaaa6602
Freddie <freddie@wafflemonster.org>
2021-09-25 14:26:17 -0700

    Hook up played time

---
+++ frontend/components/home/table/row/HomeTableRowPlayedTime.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { toNiceDuration } from '@/utils/to-nice'
+
+    export let playedTotal: number
+
+    let played: string
+    $: {
+        played = toNiceDuration(playedTotal * 1000)
+    }
+</script>
+
+<style lang="scss">
+    td {
+        @include cell-width($width-played);
+
+        text-align: right;
+    }
+    code {
+        background: none;
+    }
+</style>
+
+<td>
+    <code>{@html played}</code>
+</td>
