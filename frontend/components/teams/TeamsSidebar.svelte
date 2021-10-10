ae239643c9f8741671e830c9826dd9e329235c82
Freddie <freddie@wafflemonster.org>
2021-10-10 16:26:20 -0700

    Add basic Teams skeleton
    - Update error page styling

---
+++ frontend/components/teams/TeamsSidebar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { userTeamStore } from '@/stores'
+    import type {SidebarItem} from '@/types'
+
+    import Sidebar from '@/components/sidebar/Sidebar.svelte'
+
+    let categories: SidebarItem[]
+    $: {
+        categories = Object.values($userTeamStore.data.teams)
+        if (categories.length > 0) {
+            categories.push(null)
+        }
+
+        categories.push({
+            name: 'Create Team',
+            slug: 'create-team',
+        })
+    }
+</script>
+
+<Sidebar
+    baseUrl="/teams"
+    items={categories}
+    width="16rem"
+/>
