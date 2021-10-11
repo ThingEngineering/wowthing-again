73d83e552dae06f26fb8def2e18229b30914f5e4
Freddie <freddie@wafflemonster.org>
2021-10-10 18:13:47 -0700

    Lint fixes

--- frontend/types/data/user-team.ts
+++ frontend/types/data/user-team.ts
@@ -3,13 +3,7 @@ export interface UserTeamData {
 }
 
 export interface UserTeamDataTeam {
-    dailyQuests?: Map<number, boolean>
-    quests?: Map<number, boolean>
-    weeklyQuests?: Map<number, boolean>
-
-    dailyQuestsPacked: string
-    questsPacked: string
-    weeklyQuestsPacked: string
-
-    scannedAt: string
+    id: number
+    name: string
+    slug: string
 }
