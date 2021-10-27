ebfb0069ec6f72b1da4fe7423f620f2e3a6c1957
Freddie <freddie@wafflemonster.org>
2021-10-27 09:26:56 -0700

    Sort characters with lockouts first in lockouts

--- frontend/components/lockouts/Lockouts.svelte
+++ frontend/components/lockouts/Lockouts.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import { userStore } from '@/stores'
+    import { data as settingsData } from '@/stores/settings'
     import type {Character} from '@/types'
+    import getCharacterSortFunc from '@/utils/get-character-sort-func'
 
     import CharacterTable from '@/components/character-table/CharacterTable.svelte'
     import CharacterTableHead from '@/components/character-table/CharacterTableHead.svelte'
@@ -10,9 +12,17 @@
     const filterFunc = function(char: Character): boolean {
         return char.level > 10
     }
+
+    const anyLockouts = function(char: Character): string {
+        return Object.keys(char.lockouts || {}).length > 0 ? 'a' : 'z'
+    }
+    const sortFunc = getCharacterSortFunc($settingsData, anyLockouts)
 </script>
 
-<CharacterTable {filterFunc}>
+<CharacterTable
+    {filterFunc}
+    {sortFunc}
+>
     <CharacterTableHead slot="head">
         {#each $userStore.data.allLockouts as instanceDifficulty}
             <HeadInstance {instanceDifficulty} />
