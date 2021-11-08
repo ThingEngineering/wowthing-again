d68ebe4f9e37aa39ce41665b4161d9fa2c02791c
Freddie <freddie@wafflemonster.org>
2021-11-07 16:48:56 -0800

    More achievements fiddling

--- frontend/utils/achievements/get-account-data.ts
+++ frontend/utils/achievements/get-account-data.ts
@@ -1,22 +1,44 @@
 import { get } from 'svelte/store'
 
 import { achievementStore, userAchievementStore } from '@/stores'
-import type { AchievementDataCriteriaTree, Dictionary } from '@/types'
+import type {
+    AchievementData,
+    AchievementDataAchievement,
+    AchievementDataCriteriaTree,
+    UserAchievementData
+} from '@/types'
 
-export function getAccountData(criteriaTree: AchievementDataCriteriaTree): AchievementDataAccount {
-    const ctMap = get(achievementStore).data.criteriaTree
-    const userCrits = get(userAchievementStore).data.criteria
+export function getAccountData(
+    achievementData: AchievementData,
+    userAchievementData: UserAchievementData,
+    achievement: AchievementDataAchievement
+): AchievementDataAccount {
+    //const ctMap = get(achievementStore).data.criteriaTree
+    //const userCrits = get(userAchievementStore).data.criteria
 
     const ret = new AchievementDataAccount()
 
-    for (const child of criteriaTree.children) {
-        const childTree = ctMap[child]
+    const rootCriteriaTree = achievementData.criteriaTree[achievement.criteriaTreeId]
+
+    for (const child of rootCriteriaTree.children) {
+        const childTree = achievementData.criteriaTree[child]
         if (childTree) {
             ret.criteria.push(childTree)
 
-            const value: number[] = (userCrits[child] || [[]])[0]
-            ret.have[child] = value ? value[1] : 0
+            const value: number[] = (userAchievementData.criteria[child] || [[]])[0]
+            ret.have[child] = value[1] || 0
+
+            if (ret.have[child] === 0) {
+                for (const childChild of childTree.children) {
+                    const value: number[] = (userAchievementData.criteria[childChild] || [[]])[0]
+                    ret.have[child] = value[1] || 0
+                }
+            }
         }
+    }
+
+    if (achievement.id === 8728) {
+        console.log(ret)
     }
 
     return ret
@@ -24,7 +46,7 @@ export function getAccountData(criteriaTree: AchievementDataCriteriaTree): Achie
 
 export class AchievementDataAccount {
     criteria: AchievementDataCriteriaTree[]
-    have: Dictionary<number>
+    have: Record<number, number>
 
     constructor() {
         this.criteria = []
