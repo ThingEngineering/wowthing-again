a6156f48fa4f6352bd45b009fe6365a6dba9ef6d
Freddie <freddie@wafflemonster.org>
2021-11-11 10:50:17 -0800

    Add some magic around data set names and sorting

--- frontend/components/appearances/AppearancesTable.svelte
+++ frontend/components/appearances/AppearancesTable.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import filter from 'lodash/filter'
     import find from 'lodash/find'
+    import sortBy from 'lodash/sortBy'
 
     import {transmogStore} from '@/stores'
     import {data as settingsData} from '@/stores/settings'
--- src/Wowthing.Backend/Jobs/Misc/CacheStaticJob.cs
+++ src/Wowthing.Backend/Jobs/Misc/CacheStaticJob.cs
@@ -34,7 +34,7 @@ namespace Wowthing.Backend.Jobs.Misc
             Type = JobType.CacheStatic,
             Priority = JobPriority.High,
             Interval = TimeSpan.FromHours(1),
-            Version = 20,
+            Version = 21,
         };
 
         public override async Task Run(params string[] data)
