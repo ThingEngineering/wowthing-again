19e322b17dccf3904c07af414168e3a9b3747219
Freddie <freddie@wafflemonster.org>
2021-11-13 09:08:11 -0800

    Add set percentages to appearances

--- frontend/stores/user-transmog.ts
+++ frontend/stores/user-transmog.ts
@@ -24,32 +24,38 @@ export class UserTransmogDataStore extends WritableFancyStore<UserTransmogData> 
         const has: Dictionary<UserTransmogDataHas> = {}
         const overallData = has['OVERALL'] = new UserTransmogDataHas(0, 0)
 
-        for (const baseSet of transmogData.sets) {
-            if (baseSet === null) {
+        for (const categories of transmogData.sets) {
+            if (categories === null) {
                 continue
             }
 
-            const baseData = has[baseSet[0].slug] = new UserTransmogDataHas(0, 0)
+            const baseData = has[categories[0].slug] = new UserTransmogDataHas(0, 0)
 
-            for (const set of baseSet.slice(1)) {
-                const setData = has[`${baseSet[0].slug}--${set.slug}`] = new UserTransmogDataHas(0, 0)
+            for (const category of categories.slice(1)) {
+                const catKey = `${categories[0].slug}--${category.slug}`
+                const catData = has[catKey] = new UserTransmogDataHas(0, 0)
 
-                for (const group of set.groups) {
+                for (const group of category.groups) {
                     for (const [dataKey, dataValue] of toPairs(group.data)) {
                         if (skipClasses[dataKey]) {
                             continue
                         }
 
-                        for (let groupIndex = 0; groupIndex < dataValue.length; groupIndex++) {
-                            if (group.sets[groupIndex].endsWith('*')) {
+                        const groupKey = `${catKey}--${group.name}`
+                        for (let setIndex = 0; setIndex < dataValue.length; setIndex++) {
+                            if (group.sets[setIndex].endsWith('*')) {
                                 continue
                             }
 
-                            const groupSigh = dataValue[groupIndex]
+                            const setKey = `${groupKey}--${setIndex}`
+                            const setData = has[setKey] = has[setKey] || new UserTransmogDataHas(0, 0)
+
+                            const groupSigh = dataValue[setIndex]
                             const slotKeys = keys(groupSigh.items)
 
                             overallData.total += slotKeys.length
                             baseData.total += slotKeys.length
+                            catData.total += slotKeys.length
                             setData.total += slotKeys.length
 
                             for (const slotKey of slotKeys) {
@@ -57,6 +63,7 @@ export class UserTransmogDataStore extends WritableFancyStore<UserTransmogData> 
                                     if (userTransmogData.transmog[transmogId]) {
                                         overallData.have++
                                         baseData.have++
+                                        catData.have++
                                         setData.have++
                                         break
                                     }
