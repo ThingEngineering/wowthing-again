1e0c2b6719e53552c5c0b88d0388697aa37198c7
Freddie <freddie@wafflemonster.org>
2021-11-17 18:43:38 -0800

    Add item-search API endpoint

---
+++ src/Wowthing.Web/Models/Search/ItemSearchResponseCharacter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Wowthing.Lib.Enums;
+
+namespace Wowthing.Web.Models.Search
+{
+    public class ItemSearchResponseCharacter
+    {
+        public int CharacterId { get; set; }
+        public int Count { get; set; }
+        public ItemLocation Location { get; set; }
+        public short ItemLevel { get; set; }
+        public short Quality { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public short? Context { get; set; }
+        
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public short? EnchantId { get; set; }
+        
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public short? SuffixId { get; set; }
+        
+        public List<short> BonusIds { get; set; }
+        public List<int> Gems { get; set; }
+    }
+}
