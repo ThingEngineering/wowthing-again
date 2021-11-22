fe11064e84d35677a3b0c2114b256f5041e27b49
Freddie <freddie@wafflemonster.org>
2021-11-22 10:36:32 -0800

    Update Language enum

--- src/Wowthing.Lib/Enums/Language.cs
+++ src/Wowthing.Lib/Enums/Language.cs
@@ -1,7 +1,18 @@
-﻿namespace Wowthing.Lib.Enums
+﻿// ReSharper disable InconsistentNaming
+namespace Wowthing.Lib.Enums
 {
     public enum Language : short
     {
-        English = 1,
+        enUS = 0,
+        koKR = 1,
+        frFR = 2,
+        deDE = 3,
+        zhCN = 4,
+        esES = 5,
+        zhTW = 6,
+        esMX = 7,
+        ruRU = 8,
+        ptBR = 10,
+        itIT = 11,
     }
 }
