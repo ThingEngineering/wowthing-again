9aa024d0888603224ed26392ba4ae27e06033547
Freddie <freddie@wafflemonster.org>
2021-12-12 13:46:53 -0800

    Add PlayerAccountGoldSnapshot model
    - Add GoldSnapshotService to take snapshots of (account, realm) gold values on an hourly basis

---
+++ src/Wowthing.Lib/Migrations/20211212191153_Add_PlayerAccountGoldSnapshot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Wowthing.Lib.Migrations
+{
+    public partial class Add_PlayerAccountGoldSnapshot : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "player_account_gold_snapshot",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    time = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    account_id = table.Column<int>(type: "integer", nullable: false),
+                    realm_id = table.Column<int>(type: "integer", nullable: false),
+                    gold = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_player_account_gold_snapshot", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_player_account_gold_snapshot_player_account_account_id",
+                        column: x => x.account_id,
+                        principalTable: "player_account",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_player_account_gold_snapshot_account_id",
+                table: "player_account_gold_snapshot",
+                column: "account_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "player_account_gold_snapshot");
+        }
+    }
+}
---
+++ src/Wowthing.Lib/Models/Query/LatestGoldSnapshotQuery.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Wowthing.Lib.Models.Query
+{
+    [Keyless]
+    public class LatestGoldSnapshotQuery
+    {
+        public int AccountId { get; set; }
+        public int RealmId { get; set; }
+        public int Gold { get; set; }
+
+        public static string SQL = @"
+SELECT DISTINCT ON (account_id, realm_id)
+    account_id,
+    realm_id,
+    LAST_VALUE(gold) OVER wnd AS gold
+FROM player_account_gold_snapshot
+WINDOW wnd AS (
+    PARTITION BY account_id, realm_id
+    ORDER BY time
+    ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+)
+";
+    }
+}
+ 
