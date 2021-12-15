1b318c7ed9f007473631c1b76b268e8bfe0621d9
Freddie <freddie@wafflemonster.org>
2021-12-14 20:59:18 -0800

    Add gold history option to choose stacked area or line

---
+++ frontend/stores/local-storage/history.ts
@@ -0,0 +1,16 @@
+import { writable } from 'svelte/store'
+
+
+export class HistoryState {
+    public stacked = true
+}
+
+const key = 'state-history'
+const initialState = new HistoryState()
+Object.assign(initialState, JSON.parse(localStorage.getItem(key) ?? '{}'))
+
+export const historyState = writable<HistoryState>(initialState)
+
+historyState.subscribe(state => {
+    localStorage.setItem(key, JSON.stringify(state))
+})
--- frontend/stores/local-storage/index.ts
+++ frontend/stores/local-storage/index.ts
@@ -1,5 +1,6 @@
 export * from './collection'
 export * from './currency'
+export * from './history'
 export * from './journal'
 export * from './reputation'
 export * from './zone-map'
