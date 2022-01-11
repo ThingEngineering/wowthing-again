1a8e18456eb85cd53dd240cc36ea33d3bd1801af
Freddie <freddie@wafflemonster.org>
2022-01-10 23:39:22 -0800

    Add Layout.CovenantColumn setting

--- frontend/types/settings.ts
+++ frontend/types/settings.ts
@@ -18,6 +18,7 @@ export interface Settings {
         commonFields: string[]
         homeFields: string[]
         homeLockouts: number[]
+        covenantColumn: 'current' | 'all'
         padding: 'small' | 'medium' | 'large'
     }
 
