33541a3b925ec16693c9e16ef18d0d7f7700ab48
Freddie <freddie@wafflemonster.org>
2022-01-15 13:22:45 -0800

    Add relevant reputation display to character covenants
    - Add soulshape display to Night Fae

--- frontend/components/characters/shadowlands/CharactersShadowlandsCovenant.svelte
+++ frontend/components/characters/shadowlands/CharactersShadowlandsCovenant.svelte
@@ -3,7 +3,7 @@
     import { DateTime } from 'luxon'
 
     import { Constants } from '@/data/constants'
-    import { covenantFeatureOrder, covenantOrder } from '@/data/covenant'
+    import { covenantFeatureOrder, covenantFeatureReputation, covenantOrder } from '@/data/covenant'
     import { staticStore, timeStore, userQuestStore } from '@/stores'
     import getPercentClass from '@/utils/get-percent-class'
     import getProgress from '@/utils/get-progress'
@@ -15,7 +15,9 @@
         StaticDataProgressCategory,
     } from '@/types'
 
+    import ReputationBar from '@/components/common/ReputationBar.svelte'
     import Soulbind from './CharactersShadowlandsSoulbind.svelte'
+    import Soulshapes from './CharactersShadowlandsSoulshapes.svelte'
 
     export let character: Character
     export let covenantId: number
@@ -23,7 +25,7 @@
     let characterCovenant: CharacterShadowlandsCovenant
     let campaignHave: number
     let campaignTotal: number
-    let features: {maxRank: number, name: string, rank: number, researching: string}[]
+    let features: {key: string, maxRank: number, name: string, rank: number, researching: string}[]
     let renown: number
     $: {
         characterCovenant = character.shadowlands?.covenants?.[covenantId]
@@ -50,6 +52,7 @@
         features = []
         for (const [key, name, maxRank] of covenantFeatureOrder) {
             const featureData = {
+                key,
                 maxRank,
                 name,
                 rank: 0,
@@ -100,14 +103,27 @@
         display: flex;
         justify-content: space-between;
     }
+    .left {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        width: 17rem;
+    }
     .info {
-        width: 16rem;
+        --bar-height: 1.5rem;
 
-        h2 {
+        width: 17rem;
+
+        /*h2 {
             border-bottom: 1px solid $border-color;
             margin-bottom: 0.5rem;
             padding-bottom: 0.5rem;
             text-align: center;
+        }*/
+
+        :global(.progress-container) {
+            margin: 0.25rem -0.1rem 0 -0.1rem;
+            width: calc(100% + 0.2rem);
         }
     }
     .info-row {
@@ -133,47 +149,59 @@
 </style>
 
 <div class="covenant">
-    <div class="info">
-        <h2>Information</h2>
-
-        <div class="info-row large">
-            <div>Renown</div>
-            <div class="drop-shadow {getPercentClass(renown / Constants.maxRenown * 100)}">{renown}</div>
-        </div>
-
-        <div class="info-row large">
-            <div>9.0 Campaign</div>
-            <div class="drop-shadow {getPercentClass(campaignHave / campaignTotal * 100)}">{campaignHave} / {campaignTotal}</div>
-        </div>
-
-        <div class="spacer"></div>
-
-        <div class="info-row large">
-            <div>Anima</div>
-            <div>{toNiceNumber(characterCovenant?.anima ?? 0)}</div>
-        </div>
-
-        <div class="info-row large">
-            <div>Souls</div>
-            <div>{toNiceNumber(characterCovenant?.souls ?? 0)}</div>
-        </div>
-
-        <div class="spacer"></div>
-
-        {#each features as feature, featureIndex}
-            <div class="info-row">
-                <div>{feature.name}</div>
-                <div class="{getPercentClass(feature.rank / feature.maxRank * 100)}">Rank {feature.rank}</div>
+    <div class="left">
+        <div class="info">
+            <div class="info-row large">
+                <div>Renown</div>
+                <div class="drop-shadow {getPercentClass(renown / Constants.maxRenown * 100)}">{renown}</div>
             </div>
 
-            {#if feature.researching}
-                <div class="info-research">{@html feature.researching}</div>
-            {/if}
+            <div class="info-row large">
+                <div>9.0 Campaign</div>
+                <div class="drop-shadow {getPercentClass(campaignHave / campaignTotal * 100)}">{campaignHave} / {campaignTotal}</div>
+            </div>
 
-            {#if featureIndex < (features.length - 1)}
-                <div class="spacer"></div>
-            {/if}
-        {/each}
+            <div class="spacer"></div>
+
+            <div class="info-row large">
+                <div>Anima</div>
+                <div>{toNiceNumber(characterCovenant?.anima ?? 0)}</div>
+            </div>
+
+            <div class="info-row large">
+                <div>Souls</div>
+                <div>{toNiceNumber(characterCovenant?.souls ?? 0)}</div>
+            </div>
+
+            <div class="spacer"></div>
+
+            {#each features as feature, featureIndex}
+                <div class="info-row">
+                    <div>{feature.name}</div>
+                    <div class="{getPercentClass(feature.rank / feature.maxRank * 100)}">Rank {feature.rank}</div>
+                </div>
+
+                {#if feature.researching}
+                    <div class="info-research">{@html feature.researching}</div>
+                {/if}
+
+                {#if feature.rank > 0 && covenantFeatureReputation[`${covenantId}-${feature.key}`]}
+                    <ReputationBar
+                        reputationId={covenantFeatureReputation[`${covenantId}-${feature.key}`]}
+                        {character}
+                    />
+                {/if}
+
+                {#if featureIndex < (features.length - 1)}
+                    <div class="spacer"></div>
+                {/if}
+            {/each}
+
+        </div>
+
+        {#if covenantId === 3}
+            <Soulshapes {character} />
+        {/if}
     </div>
 
     <div class="soulbinds">
---
+++ frontend/components/characters/shadowlands/CharactersShadowlandsSoulshapes.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+    import { userQuestStore } from '@/stores'
+
+    import { crittershapes, soulshapes } from '@/data/covenant'
+    import type { Character } from '@/types'
+
+    import WowheadLink from '@/components/links/WowheadLink.svelte'
+    import WowthingImage from '@/components/images/sources/WowthingImage.svelte'
+
+    export let character: Character
+</script>
+
+<style lang="scss">
+    h3 a {
+        float: right;
+        font-size: 1rem;
+        margin-top: 2px;
+    }
+    .collection {
+        border-width: 0;
+        margin: 0 -0.25rem;
+    }
+    .collection-section {
+        margin-bottom: -0.25rem;
+        padding-bottom: 0;
+        padding-left: 0.25rem;
+    }
+    .collection-objects + .collection-objects {
+        margin-top: 0.5rem;
+    }
+</style>
+
+<div class="collection thing-container">
+    <h3 class="border">
+        Soulshapes
+        <a href="https://www.wowhead.com/guides/soulshapes-night-fae-covenant">Wowhead guide</a>
+    </h3>
+    <div class="collection-section">
+            <div class="collection-objects">
+                {#each soulshapes as [questId, icon]}
+                    <div
+                        class:missing={!$userQuestStore.data.characters[character.id]?.quests?.has(questId)}
+                    >
+                        <WowheadLink
+                            type="quest"
+                            id={questId}
+                        >
+                            <WowthingImage
+                                name={icon}
+                                size={32}
+                                border={1}
+                            />
+                        </WowheadLink>
+                    </div>
+                {/each}
+            </div>
+
+            <div class="collection-objects">
+                {#each crittershapes as [questId, icon]}
+                    <div
+                        class:missing={!$userQuestStore.data.characters[character.id]?.quests?.has(questId)}
+                    >
+                        <WowheadLink
+                            type="quest"
+                            id={questId}
+                        >
+                            <WowthingImage
+                                name={icon}
+                                size={32}
+                                border={1}
+                            />
+                        </WowheadLink>
+                    </div>
+                {/each}
+            </div>
+    </div>
+</div>
