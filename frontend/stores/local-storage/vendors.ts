c360d85977aed2f03cfd26ebdd862a53e586797a
Freddie <freddie@wafflemonster.org>
2022-01-14 18:24:15 -0800

    Add basic collection controls to Vendors

---
+++ frontend/stores/local-storage/vendors.ts
@@ -0,0 +1,18 @@
+import { writable } from 'svelte/store'
+
+
+export class VendorState {
+    public highlightMissing = true
+    public showCollected = true
+    public showUncollected = true
+}
+
+const key = 'state-vendors'
+const initialState = new VendorState()
+Object.assign(initialState, JSON.parse(localStorage.getItem(key) ?? '{}'))
+
+export const vendorState = writable<VendorState>(initialState)
+
+vendorState.subscribe(state => {
+    localStorage.setItem(key, JSON.stringify(state))
+})
