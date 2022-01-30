3ff910d35e362a108a0760ec1051d1fa8d94b853
Freddie <freddie@wafflemonster.org>
2022-01-29 16:23:31 -0800

    Implement the rest of getTransmogSpan

--- frontend/utils/get-transmog-span.ts
+++ frontend/utils/get-transmog-span.ts
@@ -8,16 +8,47 @@ export default function getTransmogSpan(
 ): number {
     let span = set.span
     if (group.type === 'armor') {
-        if (set.type === 'leather') {
+        if (set.type === 'cloth') {
+            if (skipClasses['mage'] === true) {
+                span--
+            }
+            if (skipClasses['priest'] === true) {
+                span--
+            }
+            if (skipClasses['warlock'] === true) {
+                span--
+            }
+        }
+        else if (set.type === 'leather') {
             if (skipClasses['demon-hunter'] === true) {
+                span--
+            }
+            if (skipClasses['druid'] === true) {
                 span--
             }
             if (skipClasses['monk'] === true) {
                 span--
             }
+            if (skipClasses['rogue'] === true) {
+                span--
+            }
+        }
+        else if (set.type === 'mail') {
+            if (skipClasses['hunter'] === true) {
+                span--
+            }
+            if (skipClasses['shaman'] === true) {
+                span--
+            }
         }
         else if (set.type === 'plate') {
             if (skipClasses['death-knight'] === true) {
+                span--
+            }
+            if (skipClasses['paladin'] === true) {
+                span--
+            }
+            if (skipClasses['warrior'] === true) {
                 span--
             }
         }
