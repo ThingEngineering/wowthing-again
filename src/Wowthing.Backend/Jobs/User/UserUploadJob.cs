dc24d447237883ac284c8aaed2764a0e815fd3f6
Freddie <freddie@wafflemonster.org>
2022-03-27 11:04:02 -0700

    Fix error when trying to handle weird progress quest data

--- src/Wowthing.Backend/Jobs/User/UserUploadJob.cs
+++ src/Wowthing.Backend/Jobs/User/UserUploadJob.cs
@@ -756,25 +756,30 @@ namespace Wowthing.Backend.Jobs.User
                     character.AddonQuests.ProgressQuests = new();
                     foreach (var packedProgress in characterData.ProgressQuests.EmptyIfNull())
                     {
-                        var parts = packedProgress.Split('|', StringSplitOptions.RemoveEmptyEntries);
+                        var progressParts = packedProgress.Split('|', StringSplitOptions.RemoveEmptyEntries);
+                        if (progressParts.Length < 5)
+                        {
+                            Logger.Warning("Invalid progress string: {progress}", packedProgress);
+                            continue;
+                        }
                         
                         var progress = new PlayerCharacterAddonQuestsProgress
                         {
-                            Id = int.Parse(parts[1]),
-                            Name = parts[2],
-                            Status = int.Parse(parts[3]),
-                            Expires = int.Parse(parts[4]),
+                            Id = int.Parse(progressParts[1]),
+                            Name = progressParts[2],
+                            Status = int.Parse(progressParts[3]),
+                            Expires = int.Parse(progressParts[4]),
                         };
 
-                        if (parts.Length > 5)
+                        if (progressParts.Length > 5)
                         {
-                            progress.Have = int.Parse(parts[5]);
-                            progress.Need = int.Parse(parts[6]);
-                            progress.Type = parts[7];
-                            progress.Text = parts[8];
+                            progress.Have = int.Parse(progressParts[5]);
+                            progress.Need = int.Parse(progressParts[6]);
+                            progress.Type = progressParts[7];
+                            progress.Text = progressParts[8];
                         }
 
-                        character.AddonQuests.ProgressQuests[parts[0]] = progress;
+                        character.AddonQuests.ProgressQuests[progressParts[0]] = progress;
                     }
                 }
             }
