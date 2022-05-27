77bb02e35c78cc5f283401854d8c480d9c6135ec
Freddie <freddie@wafflemonster.org>
2022-05-26 23:06:47 -0700

    Add more filtering options to Journal

--- frontend/components/journal/JournalInstance.svelte
+++ frontend/components/journal/JournalInstance.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import find from 'lodash/find'
 
+    import { iconStrings } from '@/data/icons'
     import { journalStore } from '@/stores'
-    import { journalState } from '@/stores/local-storage'
+    import { JournalState, journalState } from '@/stores/local-storage'
     import type { JournalDataInstance, JournalDataTier } from '@/types/data'
 
     import CheckboxInput from '@/components/forms/CheckboxInput.svelte'
     import Group from './JournalGroup.svelte'
+    import IconifyIcon from '@/components/images/IconifyIcon.svelte'
     import SectionTitle from '@/components/collections/CollectionSectionTitle.svelte'
 
     export let slug1: string
@@ -20,11 +22,79 @@
             instance = find(tier.instances, (instance) => instance.slug === slug2)
         }
     }
+
+    function getFilters(state: JournalState): string {
+        let byType: string[] = []
+        let byDifficulty: string[] = []
+
+        if (state.showCloth) {
+            byType.push('C')
+        }
+        if (state.showLeather) {
+            byType.push('L')
+        }
+        if (state.showMail) {
+            byType.push('M')
+        }
+        if (state.showPlate) {
+            byType.push('P')
+        }
+        if (state.showTrash) {
+            byType.push('T')
+        }
+        if (state.showWeapons) {
+            byType.push('W')
+        }
+
+        if (byType.length === 0 || byType.length === 6) {
+            byType = ['ALL']
+        }
+
+        if (state.showLfr) {
+            byDifficulty.push('L')
+        }
+        if (state.showNormal) {
+            byDifficulty.push('N')
+        }
+        if (state.showHeroic) {
+            byDifficulty.push('H')
+        }
+        if (state.showMythic) {
+            byDifficulty.push('M')
+        }
+        if (state.showTimewalking) {
+            byDifficulty.push('T')
+        }
+
+        if (byDifficulty.length === 0 || byDifficulty.length === 5) {
+            byDifficulty = ['ALL']
+        }
+
+        return `${byType.join('')} | ${byDifficulty.join('')}`
+    }
 </script>
 
 <style lang="scss">
     .wrapper {
         width: 100%;
+    }
+    .filters-toggle {
+        margin-left: auto;
+        margin-right: 0;
+
+        :global(svg) {
+            margin-top: -4px;
+        }
+    }
+    .filters-container {
+        justify-content: flex-end;
+        margin-top: -0.25rem;
+
+        button {
+            background: #24282f;
+            margin-left: -1px;
+            margin-right: 0;
+        }
     }
 </style>
 
@@ -53,66 +123,121 @@
             >Missing</CheckboxInput>
         </button>
 
-        <button class="margin-left">
-            <CheckboxInput
-                name="show_cloth"
-                bind:value={$journalState.showCloth}
-            >Cloth</CheckboxInput>
-        </button>
+        <button class="filters-toggle"
+            on:click={() => $journalState.filtersExpanded = !$journalState.filtersExpanded}
+        >
+            Filters: {getFilters($journalState)}
 
-        <button>
-            <CheckboxInput
-                name="show_leather"
-                bind:value={$journalState.showLeather}
-            >Leather</CheckboxInput>
-        </button>
-
-        <button>
-            <CheckboxInput
-                name="show_mail"
-                bind:value={$journalState.showMail}
-            >Mail</CheckboxInput>
-        </button>
-
-        <button>
-            <CheckboxInput
-                name="show_plate"
-                bind:value={$journalState.showPlate}
-            >Plate</CheckboxInput>
-        </button>
-
-        <button>
-            <CheckboxInput
-                name="show_weapons"
-                bind:value={$journalState.showWeapons}
-            >Weapons</CheckboxInput>
-        </button>
-
-        <button class="margin-left">
-            <CheckboxInput
-                name="show_timewalking"
-                bind:value={$journalState.showTimewalking}
-            >Timewalking</CheckboxInput>
+            <IconifyIcon
+                icon={iconStrings['chevron-' + ($journalState.filtersExpanded ? 'down' : 'right')]}
+            />
         </button>
     </div>
+
+    {#if $journalState.filtersExpanded}
+        <div class="options-container filters-container">
+            <button>
+                <CheckboxInput
+                    name="show_cloth"
+                    bind:value={$journalState.showCloth}
+                >Cloth</CheckboxInput>
+            </button>
+
+            <button>
+                <CheckboxInput
+                    name="show_leather"
+                    bind:value={$journalState.showLeather}
+                >Leather</CheckboxInput>
+            </button>
+
+            <button>
+                <CheckboxInput
+                    name="show_mail"
+                    bind:value={$journalState.showMail}
+                >Mail</CheckboxInput>
+            </button>
+
+            <button>
+                <CheckboxInput
+                    name="show_plate"
+                    bind:value={$journalState.showPlate}
+                >Plate</CheckboxInput>
+            </button>
+
+            <button>
+                <CheckboxInput
+                    name="show_trash"
+                    bind:value={$journalState.showTrash}
+                >Trash Drops</CheckboxInput>
+            </button>
+
+            <button>
+                <CheckboxInput
+                    name="show_weapons"
+                    bind:value={$journalState.showWeapons}
+                >Weapons</CheckboxInput>
+            </button>
+        </div>
+
+        <div class="options-container filters-container">
+            <button>
+                <CheckboxInput
+                    name="show_lfr"
+                    bind:value={$journalState.showLfr}
+                >LFR</CheckboxInput>
+            </button>
+
+            <button>
+                <CheckboxInput
+                    name="show_normal"
+                    bind:value={$journalState.showNormal}
+                >Normal</CheckboxInput>
+            </button>
+
+            <button>
+                <CheckboxInput
+                    name="show_heroic"
+                    bind:value={$journalState.showHeroic}
+                >Heroic</CheckboxInput>
+            </button>
+
+            <button>
+                <CheckboxInput
+                    name="show_mythic"
+                    bind:value={$journalState.showMythic}
+                >Mythic</CheckboxInput>
+            </button>
+
+            <button>
+                <CheckboxInput
+                    name="show_timewalking"
+                    bind:value={$journalState.showTimewalking}
+                >Timewalking</CheckboxInput>
+            </button>
+        </div>
+    {/if}
 
     {#if instance}
         {#key instance.id}
             <div class="collection thing-container">
                 {#each instance.encounters as encounter, encounterIndex}
-                    <SectionTitle
-                        title={encounter.name}
-                        count={$journalStore.data.stats[`${slug1}--${slug2}--${encounter.name}`]}
-                    />
-                    <div class="collection-section">
-                        {#each encounter.groups as group}
-                            <Group
-                                bonusIds={instance.bonusIds}
-                                stats={$journalStore.data.stats[`${slug1}--${slug2}--${encounter.name}--${group.name}`]}
-                                {group}
+                    {#key encounterIndex}
+                        {#if $journalState.showTrash || encounter.name !== 'Trash Drops'}
+                            <SectionTitle
+                                title={encounter.name}
+                                count={$journalStore.data.stats[`${slug1}--${slug2}--${encounter.name}`]}
                             />
-                        {/each}
-                    </div>
+                            <div class="collection-section">
+                                {#each encounter.groups as group}
+                                    <Group
+                                        bonusIds={instance.bonusIds}
+                                        stats={$journalStore.data.stats[`${slug1}--${slug2}--${encounter.name}--${group.name}`]}
+                                        {group}
+                                    />
+                                {/each}
+                            </div>
+                        {/if}
+                    {/key}
                 {/each}
             </div>
         {/key}
--- frontend/utils/journal/get-filtered-items.ts
+++ frontend/utils/journal/get-filtered-items.ts
@@ -1,11 +1,11 @@
+import cloneDeep from 'lodash/cloneDeep'
 import every from 'lodash/every'
-import filter from 'lodash/filter'
 import some from 'lodash/some'
 
 import getTransmogClassMask from '@/utils/get-transmog-class-mask'
 import type { JournalState } from '@/stores/local-storage'
 import type { Settings } from '@/types'
-import type { JournalDataEncounterItemGroup, JournalDataEncounterItem, UserTransmogData } from '@/types/data'
+import type { JournalDataEncounterItemGroup, JournalDataEncounterItem, JournalDataEncounterItemAppearance, UserTransmogData } from '@/types/data'
 
 
 export default function getFilteredItems(
@@ -17,68 +17,117 @@ export default function getFilteredItems(
     const classMask = getTransmogClassMask(settingsData)
     const masochist = settingsData.transmog.completionistMode
 
-    return filter(
-        group.items,
-        (item: JournalDataEncounterItem) => {
-            let keep = true
+    const items: JournalDataEncounterItem[] = []
 
-            // Class mask is a quick check
-            if (item.classMask > 0) {
-                keep = (item.classMask & classMask) > 0
-            }
+    for (const origItem of group.items) {
+        const item = cloneDeep(origItem)
+        let keep = true
 
-            // Armor types
-            if (keep && group.name === 'Cloth') {
-                keep = journalState.showCloth
-            }
-            if (keep && group.name === 'Leather') {
-                keep = journalState.showLeather
-            }
-            if (keep && group.name === 'Mail') {
-                keep = journalState.showMail
-            }
-            if (keep && group.name === 'Plate') {
-                keep = journalState.showPlate
-            }
+        // Class mask is a quick check
+        if (item.classMask > 0) {
+            keep = (item.classMask & classMask) > 0
+        }
 
-            // Weapons
-            if (keep && group.name === 'Weapons') {
-                keep = journalState.showWeapons
-            }
+        // Armor types
+        if (keep && group.name === 'Cloth') {
+            keep = journalState.showCloth
+        }
+        if (keep && group.name === 'Leather') {
+            keep = journalState.showLeather
+        }
+        if (keep && group.name === 'Mail') {
+            keep = journalState.showMail
+        }
+        if (keep && group.name === 'Plate') {
+            keep = journalState.showPlate
+        }
 
-            // Timewalking
-            if (!journalState.showTimewalking &&
-                keep &&
-                item.appearances.length === 1 &&
-                item.appearances[0].difficulties.length === 1 &&
-                [24, 33].indexOf(item.appearances[0].difficulties[0]) >= 0
-            ) {
-                keep = false
-            }
+        // Weapons
+        if (keep && group.name === 'Weapons') {
+            keep = journalState.showWeapons
+        }
 
-            // Collected/uncollected toggles
-            if (userTransmogData !== null && keep) {
-                const allCollected = every(
-                    item.appearances,
-                    (appearance) => masochist ?
-                        userTransmogData.sourceHas[`${item.id}_${appearance.modifierId}`] :
-                        userTransmogData.userHas[appearance.appearanceId]
-                )
-                const anyCollected = some(
-                    item.appearances,
-                    (appearance) => masochist ?
-                        userTransmogData.sourceHas[`${item.id}_${appearance.modifierId}`] :
-                        userTransmogData.userHas[appearance.appearanceId]
-                )
-                if (
-                    (!journalState.showUncollected && !anyCollected) ||
-                    (!journalState.showCollected && allCollected)
-                ) {
-                    keep = false
+        // Appearances
+        if (keep) {
+            const appearances: JournalDataEncounterItemAppearance[] = []
+
+            for (const appearance of item.appearances) {
+                const difficulties: number[] = []
+
+                for (const difficulty of appearance.difficulties) {
+                    // LFR
+                    if ([7, 17].indexOf(difficulty) >= 0) {
+                        if (journalState.showLfr) {
+                            difficulties.push(difficulty)
+                        }
+                    }
+                    // Normal
+                    else if ([1, 3, 4, 9, 14].indexOf(difficulty) >= 0) {
+                        if (journalState.showNormal) {
+                            difficulties.push(difficulty)
+                        }
+                    }
+                    // Heroic
+                    else if ([2, 5, 6, 15].indexOf(difficulty) >= 0) {
+                        if (journalState.showHeroic) {
+                            difficulties.push(difficulty)
+                        }
+                    }
+                    // Mythic
+                    else if ([8, 16, 23].indexOf(difficulty) >= 0) {
+                        if (journalState.showMythic) {
+                            difficulties.push(difficulty)
+                        }
+                    }
+                    // Timewalking
+                    else if ([24, 33].indexOf(difficulty) >= 0) {
+                        if (journalState.showTimewalking) {
+                            difficulties.push(difficulty)
+                        }
+                    }
+                    else {
+                        difficulties.push(difficulty)
+                    }
+                }
+
+                appearance.difficulties = difficulties
+                if (appearance.difficulties.length > 0) {
+                    appearances.push(appearance)
                 }
             }
 
-            return keep
+            item.appearances = appearances
+            if (appearances.length === 0) {
+                keep = false
+            }
         }
-    )
+
+        // Collected/uncollected toggles
+        if (userTransmogData !== null && keep) {
+            const allCollected = every(
+                item.appearances,
+                (appearance) => masochist ?
+                    userTransmogData.sourceHas[`${item.id}_${appearance.modifierId}`] :
+                    userTransmogData.userHas[appearance.appearanceId]
+            )
+            const anyCollected = some(
+                item.appearances,
+                (appearance) => masochist ?
+                    userTransmogData.sourceHas[`${item.id}_${appearance.modifierId}`] :
+                    userTransmogData.userHas[appearance.appearanceId]
+            )
+            if (
+                (!journalState.showUncollected && !anyCollected) ||
+                (!journalState.showCollected && allCollected)
+            ) {
+                keep = false
+            }
+        }
+
+        if (keep) {
+            items.push(item)
+        }
+    }
+
+    return items
 }
