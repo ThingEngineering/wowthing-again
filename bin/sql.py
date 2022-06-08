72d0c028faf215298276174a5d7b7a24d4c246a7
Freddie <freddie@wafflemonster.org>
2022-06-07 19:53:58 -0700

    Update sql script to handle name changes better

--- bin/sql.py
+++ bin/sql.py
@@ -2,13 +2,12 @@ query = """
 SELECT *
 FROM (
     SELECT  user_name,
-            settings#>'{General,DesiredAccountName}' AS desired
+            REGEXP_REPLACE(settings#>>'{General,DesiredAccountName}', '[ "#]', '') AS desired
     FROM    asp_net_users
 ) oof
 WHERE   desired IS NOT NULL
-        AND desired != '""'
-        AND desired != 'null'
-        AND char_length(user_name) > 30
+        AND desired != ''
+        AND desired != user_name
 ;
 """
 
@@ -23,7 +22,5 @@ for line in blah.splitlines():
     current_name, new_name = line.split(' | ')
     current_name = current_name.strip()
     new_name = new_name.replace('"', '')
-    if len(current_name) != 32:
-        continue
 
     print(f"update asp_net_users set user_name = '{new_name}', normalized_user_name = '{new_name.upper()}' where user_name = '{current_name}';")
