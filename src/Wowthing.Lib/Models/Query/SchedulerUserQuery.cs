08e8d583c5c5d68bb7dfc7226bce0b0a87420962
Freddie <freddie@wafflemonster.org>
2022-06-21 09:04:59 -0700

    Increase user account check to every 60 minutes

--- src/Wowthing.Lib/Models/Query/SchedulerUserQuery.cs
+++ src/Wowthing.Lib/Models/Query/SchedulerUserQuery.cs
@@ -11,8 +11,8 @@ FROM    asp_net_users u
 LEFT JOIN asp_net_user_tokens ut ON u.id = ut.user_id
 WHERE   ut.login_provider = 'BattleNet'
         AND ut.name = 'expires_at'
-        AND (ut.value::timestamp - '10 minute'::interval) > current_timestamp
+        AND (ut.value::timestamp - '60 minute'::interval) > current_timestamp
         AND u.last_api_check != '-infinity'
-        AND u.last_api_check < (current_timestamp - '10 minute'::interval)
+        AND u.last_api_check < (current_timestamp - '60 minute'::interval)
 ";
 }
