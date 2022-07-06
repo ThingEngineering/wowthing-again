8f6631dd4a9027811e453dd34caeeea7679448ce
Freddie <freddie@wafflemonster.org>
2022-07-06 10:02:39 -0700

    Remove Nx

--- packages/testlib/vitest.config.ts
+++
@@ -1,7 +0,0 @@
-import { mergeConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import baseConfig from '../../vitest.config';
-
-export default mergeConfig(baseConfig, {
-  plugins: [svelte({ hot: !process.env.VITEST })],
-});
