967b6e6411c6623822e1ebd8d6c156c02f046c53
Freddie <freddie@wafflemonster.org>
2022-07-13 12:29:30 -0700

    Fix manual vendor data reputations

--- apps/backend/Models/Data/Vendors/DataVendorItem.cs
+++ apps/backend/Models/Data/Vendors/DataVendorItem.cs
@@ -5,6 +5,7 @@
         public int AppearanceId { get; set; }
         public int Id { get; set; }
         public string Note { get; set; }
+        public string Reputation { get; set; }
         public string Type { get; set; }
         public Dictionary<int, int> Costs { get; set; }
     }
