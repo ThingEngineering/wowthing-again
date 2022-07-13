8c235f67768431eaabee1d3d212df1762cd4b374
Freddie <freddie@wafflemonster.org>
2022-07-13 15:35:23 -0700

    Remove toNiceNumber decimal places at 100k

--- apps/frontend/utils/to-nice.ts
+++ apps/frontend/utils/to-nice.ts
@@ -6,6 +6,9 @@ export function toNiceNumber(n: number): string {
         // 2597600 -> 25.976 (divide by 100000) -> 25 (floor) -> 2.5 (divide by 10) -> 2.5m
         return `${(Math.floor(n / 100000) / 10).toFixed(1).toLocaleString()}m`
     }
+    else if (n >= 100000) {
+        return `${Math.floor(n / 1000).toLocaleString()}k`
+    }
     else if (n >= 1000) {
         // 25976 -> 259.76 (divide by 100) -> 259 (floor) -> 25.9 (divide by 10) -> 25.9k
         return `${(Math.floor(n / 100) / 10).toFixed(1).toLocaleString()}k`
