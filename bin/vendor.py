ec682ea44b349644b765f78703c612a9ed9542dc
Freddie <freddie@wafflemonster.org>
2022-07-17 12:27:31 -0700

    Update vendor tool

--- bin/vendor.py
+++ bin/vendor.py
@@ -9,6 +9,13 @@ HEADERS = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
 }
 
+STANDING = {
+    4: 'friendly',
+    5: 'honored',
+    6: 'revered',
+    7: 'exalted',
+}
+
 npc_re = re.compile(r'^var g_pageInfo = .*?typeId: (\d+), name: \"(.*?)\"\}\;$', re.MULTILINE)
 sells_re = re.compile(r'^new Listview\(.*?data: (.*?)\}\)\;$', re.MULTILINE)
 
@@ -18,7 +25,7 @@ r = requests.get(sys.argv[1], headers=HEADERS)
 m = npc_re.search(r.text)
 if m:
     print(f'id: {m.group(1)}')
-    print(f'name: {m.group(2)}')
+    print(f'name: "{m.group(2)}"')
 
 print()
 print('locations:')
@@ -45,6 +52,9 @@ if m:
             for cost in costs[2]:
                 print(f'      1{cost[0]:06}: {cost[1]}')
         
+        if item['standing'] > 0:
+            print(f'    reputation: 0 {STANDING[item["standing"]]}')
+
         print('')
         #print('>', item)
 
