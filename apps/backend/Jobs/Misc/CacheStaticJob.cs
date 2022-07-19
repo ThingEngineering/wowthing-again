85b8cd8dd836ecddc301db5a180d28316059a6c7
Freddie <freddie@wafflemonster.org>
2022-07-19 12:18:50 -0700

    Add IgnoredAchievements (#189)

--- apps/backend/Jobs/Misc/CacheStaticJob.cs
+++ apps/backend/Jobs/Misc/CacheStaticJob.cs
@@ -716,6 +716,7 @@ public class CacheStaticJob : JobBase, IScheduledJob
 
         var achievementMap = records
             .Where(a => !a.Flags.HasFlag(WowAchievementFlags.Tracking))
+            .Where(a => !Hardcoded.IgnoredAchievements.Contains(a.ID))
             .Select(a => new OutAchievement(a))
             .ToDictionary(a => a.Id);
 
