173c15b1ce3925325ddf920086bfbe17c922b2c5
Freddie <freddie@wafflemonster.org>
2022-07-27 21:04:50 -0700

    Add heirlooms data and include it in manual cache

---
+++ apps/backend/Models/Data/Heirlooms/DataHeirloomGroup.cs
@@ -0,0 +1,19 @@
+﻿using Wowthing.Backend.Converters;
+
+namespace Wowthing.Backend.Models.Data.Heirlooms;
+
+[JsonConverter(typeof(DataHeirloomGroupConverter))]
+public class DataHeirloomGroup : ICloneable, IDataCategory
+{
+    public string Name { get; set; }
+    public DataHeirloomItem[] Items { get; set; }
+
+    public object Clone()
+    {
+        return new DataHeirloomGroup
+        {
+            Name = Name,
+            Items = Items,
+        };
+    }
+}
