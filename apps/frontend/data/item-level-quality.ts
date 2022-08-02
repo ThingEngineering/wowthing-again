770211cc6c190b8760fb0535f109f7cbac56bb08
Freddie <freddie@wafflemonster.org>
2022-08-02 09:20:16 -0700

    Update drop levels for SL S4

--- apps/frontend/data/item-level-quality.ts
+++ apps/frontend/data/item-level-quality.ts
@@ -1,7 +1,7 @@
 export const itemLevelQuality: number[][] = [
-    [275, 5], // Mythic raid = 278/285, +15 vault = 278
-    [262, 4], // Heroic raid = 265/272, +10 vault = 265
-    [249, 3], // Normal raid = 252/259, + 2 vault = 252
-    [236, 2], // LFR raid    = 239/246
-    [223, 1], //
+    [301, 5], // Mythic raid = 304/311, +15 vault = 304
+    [288, 4], // Heroic raid = 291/298, +10 vault = 291
+    [275, 3], // Normal raid = 278/285, + 2 vault = 278
+    [262, 2], // LFR raid    = 265/272
+    [249, 1], //
 ]
