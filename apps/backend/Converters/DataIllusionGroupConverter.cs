11b09130b3f33e3d6ed4e68d8f51efd330103524
Freddie <freddie@wafflemonster.org>
2022-08-13 13:20:07 -0700

    Add illusion data and send it to frontend

---
+++ apps/backend/Converters/DataIllusionGroupConverter.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json.Linq;
+using Wowthing.Backend.Models.Data.Illusions;
+
+namespace Wowthing.Backend.Converters;
+
+public class DataIllusionGroupConverter : JsonConverter
+{
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    {
+        var group = (DataIllusionGroup) value;
+        var arr = new JArray();
+        arr.Add(group.Name);
+
+        var itemsArray = new JArray();
+        foreach (var item in group.Items)
+        {
+            var itemArray = new JArray();
+            itemArray.Add(item.Id);
+
+            if (item.Classes?.Count > 0)
+            {
+                var classArray = new JArray();
+                foreach (int classId in item.Classes)
+                {
+                    classArray.Add(classId);
+                }
+                itemArray.Add(classArray);
+            }
+
+            itemsArray.Add(itemArray);
+        }
+        arr.Add(itemsArray);
+
+        arr.WriteTo(writer);
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool CanConvert(Type objectType)
+    {
+        return typeof(DataIllusionGroup) == objectType;
+    }
+
+    public override bool CanRead => false;
+}
---
+++ apps/backend/Models/Data/Illusions/DataIllusiontem.cs
@@ -0,0 +1,7 @@
+﻿namespace Wowthing.Backend.Models.Data.Illusions;
+
+public class DataIllusionItem
+{
+    public int Id { get; set; }
+    public List<int> Classes { get; set; }
+}
