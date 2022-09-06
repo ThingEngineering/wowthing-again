03c5ff152b75655ce766bb6dfcbf355b5412f0a4
Freddie <freddie@wafflemonster.org>
2022-09-06 16:58:18 -0700

    Add initial Discord project

---
+++ apps/discord/Program.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace Wowthing.Discord;
+
+public class Program
+{
+    private DiscordSocketClient _client;
+
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+
+        var token = Environment.GetEnvironmentVariable("WOWTHING_DISCORD_TOKEN");
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block forever
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
