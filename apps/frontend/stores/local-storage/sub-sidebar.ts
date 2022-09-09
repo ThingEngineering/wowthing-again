15b4e9997ed9c9289256b9b628e214e5743604ed
Freddie <freddie@wafflemonster.org>
2022-09-08 22:12:21 -0700

    Implement janky arbitrary expand/collapse for SubSidebar

---
+++ apps/frontend/stores/local-storage/sub-sidebar.ts
@@ -0,0 +1,16 @@
+import { writable } from 'svelte/store'
+
+
+export class SubSidebarState {
+    public expanded: Record<string, boolean> = {}
+}
+
+const key = 'state-sub-sidebar'
+const initialState = new SubSidebarState()
+Object.assign(initialState, JSON.parse(localStorage.getItem(key) ?? '{}'))
+
+export const subSidebarState = writable<SubSidebarState>(initialState)
+
+subSidebarState.subscribe(state => {
+    localStorage.setItem(key, JSON.stringify(state))
+})
