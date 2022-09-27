8e95b10ff719ee1a7ab53ced168c6da8c4a88b63
Freddie <freddie@wafflemonster.org>
2022-09-27 07:03:18 -0700

    Filter out unavailable Sets if skipUnavailable
    - Add per-group percentage in Sets
    - Fix NaN% in Sets

--- apps/frontend/utils/transmog/get-filtered-sets.ts
+++ apps/frontend/utils/transmog/get-filtered-sets.ts
@@ -1,25 +1,49 @@
+import some from 'lodash/some'
+
 import type { Settings } from '@/types'
-import type { TransmogDataGroup } from '@/types/data'
+import type { ManualDataTransmogGroup, ManualDataTransmogGroupData } from '@/types/data/manual'
+import type { UserTransmogData } from '@/types/data'
 
 
 export default function getFilteredSets(
     settings: Settings,
-    group: TransmogDataGroup
+    userTransmogData: UserTransmogData,
+    group: ManualDataTransmogGroup
 ): [boolean, string][] {
     const ret: [boolean, string][] = []
 
     const skipAlliance = !settings.transmog.showAllianceOnly
     const skipHorde = !settings.transmog.showHordeOnly
+    const skipUnavailable = settings.collections.hideUnavailable
 
-    for (const setName of group.sets) {
+    for (let setIndex = 0; setIndex < group.sets.length; setIndex++) {
+        const setName = group.sets[setIndex]
         ret.push([
             !(
                 (skipAlliance && setName.indexOf(':alliance:') >= 0) ||
-                (skipHorde && setName.indexOf(':horde:') >= 0)
+                (skipHorde && setName.indexOf(':horde:') >= 0) ||
+                (skipUnavailable && setName.endsWith('*') && !hasAnyOfSets(userTransmogData,
+                    Object.values(group.data).map((datas) => datas[setIndex])))
             ),
             setName
         ])
     }
 
     return ret
+}
+
+function hasAnyOfSets(
+    userTransmogData: UserTransmogData,
+    sets: ManualDataTransmogGroupData[]
+): boolean {
+    return some(
+        sets,
+        (set) => some(
+            set.items || {},
+            (itemIds) => some(
+                itemIds,
+                (itemId) => userTransmogData.userHas[itemId]
+            )
+        )
+    )
 }
