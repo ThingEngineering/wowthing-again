708ffd74ebff6683faccf163cca29e64dda1339a
Freddie <freddie@wafflemonster.org>
2022-10-02 14:35:15 -0700

    Generate appearanceID -> itemIDs mapping in ItemStore

--- apps/frontend/stores/item.ts
+++ apps/frontend/stores/item.ts
@@ -1,3 +1,5 @@
+import sortBy from 'lodash'
+
 import { WritableFancyStore } from '@/types'
 import { ItemDataItem, type ItemData } from '@/types/data/item'
 
@@ -10,12 +12,28 @@ export class ItemDataStore extends WritableFancyStore<ItemData> {
     initialize(data: ItemData) {
         // console.time('ItemDataStore.initialize')
 
+        const appearanceIds: Record<number, Record<number, boolean>> = {}
+
         data.items = {}
         for (const itemArray of data.rawItems) {
             const obj = new ItemDataItem(...itemArray)
             data.items[obj.id] = obj
+
+            for (const appearance of Object.values(obj.appearances)) {
+                appearanceIds[appearance.appearanceId] ||= {}
+                appearanceIds[appearance.appearanceId][obj.id] = true
+            }
         }
         data.rawItems = null
+
+        data.appearanceToItems = Object.fromEntries(
+            Object.entries(appearanceIds)
+                .map(([id, items]) => [
+                    parseInt(id),
+                    Object.keys(items)
+                        .map((itemId) => parseInt(itemId))
+                ])
+        )
 
         // console.timeEnd('ItemDataStore.initialize')
     }
