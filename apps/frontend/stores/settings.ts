7af6a8b57e09045a1008fcb846d1d7963c39efa0
Freddie <freddie@wafflemonster.org>
2022-10-21 16:56:16 -0700

    Only activate auto-refresh for non-public user data

--- apps/frontend/stores/settings.ts
+++ apps/frontend/stores/settings.ts
@@ -1,4 +1,4 @@
-import { writable } from 'svelte/store'
+import { get, writable } from 'svelte/store'
 
 import { userAchievementStore } from './user-achievements'
 import { userStore } from './user'
@@ -18,7 +18,7 @@ export const data = {
             interval = null
         }
 
-        if (settings.general.refreshInterval > 0) {
+        if (get(userStore).data.public === false && settings.general.refreshInterval > 0) {
             interval = setInterval(
                 async () => await Promise.all([
                     userAchievementStore.fetch({ evenIfLoaded: true }),
