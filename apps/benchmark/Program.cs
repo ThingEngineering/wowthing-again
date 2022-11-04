4a3552a0f4ee95ee5dc77f7b17478bc6f7753d2c
Freddie <freddie@wafflemonster.org>
2022-11-03 19:54:33 -0700

    Add experimental Lua parser stuff

---
+++ apps/benchmark/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Wowthing.Lib.Utilities;
+using Wowthing.Lib.Utilities.LuaParser;
+
+[MemoryDiagnoser]
+public class JsonConverterBench
+{
+    private readonly string _data;
+
+    public JsonConverterBench()
+    {
+        _data = File.ReadAllText("/home/freddie/projects/wowthing-again/temp/WoWthing_Collector.lua");
+    }
+
+    [Benchmark]
+    public string Original() => LuaToJsonConverter.Convert(_data);
+
+    [Benchmark]
+    public string StringBuilder() => LuaToJsonConverter2.Convert(_data);
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkSwitcher
+            .FromAssembly(typeof(Program).Assembly)
+            .Run(args);
+    }
+}
---
+++ apps/luaparser/Program.cs
@@ -0,0 +1,14 @@
+﻿using Wowthing.Lib.Utilities;
+using Wowthing.Lib.Utilities.LuaParser;
+
+var data = File.ReadAllText("/home/freddie/projects/wowthing-again/temp/WoWthing_Collector.lua")
+    .Replace("WWTCSaved = ", "");
+Console.WriteLine("data    : {0:n0}", data.Length);
+
+var originalVersion = LuaToJsonConverter.Convert(data);
+Console.WriteLine("original: {0:n0}", originalVersion.Length);
+File.WriteAllText("/home/freddie/projects/wowthing-again/temp/lua1.json", originalVersion);
+
+var newVersion = LuaToJsonConverter2.Convert(data);
+Console.WriteLine("new     : {0:n0}", newVersion.Length);
+File.WriteAllText("/home/freddie/projects/wowthing-again/temp/lua2.json", newVersion);
