3628e99f0453f9f582101b0f719352d5f4676cc7
Freddie <freddie@wafflemonster.org>
2022-12-12 12:18:32 -0800

    Add season-df-1 to RaiderIo season map

--- apps/backend/Models/API/NonBlizzard/ApiCharacterRaiderIo.cs
+++ apps/backend/Models/API/NonBlizzard/ApiCharacterRaiderIo.cs
@@ -21,6 +21,7 @@ public class ApiCharacterRaiderIoSeason
         { "season-sl-2", 6 },
         { "season-sl-3", 7 },
         { "season-sl-4", 8 },
+        { "season-df-1", 9 },
     };
 
     public string Season { get; set; }
