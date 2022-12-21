a4374c7be63d6ffeee6822ab3971ff6b63efe48b
Freddie <freddie@wafflemonster.org>
2022-12-20 22:22:33 -0800

    Add basic Character Professions view

---
+++ apps/frontend/components/characters/professions/CharacterProfessions.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import type { Character } from '@/types'
+
+    export let character: Character
+</script>
+
+<style lang="scss">
+</style>
+
+<div class="professions">
+
+</div>
