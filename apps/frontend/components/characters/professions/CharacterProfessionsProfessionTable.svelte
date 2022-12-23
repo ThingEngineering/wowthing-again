c1039bece82e83b09eb366ea3946542ff5a2c01a
Freddie <freddie@wafflemonster.org>
2022-12-22 18:35:03 -0800

    Add tier 2/3 stars for Dragonflight recipes

--- apps/frontend/components/characters/professions/CharacterProfessionsProfessionTable.svelte
+++ apps/frontend/components/characters/professions/CharacterProfessionsProfessionTable.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
     import type { StaticDataProfessionCategory } from '@/types/data/static'
 
+    import ParsedText from '@/components/common/ParsedText.svelte'
     import WowheadLink from '@/components/links/WowheadLink.svelte'
     import WowthingImage from '@/components/images/sources/WowthingImage.svelte'
 
     export let knownRecipes: Set<number>
     export let category: StaticDataProfessionCategory
+
+    const getFixedName = function(name: string): string {
+        name = name.replace(/- \|A:Professions-Icon-Quality-Tier\d-Small:20:20\|a/, ':starFull:')
+        return name
+    }
 </script>
 
 <style lang="scss">
@@ -34,6 +40,16 @@
             margin-right: 0.1rem;
         }
     }
+    .tier2 {
+        :global(span[data-string="starFull"]) {
+            color: rgb(215, 215, 215);
+        }
+    }
+    .tier3 {
+        :global(span[data-string="starFull"]) {
+            color: rgb(255, 215, 0);
+        }
+    }
 </style>
 
 {#if category.abilities.length > 0}
@@ -50,6 +66,8 @@
                         class="ability-name"
                         class:status-success={userHas}
                         class:status-fail={!userHas}
+                        class:tier2={ability.name.includes('Tier2')}
+                        class:tier3={ability.name.includes('Tier3')}
                     >
                         <WowheadLink
                             id={ability.spellId}
@@ -60,7 +78,7 @@
                                 size={20}
                                 border={1}
                             />
-                            {ability.name}
+                            <ParsedText text={getFixedName(ability.name)} />
                         </WowheadLink>
                     </td>
                 </tr>
