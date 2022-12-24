404f3e078957af339fc32fa4c3c6b361c0962f2a
Freddie <freddie@wafflemonster.org>
2022-12-24 11:11:26 -0800

    Update profession ignore lists

--- apps/backend/Data/IgnoredSkillLineAbilities.cs
+++ apps/backend/Data/IgnoredSkillLineAbilities.cs
@@ -4,10 +4,14 @@ public static partial class Hardcoded
 {
     public static readonly HashSet<int> IgnoredSkillLineAbilities = new()
     {
-        #region Mining
-        // Dragonflight
+        // Alchemy
+        28577, // Major Holy Protection Potion
+        156567, // Draenor Armor Flask
+        370771, // Dragon Isles Alchemy Troubleshooting Test Recipe (DNT)
+        371635, // Demonstration Item Recipe
+
+        // Mining
         389465, // Severite Seam
         389458, // Draconium Seam
-        #endregion
     };
 }
