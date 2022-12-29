85eee544e0fabb68af9c051da4b689d6c84a0374
Freddie <freddie@wafflemonster.org>
2022-12-28 16:44:46 -0800

    Add crafted quality display to profession tooltip

--- apps/frontend/components/tooltips/professions/TooltipProfessionsEquipment.svelte
+++ apps/frontend/components/tooltips/professions/TooltipProfessionsEquipment.svelte
@@ -5,7 +5,9 @@
     import type { Character, CharacterEquippedItem } from '@/types'
     import type { StaticDataProfession} from '@/types/data/static'
 
+    import IconifyIcon from '@/components/images/IconifyIcon.svelte'
     import WowthingImage from '@/components/images/sources/WowthingImage.svelte'
+    import { iconStrings } from '@/data/icons';
 
     export let character: Character
     export let profession: StaticDataProfession
@@ -46,6 +48,15 @@
     .item-level {
         bottom: 2px;
     }
+    .crafted-quality {
+        --scale: 1.2;
+
+        background: rgba(0, 0, 0, 1);
+        border-radius: 50%;
+        position: absolute;
+        right: -1px;
+        top: -1px;
+    }
     .empty-slot {
         background: rgba(0, 0, 0, 0.2);
         border-width: var(--image-border-width);
@@ -58,13 +69,21 @@
     {#each Array(3) as _, index}
         {@const equippedItem = equippedItems[index]}
         {#if equippedItem}
+            {@const craftedQuality = equippedItem.craftedQuality || 1}
             <div class="item quality{equippedItem.quality}">
                 <WowthingImage
                     name="item/{equippedItem.itemId}"
                     size={48}
                     border={2}
                 />
+                
                 <span class="item-level pill abs-center">{equippedItem.itemLevel}</span>
+
+                <span class="crafted-quality quality{craftedQuality}">
+                    <IconifyIcon
+                        icon={iconStrings[`circle${craftedQuality}`]}
+                    />
+                </span>
             </div>
         {:else}
             <div class="empty-slot border"></div>
