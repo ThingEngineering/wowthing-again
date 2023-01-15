03e0eb9a511b8e7bb00fd271967dbfa8352b43e8
Freddie <freddie@wafflemonster.org>
2023-01-15 14:35:52 -0800

    Calculate time remaining to rested fullness

--- apps/frontend/components/home/table/row/HomeTableRowRestedExperience.svelte
+++ apps/frontend/components/home/table/row/HomeTableRowRestedExperience.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
     import { timeStore } from '@/stores'
-    import { getCharacterRested } from '@/utils/get-character-rested';
+    import { getCharacterRested } from '@/utils/get-character-rested'
     import tippy from '@/utils/tippy'
     import type { Character } from '@/types'
 
     export let character: Character
 
-    let rested: string
-    $: {
-        rested = getCharacterRested($timeStore, character)
-    }
+    $: [rested,] = getCharacterRested($timeStore, character)[0]
 </script>
 
 <style lang="scss">
