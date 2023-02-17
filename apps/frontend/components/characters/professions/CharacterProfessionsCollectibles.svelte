839a305e9eb819f79531cbf212969d020c6c4b62
Freddie <freddie@wafflemonster.org>
2023-02-17 15:41:14 -0800

    Add collectible books to Characters>Professions

--- apps/frontend/components/characters/professions/CharacterProfessionsCollectibles.svelte
+++ apps/frontend/components/characters/professions/CharacterProfessionsCollectibles.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
     import mdiCheckboxOutline from '@iconify/icons-mdi/check-circle-outline'
 
-    import { dragonflightProfessionMap, type DragonflightProfession } from '@/data/professions'
     import { Constants } from '@/data/constants'
     import { expansionSlugMap } from '@/data/expansion'
-    import { itemStore, userQuestStore } from '@/stores'
+    import { dragonflightProfessionMap } from '@/data/professions'
+    import { staticStore, userQuestStore } from '@/stores'
+    import findReputationTier from '@/utils/find-reputation-tier'
     import type { Character } from '@/types'
+    import type { DragonflightProfession } from '@/types/data'
     import type { StaticDataProfession } from '@/types/data/static'
-    
+
+    import Collectible from './CharacterProfessionsCollectible.svelte'
     import IconifyIcon from '@/components/images/IconifyIcon.svelte'
-    import WowheadLink from '@/components/links/WowheadLink.svelte'
     import WowthingImage from '@/components/images/sources/WowthingImage.svelte'
 
     export let character: Character
@@ -33,63 +35,83 @@
         
         dfData = dragonflightProfessionMap[staticProfession.id]
     }
+
+    $: tiers = $staticStore.reputationTiers[398]
+                
+
 </script>
 
 <style lang="scss">
     .profession-collectibles {
         --image-border-width: 2px;
 
+        flex-direction: column;
+        margin: 0 0.5rem;
+    }
+    .collection-objects {
+        flex-wrap: wrap;
         justify-content: center;
-        margin: 1rem 1rem 0 1rem;
+        margin-top: 1rem;
+        width: 100%;
+    }
+    .collectible-group {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 5px;
+
+        > div {
+            position: relative;
+        }
     }
 </style>
 
 {#if dfData}
-    <div class="profession-collectibles collection-objects">
-        {#if dfData.masterQuestId}
-            {@const userHas = userQuestStore.hasAny(character.id, dfData.masterQuestId)}
-            <div
-                class="quality5"
-                class:missing={userHas}
-            >
-                <WowthingImage
-                    name="achievement/1683"
-                    size={48}
-                    border={2}
-                    tooltip={"Profession Master"}
-                />
+    <div class="profession-collectibles">
+        {#if dfData.masterQuestId || dfData?.bookQuests?.length > 0}
+            <div class="collection-objects">
+                {#if dfData.masterQuestId}
+                    {@const userHas = userQuestStore.hasAny(character.id, dfData.masterQuestId)}
+                    <div
+                        class="quality5"
+                        class:missing={userHas}
+                    >
+                        <WowthingImage
+                            name="achievement/1683"
+                            size={40}
+                            border={2}
+                            tooltip={"Profession Master"}
+                        />
 
-                {#if userHas}
-                    <div class="collected-icon drop-shadow">
-                        <IconifyIcon icon={mdiCheckboxOutline} />
+                        {#if userHas}
+                            <div class="collected-icon drop-shadow">
+                                <IconifyIcon icon={mdiCheckboxOutline} />
+                            </div>
+                        {/if}
                     </div>
                 {/if}
+
+                {#each (dfData.bookQuests || []) as bookQuest, questIndex}
+                    {@const repRank = [2, 4, 5][questIndex]}
+                    {@const repTier = findReputationTier(tiers, character.reputations?.[2544] || 0)}
+                    <Collectible
+                        disabled={repTier.tier > (6 - repRank)}
+                        itemId={bookQuest.itemId}
+                        text={`AC ${repRank}`}
+                        userHas={userQuestStore.hasAny(character.id, bookQuest.questId)}
+                    />
+                {/each}
             </div>
         {/if}
 
-        {#each (dfData.treasureQuests || []) as treasureQuest}
-            {@const userHas = userQuestStore.hasAny(character.id, treasureQuest.questId)}
-            <div
-                class="quality{$itemStore.items[treasureQuest.itemId]?.quality || 1}"
-                class:missing={userHas}
-            >
-                <WowheadLink
-                    type="item"
-                    id={treasureQuest.itemId}
-                >
-                    <WowthingImage
-                        name="item/{treasureQuest.itemId}"
-                        size={48}
-                        border={2}
+        {#if dfData.treasureQuests?.length > 0}
+            <div class="collection-objects">
+                {#each dfData.treasureQuests as treasureQuest}
+                    <Collectible
+                        itemId={treasureQuest.itemId}
+                        userHas={userQuestStore.hasAny(character.id, treasureQuest.questId)}
                     />
-                </WowheadLink>
-
-                {#if userHas}
-                    <div class="collected-icon drop-shadow">
-                        <IconifyIcon icon={mdiCheckboxOutline} />
-                    </div>
-                {/if}
+                {/each}
             </div>
-        {/each}
+        {/if}
     </div>
 {/if}
