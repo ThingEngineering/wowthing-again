c624d5ec1ebaba947899e8f07079fdc20a044069
Freddie <freddie@wafflemonster.org>
2023-03-04 13:09:51 -0800

    Add contract statuses

--- apps/frontend/components/home/table/row/HomeTableRowStatuses.svelte
+++ apps/frontend/components/home/table/row/HomeTableRowStatuses.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
+    import { DateTime } from 'luxon'
+
     import { Constants } from '@/data/constants'
+    import { contractAuras } from '@/data/reputation'
+    import { staticStore, timeStore } from '@/stores'
+    import { toNiceDuration } from '@/utils/formatting'
     import type { Character } from '@/types'
 
     import WowthingImage from '@/components/images/sources/WowthingImage.svelte'
+    import ParsedText from '@/components/common/ParsedText.svelte';
 
     export let character: Character
 
@@ -10,18 +16,42 @@
     $: {
         images = []
         
-        if (character.auras?.indexOf(359530) >= 0) {
+        if (character.auras?.[359530] === 0) {
             images.push([Constants.icons.anniversary, 'Anniversary Buff'])
         }
-        if (character.chromieTime) {
-            images.push([Constants.icons.chromieTime, 'Chromie Time'])
+
+        if (character.level < Constants.characterMaxLevel) {
+            if (character.chromieTime) {
+                images.push([Constants.icons.chromieTime, 'Chromie Time'])
+            }
+            if (character.isResting) {
+                images.push([Constants.icons.resting, 'Resting'])
+            }
         }
-        if (character.isResting && character.level < Constants.characterMaxLevel) {
-            images.push([Constants.icons.resting, 'Resting'])
-        }
+
         if (character.isWarMode) {
             images.push([Constants.icons.warMode, 'War Mode'])
         }
+
+        for (const spellId in contractAuras) {
+            if (character.auras?.[spellId] > 0) {
+                const diff = DateTime.fromSeconds(character.auras[spellId])
+                    .diff($timeStore)
+                    .toMillis()
+                if (diff <= 0) {
+                    continue
+                }
+                
+                const niceRemaining = toNiceDuration(diff).replace('&nbsp;', '')
+                const [reputationId, rank] = contractAuras[spellId]
+                const reputation = $staticStore.reputations[reputationId]
+                images.push([
+                    `spell/${spellId}`,
+                    `<div class="center">{craftedQuality:${rank}} ${reputation.name}<br>${niceRemaining} remaining</div>`
+                ])
+            }
+        }
+        
         if (character.currencies?.[Constants.currencies.dragonridingPassengers]?.quantity > 0) {
             images.push([Constants.icons.dragonridingPassengers, 'Dragonriding Passengers'])
         }
@@ -53,7 +83,7 @@
             name={icon}
             size={20}
             border={1}
-            tooltip={tooltip}
+            {tooltip}
         />
     {/each}
 </td>
--- apps/frontend/data/reputation.ts
+++ apps/frontend/data/reputation.ts
@@ -28,3 +28,21 @@ export const factionMaxRenown: Record<number, number> = {
     2503: 25, // Maruuk Centaur
     2510: 30, // Valdrakken Accord
 }
+
+export const contractAuras: Record<number, [number, number]> = {
+    384317: [2544, 1], // Contract: Artisan's Consortium 1
+    384320: [2544, 2], // Contract: Artisan's Consortium 2
+    384321: [2544, 3], // Contract: Artisan's Consortium 3
+    384468: [2507, 1], // Contract: Dragonscale Expedition 1
+    384469: [2507, 2], // Contract: Dragonscale Expedition 2
+    384470: [2507, 3], // Contract: Dragonscale Expedition 3
+    384461: [2511, 1], // Contract: Iskaara Tuskarr 1
+    384460: [2511, 2], // Contract: Iskaara Tuskarr 2
+    384459: [2511, 3], // Contract: Iskaara Tuskarr 3
+    384465: [2503, 1], // Contract: Maruuk Centaur 1
+    384466: [2503, 2], // Contract: Maruuk Centaur 2
+    384467: [2503, 3], // Contract: Maruuk Centaur 3
+    384462: [2510, 1], // Contract: Valdrakken Accord 1
+    384463: [2510, 1], // Contract: Valdrakken Accord 2
+    384464: [2510, 1], // Contract: Valdrakken Accord 3
+}
