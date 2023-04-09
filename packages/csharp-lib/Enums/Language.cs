9eb98c79bcd67b814fd84ac672595806f41fb96d
Freddie <freddie@wafflemonster.org>
2023-04-08 19:56:45 -0700

    Enable koKR/zhCN/zhTW

--- packages/csharp-lib/Enums/Language.cs
+++ packages/csharp-lib/Enums/Language.cs
@@ -4,12 +4,12 @@ namespace Wowthing.Lib.Enums;
 public enum Language : short
 {
     enUS = 0,
-    //koKR = 1,
+    koKR = 1,
     frFR = 2,
     deDE = 3,
-    //zhCN = 4,
+    zhCN = 4,
     esES = 5,
-    //zhTW = 6,
+    zhTW = 6,
     esMX = 7,
     ruRU = 8,
     ptBR = 10,
