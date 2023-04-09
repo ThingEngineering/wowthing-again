763b04e892a74ec6c02c6c0a900e386e489e13b5
Freddie <freddie@wafflemonster.org>
2023-04-08 19:57:15 -0700

    Remove WowReputationTier.MaxValues and .Names

---
+++ packages/csharp-lib/Migrations/20230409024024_Remove_WowReputationTier_fields.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wowthing.Lib.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_WowReputationTier_fields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "max_values",
+                table: "wow_reputation_tier");
+
+            migrationBuilder.DropColumn(
+                name: "names",
+                table: "wow_reputation_tier");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int[]>(
+                name: "max_values",
+                table: "wow_reputation_tier",
+                type: "integer[]",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string[]>(
+                name: "names",
+                table: "wow_reputation_tier",
+                type: "text[]",
+                nullable: true);
+        }
+    }
+}
--- packages/csharp-lib/Models/Wow/WowReputationTier.cs
+++ packages/csharp-lib/Models/Wow/WowReputationTier.cs
@@ -6,8 +6,11 @@ public class WowReputationTier
 {
     [DatabaseGenerated(DatabaseGeneratedOption.None)]
     public int Id { get; set; }
-        
+
     public int[] MinValues { get; set; }
-    public int[] MaxValues { get; set; }
-    public string[] Names { get; set; }
+
+    public WowReputationTier(int id)
+    {
+        Id = id;
+    }
 }
