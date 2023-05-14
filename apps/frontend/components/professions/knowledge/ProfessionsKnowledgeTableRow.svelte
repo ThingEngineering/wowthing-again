19f20c0e59fd88737c0d19acfb12038d797674de
Freddie <freddie@wafflemonster.org>
2023-05-13 22:52:34 -0700

    Implement a somewhat useful Professions>Knowledge page

---
+++ apps/frontend/components/professions/knowledge/ProfessionsKnowledgeTableRow.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+    import { dragonflightKnowledge, dragonflightProfessionMap, dragonflightProfessions } from '@/data/professions'
+    import { userQuestStore } from '@/stores'
+    import { UserCount } from '@/types'
+    import type { Profession } from '@/enums'
+    import type {  Character } from '@/types'
+    import getPercentClass from '@/utils/get-percent-class';
+
+    export let character: Character
+
+    let counts: UserCount[]
+    $: {
+        counts = []
+
+        dragonflightKnowledge.forEach((dk) => {
+            if (dk === null) {
+                counts.push(null)
+                return
+            }
+
+            const dkCount = new UserCount()
+            
+            for (const professionId of (dk.masters || [])) {
+                if (character.professions?.[professionId]) {
+                    const profData = dragonflightProfessionMap[professionId]
+                    
+                    dkCount.total++
+                    if (userQuestStore.hasAny(character.id, profData.masterQuestId)) {
+                        dkCount.have++
+                    }
+                }
+            }
+            
+            for (const profData of dragonflightProfessions) {
+                if (!character.professions?.[profData.id]) {
+                    continue
+                }
+
+                if (dk.shortName === 'VD') {
+                    for (const bookQuest of (profData.bookQuests || [])) {
+                        dkCount.total++
+                        if (userQuestStore.hasAny(character.id, bookQuest.questId)) {
+                            dkCount.have++
+                        }
+                    }
+                }
+
+                for (const treasureQuest of (profData.treasureQuests || [])) {
+                    if (treasureQuest.source === dk.shortName) {
+                        dkCount.total++
+                        if (userQuestStore.hasAny(character.id, treasureQuest.questId)) {
+                            dkCount.have++
+                        }
+                    }
+                }
+            }
+
+            counts.push(dkCount)
+        })
+
+        console.log(counts)
+    }
+</script>
+
+<style lang="scss">
+    .profession-knowledge {
+        @include cell-width($width-progress, $maxWidth: $width-progress-max);
+
+        border-left: 1px solid $border-color;
+        text-align: center;
+    }
+    .faded {
+        color: #aaa;
+    }
+</style>
+
+<td class="spacer"></td>
+{#each dragonflightKnowledge as dk, dkIndex}
+    {@const count = counts[dkIndex]}
+    {#if dk === null}
+        <td class="spacer"></td>
+    {:else if count.total > 0}
+        <td
+            class="profession-knowledge"
+            class:status-fail={count.have === 0}
+            class:status-shrug={count.have > 0 && count.have < count.total}
+            class:status-success={count.have === count.total}
+        >
+            {count.have} / {count.total}
+        </td>
+    {:else}
+        <td class="profession-knowledge faded">---</td>
+    {/if}
+{/each}
---
+++ apps/frontend/data/professions/knowledge.ts
@@ -0,0 +1,64 @@
+import { Profession } from '@/enums'
+
+type DragonflightKnowledge = {
+    name: string
+    shortName: string
+    icon: string
+    masters: Profession[]
+}
+
+export const dragonflightKnowledge: DragonflightKnowledge[] = [
+    {
+        name: 'Valdrakken',
+        shortName: 'VD',
+        icon: 'achievement/16530',
+        masters: [
+            Profession.Tailoring,
+        ],
+    },
+    null,
+    {
+        name: 'Azure Span',
+        shortName: 'AS',
+        icon: 'achievement/16336',
+        masters: [
+            Profession.Engineering,
+            Profession.Inscription,
+            Profession.Jewelcrafting,
+        ],
+    },
+    {
+        name: "Ohn'ahran Plains",
+        shortName: 'OP',
+        icon: 'achievement/15394',
+        masters: [
+            Profession.Enchanting,
+            Profession.Herbalism,
+            Profession.Leatherworking,
+        ],
+    },
+    {
+        name: 'Thaldraszus',
+        shortName: 'TD',
+        icon: 'achievement/16363',
+        masters: [
+            Profession.Mining,
+        ],
+    },
+    {
+        name: 'The Waking Shores',
+        shortName: 'WS',
+        icon: 'achievement/16334',
+        masters: [
+            Profession.Alchemy,
+            Profession.Blacksmithing,
+            Profession.Skinning,
+        ],
+    },
+    null,
+    {
+        name: 'Zaralek Cavern',
+        shortName: 'ZC',
+        icon: 'achievement/16401',
+    },
+]
