f3735f2e8d9bf0fe9f47cb3340142e2b3a349fa1
Freddie <freddie@wafflemonster.org>
2023-05-24 18:45:20 -0700

    Add 10.1.0 chores to settings

--- apps/frontend/components/settings/sections/SettingsTasks.svelte
+++ apps/frontend/components/settings/sections/SettingsTasks.svelte
@@ -1,11 +1,12 @@
 <script lang='ts'>
     import debounce from 'lodash/debounce'
 
-    import { multiTaskMap, taskList } from '@/data/tasks'
+    import { taskList } from '@/data/tasks'
     import { settingsStore } from '@/stores'
     import type { SettingsChoice } from '@/types'
 
     import CheckboxInput from '@/components/forms/CheckboxInput.svelte'
+    import Multi from './SettingsTasksMulti.svelte'
     import MagicLists from '../SettingsMagicLists.svelte'
  
     const taskChoices: SettingsChoice[] = taskList.map((t) => ({ key: t.key, name: t.name }))
@@ -21,28 +22,13 @@
             return state
         })
     }, 100)
-
-    // Dragonflight Chores
-    const dfChoreChoices: SettingsChoice[] = multiTaskMap.dfChores.map((t) => ({ key: t.taskKey, name: t.taskName }))
-
-    const dfChoreInactive = ($settingsStore.tasks.disabledChores?.dfChores || [])
-        .map((f) => dfChoreChoices.filter((c) => c.key === f)[0])
-        .filter(f => f !== undefined)
-    const dfChoreActive = dfChoreChoices.filter((c) => dfChoreInactive.indexOf(c) === -1)
-
-    const onDfChoreChange = debounce(() => {
-        settingsStore.update(state => {
-            (state.tasks.disabledChores ||= {})['dfChores'] = dfChoreInactive.map((c) => c.key)
-            return state
-        })
-    }, 100)
 </script>
 
-<div class='thing-container settings-container'>
+<div class="thing-container settings-container">
     <h2>Tasks</h2>
 
     <p>
-        <code>Holiday</code> tasks will only show that column when that holiday is active.
+        <code>[Holiday]</code> tasks will only show that column when that holiday is active.
         You'll also need to add <code>Tasks</code> to <code>Home columns</code> in
         <a href='#/settings/layout'>Settings->Layout</a>.
     </p>
@@ -77,12 +63,13 @@
         Show Treatises in Profession Weeklies.
     </CheckboxInput>
 
-    <h3>Dragonflight Chores</h3>
+    <div>
+        <h3>Dragonflight Chores</h3>
+        <Multi multiTaskKey="dfChores" />
+    </div>
 
-    <MagicLists
-        key='dragonflight-chore'
-        onFunc={onDfChoreChange}
-        active={dfChoreActive}
-        inactive={dfChoreInactive}
-    />
+    <div>
+        <h3>Dragonflight Chores - 10.1.0</h3>
+        <Multi multiTaskKey="dfChores10_1_0" />
+    </div>
 </div>
---
+++ apps/frontend/components/settings/sections/SettingsTasksMulti.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import debounce from 'lodash/debounce'
+
+    import { multiTaskMap } from '@/data/tasks'
+    import { settingsStore } from '@/stores'
+    import type { SettingsChoice } from '@/types'
+
+    import MagicLists from '../SettingsMagicLists.svelte'
+
+    export let multiTaskKey: string
+
+    const taskChoices: SettingsChoice[] = multiTaskMap[multiTaskKey].map((t) => ({ key: t.taskKey, name: t.taskName }))
+
+    const taskInactive = ($settingsStore.tasks.disabledChores?.[multiTaskKey] || [])
+        .map((f) => taskChoices.filter((c) => c.key === f)[0])
+        .filter(f => f !== undefined)
+    const taskActive = taskChoices.filter((c) => taskInactive.indexOf(c) === -1)
+
+    const onTaskChange = debounce(() => {
+        settingsStore.update(state => {
+            (state.tasks.disabledChores ||= {})[multiTaskKey] = taskInactive.map((c) => c.key)
+            console.log({taskChoices, taskInactive, taskActive, state})
+            return state
+        })
+    }, 100)
+
+    console.log({taskChoices, taskInactive, taskActive})
+</script>
+
+<MagicLists
+    key={multiTaskKey.toLowerCase()}
+    onFunc={onTaskChange}
+    active={taskActive}
+    inactive={taskInactive}
+/>
