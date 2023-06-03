4ed458a6521324793fe90036eaa15e763c6d3ad7
Freddie <freddie@wafflemonster.org>
2023-06-02 18:26:47 -0700

    Clean up Characters>Professions display, add crafted yes/no

---
+++ apps/frontend/components/characters/professions/CharacterProfessionsProfessionCraftLevels.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import type { CharacterProfession } from '@/types'
+    import type { StaticDataProfessionAbility } from '@/types/data/static'
+
+    export let ability: StaticDataProfessionAbility
+    export let charSubProfession: CharacterProfession
+
+    $: useLow = ability.trivialLow &&
+                ability.trivialLow < ability.trivialHigh &&
+                ability.trivialLow > charSubProfession.currentSkill
+    $: mid = Math.floor((ability.trivialLow + ability.trivialHigh) / 2)
+</script>
+
+<style lang="scss">
+    .flex-wrapper {
+        justify-content: flex-end;
+        padding-left: 0;
+        text-align: right;
+
+        span {
+            white-space: nowrap;
+            width: 2.2rem;
+        }
+    }
+    .trivial-low {
+        color: $colour-shrug;
+    }
+    .trivial-mid {
+        color: $colour-success;
+    }
+    .trivial-high {
+        color: #bbb;
+    }
+</style>
+
+<div class="flex-wrapper">
+    {#if useLow}
+        <span class="trivial-low">{ability.trivialLow}</span>
+
+        {#if mid > charSubProfession.currentSkill}
+            <span class="trivial-mid">{mid}</span>
+        {/if}
+    {/if}
+
+    {#if ability.trivialHigh > 1}
+        <span class="trivial-high">{ability.trivialHigh}</span>
+    {/if}
+</div>
