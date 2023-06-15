0fe9b7d3f8d9598e9c1273dd3f135cf374a868e1
Freddie <freddie@wafflemonster.org>
2023-06-14 18:20:41 -0700

    Add all tier transmog sets instead of just normal, Priest data is jank

--- apps/frontend/data/gear.ts
+++ apps/frontend/data/gear.ts
@@ -10,19 +10,71 @@ export type TierData = {
 
 export const currentTier: TierData = {
     sets: Object.fromEntries([
-        'transmog:2870', // Death Knight
-        'transmog:2869', // Demon Hunter
-        'transmog:2868', // Druid
-        'transmog:2867', // Evoker
-        'transmog:2866', // Hunter
-        'transmog:2865', // Mage
-        'transmog:2864', // Monk
-        'transmog:2859', // Paladin
-        'transmog:2863', // Priest
-        'transmog:2862', // Rogue
-        'transmog:2861', // Shaman
-        'transmog:2860', // Warlock
-        'transmog:2858', // Warrior
+        // Death Knight - Lingering Phantom's Encasement
+        'transmog:2870',
+        'transmog:2895',
+        'transmog:2896',
+        'transmog:2897',
+        // Demon Hunter - Kinslayer's Burdens
+        'transmog:2869',
+        'transmog:2901',
+        'transmog:2902',
+        'transmog:2903',
+        // Druid - Strands of the Autumn Blaze
+        'transmog:2868',
+        'transmog:2892',
+        'transmog:2893',
+        'transmog:2894',
+        // Evoker - Legacy of Obsidian Secrets
+        'transmog:2867',
+        'transmog:2904',
+        'transmog:2905',
+        'transmog:2906',
+        // Hunter - Ashen Predator's Scaleform
+        'transmog:2866',
+        'transmog:2889',
+        'transmog:2890',
+        'transmog:2891',
+        // Mage - Underlight Conjurer's Brilliance
+        'transmog:2865',
+        'transmog:2907',
+        'transmog:2908',
+        'transmog:2909',
+        // Monk - Fangs of the Vermillion Forge
+        'transmog:2864',
+        'transmog:2886',
+        'transmog:2887',
+        'transmog:2888',
+        // Paladin - Heartfire Sentinel's Authority
+        'transmog:2859',
+        'transmog:2871',
+        'transmog:2872',
+        'transmog:2873',
+        // Priest - The Furnace Seraph's Verdict
+        'transmog:2863',
+        'transmog:2883',
+        'transmog:2884',
+        'transmog:2885',
+        // Rogue - Lurking Specter's Shadeweave
+        'transmog:2862',
+        'transmog:2880',
+        'transmog:2881',
+        'transmog:2882',
+        // Shaman - Runes of the Cinderwolf
+        'transmog:2861',
+        'transmog:2877',
+        'transmog:2878',
+        'transmog:2879',
+        // Warlock - Sinister Savant's Cursethreads
+        'transmog:2860',
+        'transmog:2874',
+        'transmog:2875',
+        'transmog:2876',
+        // Warrior - Irons of the Onyx Crucible
+        'transmog:2858',
+        'transmog:2898',
+        'transmog:2899',
+        'transmog:2900',
     ].map((name) => [name, true])),
     slots: [
         InventoryType.Head,
