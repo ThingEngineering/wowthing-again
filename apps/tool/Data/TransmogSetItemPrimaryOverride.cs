d5cfdd95e7d584a8a7c89b26b9824c78ab59db98
Freddie <freddie@wafflemonster.org>
2023-07-19 17:32:37 -0700

    Add transmog set item primary override and use it for Aberrus priest LFR set

---
+++ apps/tool/Data/TransmogSetItemPrimaryOverride.cs
@@ -0,0 +1,11 @@
+﻿namespace Wowthing.Tool.Data;
+
+public static partial class Hardcoded
+{
+    public static Dictionary<int, int> TransmogSetItemPrimaryOverride = new()
+    {
+        // Dragonflight - Aberrus - Priest LFR
+        { 56997, 0 }, // Crucible Curator's Wingspan (wrong shoulders)
+        { 56998, 1 }, // Devotion of the Furnace Seraph (right shoulders)
+    };
+}
