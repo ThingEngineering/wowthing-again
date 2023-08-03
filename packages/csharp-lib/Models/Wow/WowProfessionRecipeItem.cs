754577f1fe1908e81062f7606ef9e9190bdba6fa
Freddie <freddie@wafflemonster.org>
2023-08-02 22:11:26 -0700

    Add WowProfessionRecipeItem and populate it

---
+++ packages/csharp-lib/Models/Wow/WowProfessionRecipeItem.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wowthing.Lib.Models.Wow;
+
+public class WowProfessionRecipeItem
+{
+    [Key]
+    public int SkillLineAbilityId { get; set; }
+    public int SkillLineId { get; set; }
+    public int ItemId { get; set; }
+}
