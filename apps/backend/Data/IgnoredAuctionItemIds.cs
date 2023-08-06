143536a2946d8ea7a95e282805b4222887f2ca6c
Freddie <freddie@wafflemonster.org>
2023-08-05 17:29:38 -0700

    Add IgnoredAuctionItemIds to skip auction items for cheapest purposes

---
+++ apps/backend/Data/IgnoredAuctionItemIds.cs
@@ -0,0 +1,10 @@
+﻿namespace Wowthing.Backend.Data;
+
+public static partial class Hardcoded
+{
+    public static readonly HashSet<int> IgnoredAuctionItemIds = new()
+    {
+        87213,  // Mist-Piercing Goggles - not BoE so not learnable
+        158078, // Cracked Overlord's Scepter - not BoE so not learnable
+    };
+}
