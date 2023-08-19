04cb852aa1c0100f687cc0e86dc22a263a5cbec4
Freddie <freddie@wafflemonster.org>
2023-08-18 19:33:43 -0700

    Handle ignoreRussia parameter in auction API endpoints

--- apps/web/Forms/ApiMissingTransmogForm.cs
+++ apps/web/Forms/ApiMissingTransmogForm.cs
@@ -5,5 +5,6 @@ namespace Wowthing.Web.Forms;
 public class ApiMissingTransmogForm
 {
     public bool AllRealms { get; set; }
+    public bool IncludeRussia { get; set; }
     public WowRegion Region { get; set; }
 }
