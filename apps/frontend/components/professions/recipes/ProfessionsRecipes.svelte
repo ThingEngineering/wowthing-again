d7708ff239ef03c1b5b7509fe509aa804fa9386b
Freddie <freddie@wafflemonster.org>
2023-08-26 13:07:37 -0700

    WIP profession recipes

---
+++ apps/frontend/components/professions/recipes/ProfessionsRecipes.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import type { MultiSlugParams } from '@/types'
+
+    import Sidebar from './ProfessionsRecipesSidebar.svelte'
+
+    export let params: MultiSlugParams
+
+    // blah blah load view based on params
+    // - show every recipe for profession/expansion combo as a row,
+    //   characters as columns?
+</script>
+
+<style lang="scss">
+</style>
+
+<Sidebar />
