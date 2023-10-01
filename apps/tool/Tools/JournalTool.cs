7d3d554ba3777192c3a6abf1036c42ca42ade6d2
Freddie <freddie@wafflemonster.org>
2023-09-30 18:48:40 -0700

    Comment out the difficulty skip spam

--- apps/tool/Tools/JournalTool.cs
+++ apps/tool/Tools/JournalTool.cs
@@ -514,13 +514,13 @@ public class JournalTool
                                 );
                                 if (!forThis)
                                 {
-                                    ToolContext.Logger.Warning("Difficulty skip? {instanceId} {instanceName} / {encounterId} {encounterName} / {mask} {diffs}",
-                                        instance.ID,
-                                        instance.Name,
-                                        encounter.ID,
-                                        encounter.Name,
-                                        encounterItem.DifficultyMask,
-                                        string.Join(',', difficulties));
+                                    // ToolContext.Logger.Warning("Difficulty skip? {instanceId} {instanceName} / {encounterId} {encounterName} / {mask} {diffs}",
+                                    //     instance.ID,
+                                    //     instance.Name,
+                                    //     encounter.ID,
+                                    //     encounter.Name,
+                                    //     encounterItem.DifficultyMask,
+                                    //     string.Join(',', difficulties));
                                     continue;
                                 }
                             }
