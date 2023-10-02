18a28d04332d2378ee578cdde4bfedebacbcb27d
Freddie <freddie@wafflemonster.org>
2023-10-01 20:07:17 -0700

    Use the generated manifest.json for loading CSS/JS in production

---
+++ apps/web/Models/FrontendManifestJson.cs
@@ -0,0 +1,10 @@
+﻿namespace Wowthing.Web.Models;
+
+public class FrontendManifestJson
+{
+    public bool IsEntry { get; set; }
+    public string File { get; set; }
+    public string Src { get; set; }
+    public string[] Css { get; set; }
+    public string[] Imports { get; set; }
+}
