c571dce5377343f4d5afe5124fbf45f71d740b05
Freddie <freddie@wafflemonster.org>
2023-10-14 11:10:44 -0700

    Move home app to user-home folder

--- apps/frontend/user-home/entrypoint.ts
+++ apps/frontend/user-home/entrypoint.ts
@@ -2,7 +2,7 @@ import 'vite/modulepreload-polyfill'
 
 import '../scss/global.scss'
 
-import App from './home/AppHome.svelte'
+import App from './Main.svelte'
 
 const appTarget = document.querySelector('#app')
 const app = appTarget ? new App({ target: appTarget }) : null
