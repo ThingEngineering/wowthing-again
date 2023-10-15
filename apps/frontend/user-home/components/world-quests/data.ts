9e79561bd74dc8eb2ab90ecfee0fec1313737dcd
Freddie <freddie@wafflemonster.org>
2023-10-15 11:45:07 -0700

    Remove hardcoded Field of Ferocity world quest requirements

--- apps/frontend/user-home/components/world-quests/data.ts
+++ apps/frontend/user-home/components/world-quests/data.ts
@@ -40,12 +40,6 @@ export const zoneData: [number, string, string, string][] = [
 ]
 
 export const worldQuestPrereqs: Record<number, number> = {
-    69990: 66464, // The Field of Ferocity Redux: Lord of Decay!
-    69991: 66464, // The Field of Ferocity Redux: Foe from the Volcano!
-    69993: 66464, // The Field of Ferocity Redux: Lost in the Dream!
-    69994: 66464, // The Field of Ferocity Redux: Elemental Revenge Round!
-    69995: 66464, // The Field of Ferocity Redux: Master of the Hunt!
-    69987: 66464, // The Field of Ferocity Redux: Terror of the Swamp!
     74501: 75888, // Suffusion Camp: Cinderwind
     75280: 75888, // Suffusion Camp: Frostburn
 }
