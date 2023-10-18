0a4f13b15eee5bf9c2e9104250156a05dd6c376f
Freddie <freddie@wafflemonster.org>
2023-10-17 20:28:06 -0700

    Add basic admin controller and app view

---
+++ apps/web/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Wowthing.Web.Controllers;
+
+[Authorize(Roles = "Admin")]
+public class AdminController : Controller
+{
+    [HttpGet("admin")]
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
+}
