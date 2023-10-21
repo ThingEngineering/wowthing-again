e1f30649820e6da31f592853f807c38b7bd37f3e
Freddie <freddie@wafflemonster.org>
2023-10-21 11:31:37 -0700

    Reorder Professions > Knoweldge

--- apps/frontend/data/professions/knowledge.ts
+++ apps/frontend/data/professions/knowledge.ts
@@ -10,6 +10,60 @@ type DragonflightKnowledge = {
 
 export const dragonflightKnowledge: DragonflightKnowledge[] = [
     {
+        name: 'Azure Span',
+        shortName: 'AS',
+        icon: 'achievement/16336',
+        masters: [
+            Profession.Engineering,
+            Profession.Inscription,
+            Profession.Jewelcrafting,
+        ],
+    },
+    {
+        name: "Ohn'ahran Plains",
+        shortName: 'OP',
+        icon: 'achievement/15394',
+        masters: [
+            Profession.Enchanting,
+            Profession.Herbalism,
+            Profession.Leatherworking,
+        ],
+    },
+    {
+        name: 'Thaldraszus',
+        shortName: 'TD',
+        icon: 'achievement/16363',
+        masters: [
+            Profession.Mining,
+        ],
+    },
+    {
+        name: 'The Waking Shores',
+        shortName: 'WS',
+        icon: 'achievement/16334',
+        masters: [
+            Profession.Alchemy,
+            Profession.Blacksmithing,
+            Profession.Skinning,
+        ],
+    },
+    {
+        name: 'Valdrakken',
+        shortName: 'VD',
+        icon: 'achievement/16530',
+        masters: [
+            Profession.Tailoring,
+        ],
+    },
+    null,
+    {
+        name: 'Zaralek Cavern',
+        shortName: 'ZC',
+        icon: 'achievement/16401',
+        masters: [],
+    },
+    null,
+    {
         name: "Artisan's Consortium Books",
         shortName: 'AC',
         icon: 'spell/339979', // Booksmart
@@ -48,60 +102,6 @@ export const dragonflightKnowledge: DragonflightKnowledge[] = [
         name: 'Zaralek Cavern Books',
         shortName: 'ZC',
         icon: 'spell/339979', // Booksmart
-        masters: [],
-    },
-    null,
-    {
-        name: 'Valdrakken',
-        shortName: 'VD',
-        icon: 'achievement/16530',
-        masters: [
-            Profession.Tailoring,
-        ],
-    },
-    {
-        name: 'Azure Span',
-        shortName: 'AS',
-        icon: 'achievement/16336',
-        masters: [
-            Profession.Engineering,
-            Profession.Inscription,
-            Profession.Jewelcrafting,
-        ],
-    },
-    {
-        name: "Ohn'ahran Plains",
-        shortName: 'OP',
-        icon: 'achievement/15394',
-        masters: [
-            Profession.Enchanting,
-            Profession.Herbalism,
-            Profession.Leatherworking,
-        ],
-    },
-    {
-        name: 'Thaldraszus',
-        shortName: 'TD',
-        icon: 'achievement/16363',
-        masters: [
-            Profession.Mining,
-        ],
-    },
-    {
-        name: 'The Waking Shores',
-        shortName: 'WS',
-        icon: 'achievement/16334',
-        masters: [
-            Profession.Alchemy,
-            Profession.Blacksmithing,
-            Profession.Skinning,
-        ],
-    },
-    null,
-    {
-        name: 'Zaralek Cavern',
-        shortName: 'ZC',
-        icon: 'achievement/16401',
         masters: [],
     },
 ]
