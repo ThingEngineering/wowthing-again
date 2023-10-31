9319d883cece2c5da77f91235434e8dee4e0ca0b
Freddie <freddie@wafflemonster.org>
2023-10-30 20:13:13 -0700

    Allow transmog set groups to be collapsed

--- apps/frontend/shared/icons/ui.ts
+++ apps/frontend/shared/icons/ui.ts
@@ -3,7 +3,9 @@ import * as iconLibrary from './library'
 
 export const uiIcons = {
     lock: iconLibrary.mdiLockOutline,
+    minus: iconLibrary.mdiMinus,
     no: iconLibrary.mdiClose,
+    plus: iconLibrary.mdiPlus,
     starEmpty: iconLibrary.mdiStarOutline,
     starHalf: iconLibrary.mdiStarHalfFull,
     starFull: iconLibrary.mdiStarFace,
