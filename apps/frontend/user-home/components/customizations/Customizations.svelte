4f6a7b9e8e5ec833ee1a0c93599b3fa5d628e9fc
Freddie <freddie@wafflemonster.org>
2023-11-19 22:04:21 -0800

    Use getSavedRoute

--- apps/frontend/user-home/components/customizations/Customizations.svelte
+++ apps/frontend/user-home/components/customizations/Customizations.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+    import { afterUpdate } from 'svelte'
+
+    import getSavedRoute from '@/utils/get-saved-route'
     import type { MultiSlugParams } from '@/types'
 
     import Sidebar from './Sidebar.svelte'
     import View from './View.svelte'
 
     export let params: MultiSlugParams
+
+    afterUpdate(() => getSavedRoute('collections/customizations', params.slug1, params.slug2))
 </script>
 
 <div class="view">
