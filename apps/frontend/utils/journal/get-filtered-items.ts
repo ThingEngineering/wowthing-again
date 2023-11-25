6801ba743445913d00bf8ea51a2bd3f537e8b0e2
Freddie <freddie@wafflemonster.org>
2023-11-25 00:22:20 -0800

    Improve performance of getFilteredItems

--- apps/frontend/utils/journal/get-filtered-items.ts
+++ apps/frontend/utils/journal/get-filtered-items.ts
@@ -35,8 +35,7 @@ export default function getFilteredItems(
 ): JournalDataEncounterItem[] {
     const items: JournalDataEncounterItem[] = []
 
-    for (const origItem of group.items) {
-        const item = origItem.clone()
+    for (const item of group.items) {
         let keep = true
 
         // Class mask is a quick check
@@ -48,116 +47,112 @@ export default function getFilteredItems(
         if (keep && group.name === 'Cloth') {
             keep = journalState.showCloth
         }
-        if (keep && group.name === 'Leather') {
+        else if (keep && group.name === 'Leather') {
             keep = journalState.showLeather
         }
-        if (keep && group.name === 'Mail') {
+        else if (keep && group.name === 'Mail') {
             keep = journalState.showMail
         }
-        if (keep && group.name === 'Plate') {
+        else if (keep && group.name === 'Plate') {
             keep = journalState.showPlate
         }
-
-        if (keep && group.name.startsWith('Cloak')) {
+        else if (keep && group.name.startsWith('Cloak')) {
             keep = journalState.showCloaks
         }
-
-        if (keep && weaponGroups[group.name] === true) {
+        else if (keep && weaponGroups[group.name] === true) {
             keep = journalState.showWeapons
         }
 
+        if (!keep) {
+            continue
+        }
+
         // Appearances
-        if (keep) {
-            const appearances: JournalDataEncounterItemAppearance[] = []
+        const appearances: JournalDataEncounterItemAppearance[] = []
+        for (const appearance of item.appearances) {
+            const difficulties: number[] = []
 
-            for (const appearance of item.appearances) {
-                const difficulties: number[] = []
+            for (const difficulty of appearance.difficulties) {
+                // Early skip for 10 player
+                if ((difficulty === 3 || difficulty === 5) && !journalState.showRaid10) {
+                    continue
+                }
+                // Early skip for 25 player
+                if ((difficulty === 4 || difficulty === 6) && !journalState.showRaid25) {
+                    continue
+                }
 
-                for (const difficulty of appearance.difficulties) {
-                    // Early skip for 10 player
-                    if ((difficulty === 3 || difficulty === 5) && !journalState.showRaid10) {
-                        continue
-                    }
-                    // Early skip for 25 player
-                    if ((difficulty === 4 || difficulty === 6) && !journalState.showRaid25) {
-                        continue
-                    }
-
-                    // LFR
-                    if (difficulty === 7 || difficulty === 17) {
-                        if (journalState.showRaidLfr) {
-                            difficulties.push(difficulty)
-                        }
-                    }
-                    // Normal
-                    else if (difficulty === 1) {
-                        if (journalState.showDungeonNormal) {
-                            difficulties.push(difficulty)
-                        }
-                    }
-                    else if (difficulty === 3 || difficulty === 4 || difficulty === 9 || difficulty === 14) {
-                        if (journalState.showRaidNormal) {
-                            difficulties.push(difficulty)
-                        }
-                    }
-                    // Heroic
-                    else if (difficulty === 2) {
-                        if (journalState.showDungeonHeroic) {
-                            difficulties.push(difficulty)
-                        }
-                    }
-                    else if (difficulty === 5 || difficulty === 6 || difficulty === 15) {
-                        if (journalState.showRaidHeroic) {
-                            difficulties.push(difficulty)
-                        }
-                    }
-                    // Mythic
-                    else if (difficulty === 8 || difficulty === 23) {
-                        if (journalState.showDungeonMythic) {
-                            difficulties.push(difficulty)
-                        }
-                    }
-                    else if (difficulty === 16) {
-                        if (instanceExpansion === Constants.expansion) {
-                            if (journalState.showRaidMythic) {
-                                difficulties.push(difficulty)
-                            }
-                        }
-                        else if (journalState.showRaidMythicOld) {
-                            difficulties.push(difficulty)
-                        }
-                    }
-                    // Timewalking
-                    else if (difficulty === 24) {
-                        if (journalState.showDungeonTimewalking) {
-                            difficulties.push(difficulty)
-                        }
-                    }
-                    else if (difficulty === 33) {
-                        if (journalState.showRaidTimewalking) {
-                            difficulties.push(difficulty)
-                        }
-                    }
-                    // Leftovers
-                    else {
+                // LFR
+                if (difficulty === 7 || difficulty === 17) {
+                    if (journalState.showRaidLfr) {
                         difficulties.push(difficulty)
                     }
                 }
-
-                appearance.difficulties = difficulties
-                if (appearance.difficulties.length > 0) {
-                    appearances.push(appearance)
+                // Normal
+                else if (difficulty === 1) {
+                    if (journalState.showDungeonNormal) {
+                        difficulties.push(difficulty)
+                    }
+                }
+                else if (difficulty === 3 || difficulty === 4 || difficulty === 9 || difficulty === 14) {
+                    if (journalState.showRaidNormal) {
+                        difficulties.push(difficulty)
+                    }
+                }
+                // Heroic
+                else if (difficulty === 2) {
+                    if (journalState.showDungeonHeroic) {
+                        difficulties.push(difficulty)
+                    }
+                }
+                else if (difficulty === 5 || difficulty === 6 || difficulty === 15) {
+                    if (journalState.showRaidHeroic) {
+                        difficulties.push(difficulty)
+                    }
+                }
+                // Mythic
+                else if (difficulty === 8 || difficulty === 23) {
+                    if (journalState.showDungeonMythic) {
+                        difficulties.push(difficulty)
+                    }
+                }
+                else if (difficulty === 16) {
+                    if (instanceExpansion === Constants.expansion) {
+                        if (journalState.showRaidMythic) {
+                            difficulties.push(difficulty)
+                        }
+                    }
+                    else if (journalState.showRaidMythicOld) {
+                        difficulties.push(difficulty)
+                    }
+                }
+                // Timewalking
+                else if (difficulty === 24) {
+                    if (journalState.showDungeonTimewalking) {
+                        difficulties.push(difficulty)
+                    }
+                }
+                else if (difficulty === 33) {
+                    if (journalState.showRaidTimewalking) {
+                        difficulties.push(difficulty)
+                    }
+                }
+                // Leftovers
+                else {
+                    difficulties.push(difficulty)
                 }
             }
 
-            item.appearances = appearances
-            if (appearances.length === 0) {
-                keep = false
+            appearance.difficulties = difficulties
+            if (appearance.difficulties.length > 0) {
+                appearances.push(appearance)
             }
         }
 
-        if (keep) {
-            items.push(item)
+        if (appearances.length > 0) {
+            const clonedItem = item.clone()
+            clonedItem.appearances = appearances
+            items.push(clonedItem)
         }
     }
 
