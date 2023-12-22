4f33d2d24977cc25cec894437f0c33ffb3317b10
Freddie <freddie@wafflemonster.org>
2023-12-22 10:39:19 -0800

    Add view reordering

--- apps/frontend/shared/icons/ui.ts
+++ apps/frontend/shared/icons/ui.ts
@@ -2,6 +2,8 @@ import * as iconLibrary from './library'
 
 
 export const uiIcons = {
+    chevronDown: iconLibrary.mdiChevronDown,
+    chevronUp: iconLibrary.mdiChevronUp,
     lock: iconLibrary.mdiLockOutline,
     minus: iconLibrary.mdiMinus,
     no: iconLibrary.mdiClose,
--- apps/frontend/user-home/components/settings/sections/views/Views.svelte
+++ apps/frontend/user-home/components/settings/sections/views/Views.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-    import { settingsState } from '@/stores/local-storage'
+    import { uiIcons } from '@/shared/icons'
     import { settingsStore } from '@/shared/stores/settings'
+    import { settingsState } from '@/stores/local-storage'
     import type { SettingsView } from '@/shared/stores/settings/types'
 
+    import IconifyIcon from '@/shared/components/images/IconifyIcon.svelte'
     import UnderConstruction from '@/shared/components/under-construction/UnderConstruction.svelte'
 
     const newView = () => {
@@ -26,10 +28,41 @@
         $settingsStore.views = newCustomViews
         $settingsState.selectedView = view.id
     }
+
+    const moveUp = (index: number) => {
+        const newViews = $settingsStore.views.slice()
+        const temp = newViews[index - 1]
+        newViews[index - 1] = newViews[index]
+        newViews[index] = temp
+        $settingsStore.views = newViews
+    }
+
+    const moveDown = (index: number) => {
+        const newViews = $settingsStore.views.slice()
+        const temp = newViews[index + 1]
+        newViews[index + 1] = newViews[index]
+        newViews[index] = temp
+        $settingsStore.views = newViews
+    }
 </script>
 
 <style lang="scss">
+    table {
+        --padding: 2;
 
+        border-top: 1px solid $border-color;
+    }
+    .name {
+        @include cell-width(12rem);
+    }
+    .icon {
+        :global(svg) {
+            cursor: pointer;
+        }
+    }
+    .delete {
+        color: $color-fail;
+    }
 </style>
 
 <div class="settings-block">
@@ -39,10 +72,38 @@
 
     <table class="table table-striped">
         <tbody>
-            {#each $settingsStore.views as view}
+            {#each $settingsStore.views as view, viewIndex}
                 <tr>
-                    <td class="name">
+                    <td class="name text-overflow">
                         {view.name}
+                    </td>
+                    <td class="icon">
+                        {#if viewIndex > 1}
+                            <IconifyIcon
+                                icon={uiIcons.chevronUp}
+                                scale={'1.2'}
+                                tooltip={'Move up'}
+                                on:click={() => moveUp(viewIndex)}
+                            />
+                        {/if}
+                    </td>
+                    <td class="icon">
+                        {#if viewIndex > 0 && viewIndex < ($settingsStore.views.length - 1)}
+                            <IconifyIcon
+                                icon={uiIcons.chevronDown}
+                                scale={'1.2'}
+                                tooltip={'Move down'}
+                                on:click={() => moveDown(viewIndex)}
+                            />
+                        {/if}
+                    </td>
+                    <td class="icon delete">
+                        {#if viewIndex > 0}
+                            <IconifyIcon
+                                icon={uiIcons.no}
+                                tooltip={'Delete'}
+                            />
+                        {/if}
                     </td>
                 </tr>
             {/each}
