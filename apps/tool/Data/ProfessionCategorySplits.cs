078783333d9f72eda67f2fe3448b3fd48b9244bb
Freddie <freddie@wafflemonster.org>
2024-01-09 17:49:40 -0800

    Manually split some profession categories into more manageable chunks

---
+++ apps/tool/Data/ProfessionCategorySplits.cs
@@ -0,0 +1,42 @@
+﻿namespace Wowthing.Tool.Data;
+
+public static partial class Hardcoded
+{
+    public static readonly Dictionary<int, ProfessionCategorySplit[]> ProfessionCategorySplits = new()
+    {
+        {
+            1567, // Blacksmithing > Dragonflight > Armor
+            [
+                new ProfessionCategorySplit("Explorer's"),
+                new ProfessionCategorySplit("Primal Molten"),
+                new ProfessionCategorySplit("Crimson Combatant's"),
+                new ProfessionCategorySplit("Obsidian Combatant's"),
+                new ProfessionCategorySplit("Verdant Combatant's"),
+            ]
+        },
+        {
+            1675, // Blacksmithing > Dragonflight > Weapons
+            [
+                new ProfessionCategorySplit("Draconium"),
+                new ProfessionCategorySplit("Obsidian Seared"),
+                new ProfessionCategorySplit("Primal Molten"),
+            ]
+        },
+        {
+            1662, // Tailoring > Dragonflight > Garments
+            [
+                new ProfessionCategorySplit("Surveyor's"),
+                new ProfessionCategorySplit("Vibrant"),
+                new ProfessionCategorySplit("Crimson Combatant's"),
+                new ProfessionCategorySplit("Obsidian Combatant's"),
+                new ProfessionCategorySplit("Verdant Combatant's"),
+            ]
+        },
+    };
+}
+
+public class ProfessionCategorySplit(string name, string? prefix = null)
+{
+    public string Name { get; set; } = name;
+    public string Prefix { get; set; } = prefix ?? name;
+}
