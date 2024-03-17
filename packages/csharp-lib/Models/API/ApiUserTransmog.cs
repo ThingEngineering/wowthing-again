8c4f1d826d1667609c331120271d62ae7b601199
Freddie <freddie@wafflemonster.org>
2024-03-17 11:38:54 -0700

    Delete unused user transmog things

--- packages/csharp-lib/Models/API/ApiUserTransmog.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Wowthing.Lib.Models.API;
-
-public class ApiUserTransmog
-{
-    public List<int> AppearanceIds { get; set; }
-    public List<string> AppearanceSources { get; set; }
-    public List<short> IllusionIds { get; set; }
-}
