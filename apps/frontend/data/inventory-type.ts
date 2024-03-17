ec01800651deb841612624759d87c9a0a9a61c46
Freddie <freddie@wafflemonster.org>
2024-03-17 11:30:51 -0700

    Add tabard to inventory type order (for transmog sets display)

--- apps/frontend/data/inventory-type.ts
+++ apps/frontend/data/inventory-type.ts
@@ -9,6 +9,7 @@ export const typeOrder: InventoryType[] = [
     InventoryType.Back,
     InventoryType.Chest,
     InventoryType.Chest2,
+    InventoryType.Tabard,
     InventoryType.Wrist,
     InventoryType.Hands,
     InventoryType.Waist,
