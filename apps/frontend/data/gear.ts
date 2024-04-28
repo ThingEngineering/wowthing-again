49c7cf35a4db92ed86765d96612a584b85b35209
Freddie <freddie@wafflemonster.org>
2024-04-28 13:21:45 -0700

    Update current tier sets for S4

--- apps/frontend/data/gear.ts
+++ apps/frontend/data/gear.ts
@@ -1,20 +1,20 @@
 export const currentTier: number[] = [
-    1557, // Death Knight - Risen Nightmare's Gravemantle
-    1558, // Demon Hunter - Screaming Torchfiend's Brutality
-    1559, // Druid - Benevolent Embersage's Guidance
-    1560, // Evoker - Werynkeeper's Timeless Vigil
-    1561, // Hunter - Blazing Dreamstalker's Trophies
-    1562, // Mage - Wayward Chronomancer's Clockwork
-    1563, // Monk - Mystic Heron's Discipline
-    1564, // Paladin - Zealous Pyreknight's Ardor
-    1565, // Priest - Blessings of Lunar Communion
-    1566, // Rogue - Lucid Shadewalker's Silence
-    1567, // Shaman - Vision of the Greatwolf Outcast
-    1568, // Warlock - Devout Ashdevil's Pactweave
-    1569, // Warrior - Molten Vanguard's Mortarplate
-]
+    1603, // Death Knight - Risen Nightmare's Gravemantle
+    1604, // Demon Hunter - Screaming Torchfiend's Brutality
+    1597, // Druid - Strands of the Autumn Blaze
+    1594, // Evoker - Scales of the Awakened
+    1595, // Hunter - Stormwing Harrier's Camouflage
+    1605, // Mage - Wayward Chronomancer's Clockwork
+    1596, // Monk - Wrappings of the Waking Fist
+    1598, // Paladin - Heartfire Sentinel's Authority
+    1599, // Priest - The Furnace Seraph's Verdict
+    1600, // Rogue - Lurking Specter's Shadeweave
+    1606, // Shaman - Vision of the Greatwolf Outcast
+    1601, // Warlock - Sinister Savant's Cursethreads
+    1602, // Warrior - Irons of the Onyx Crucible
+];
 
-export const previousTier: number[] = [
+const dfTier2: number[] = [
     1540, // Death Knight - Lingering Phantom's Encasement
     1541, // Demon Hunter - Kinslayer's Burdens
     1542, // Druid - Strands of the Autumn Blaze
@@ -28,4 +28,22 @@ export const previousTier: number[] = [
     1550, // Shaman - Runes of the Cinderwolf
     1551, // Warlock - Sinister Savant's Cursethreads
     1552, // Warrior - Irons of the Onyx Crucible
-]
+];
+
+const dfTier3: number[] = [
+    1557, // Death Knight - Risen Nightmare's Gravemantle
+    1558, // Demon Hunter - Screaming Torchfiend's Brutality
+    1559, // Druid - Benevolent Embersage's Guidance
+    1560, // Evoker - Werynkeeper's Timeless Vigil
+    1561, // Hunter - Blazing Dreamstalker's Trophies
+    1562, // Mage - Wayward Chronomancer's Clockwork
+    1563, // Monk - Mystic Heron's Discipline
+    1564, // Paladin - Zealous Pyreknight's Ardor
+    1565, // Priest - Blessings of Lunar Communion
+    1566, // Rogue - Lucid Shadewalker's Silence
+    1567, // Shaman - Vision of the Greatwolf Outcast
+    1568, // Warlock - Devout Ashdevil's Pactweave
+    1569, // Warrior - Molten Vanguard's Mortarplate
+];
+
+export const previousTier = dfTier3;
