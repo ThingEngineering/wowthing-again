dbaa96a715a54f232e877666c8a9bbd17636d613
Freddie <freddie@wafflemonster.org>
2024-05-04 19:55:27 -0700

    Add "Item Level - Best" home column

---
+++ apps/frontend/components/character-table/row/BestItemLevel.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import getItemLevelQuality from '@/utils/get-item-level-quality';
+    import type { Character } from '@/types'
+
+    export let character: Character
+</script>
+
+<style lang="scss">
+    td {
+        @include cell-width($width-item-level);
+
+        text-align: right;
+    }
+</style>
+
+<td class="border-left quality{getItemLevelQuality(parseFloat(character.bestItemLevel))}">
+    {character.bestItemLevel}
+</td>
