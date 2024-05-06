508504b3f5f2a0c6f8fa9d5143abed8b95a7de72
Freddie <freddie@wafflemonster.org>
2024-05-05 17:56:08 -0700

    Aaaaaaa

--- apps/frontend/user-home/user-home.ts
+++ apps/frontend/user-home/user-home.ts
@@ -4,9 +4,6 @@ import '../scss/global.scss';
 
 import App from './Main.svelte';
 
-import { settingsStore } from '@/shared/stores/settings';
-console.log(settingsStore.wowheadBaseUrl);
-
 const appTarget = document.querySelector('#app');
 const app = appTarget ? new App({ target: appTarget }) : null;
 
