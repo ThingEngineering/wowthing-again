57d56389134660c764b5a81c0fb1c88c03d2f6c3
Freddie <freddie@wafflemonster.org>
2024-05-13 20:03:46 -0700

    Add a missing enchant substitution

--- apps/frontend/utils/get-enchantment-text.ts
+++ apps/frontend/utils/get-enchantment-text.ts
@@ -1,47 +1,50 @@
 export function getEnchantmentText(id: number, text: string): string {
     if (!text) {
-        return `Enchant #${id}`
+        return `Enchant #${id}`;
     }
 
     // Bracer T3
     if ([6574, 6580, 6586].indexOf(id) >= 0) {
-        text = text.replace('$k1', '200')
+        text = text.replace('$k1', '200');
     }
     // Legs: agi/str + sta T2
     else if ([6489].indexOf(id) >= 0) {
-        text = text.replace('$k1', '111').replace('$k2', '151')
+        text = text.replace('$k1', '111').replace('$k2', '151');
     }
     // Legs: agi/str + sta T3
     else if ([6490].indexOf(id) >= 0) {
-        text = text.replace('$k1', '131').replace('$k2', '171')
+        text = text.replace('$k1', '131').replace('$k2', '171');
+    }
+    // Legs: agi/str + armor T2
+    else if ([6495].indexOf(id) >= 0) {
+        text = text.replace('$k1', '120').replace('$k2', '151');
     }
     // Legs: int + sta T2
     else if ([6540].indexOf(id) >= 0) {
-        text = text.replace('$k1', '151').replace('$k2', '111')
+        text = text.replace('$k1', '151').replace('$k2', '111');
     }
     // Legs: int + sta T3
     else if ([6541].indexOf(id) >= 0) {
-        text = text.replace('$k1', '171').replace('$k2', '131')
+        text = text.replace('$k1', '171').replace('$k2', '131');
     }
     // Legs: int + mana T2
     else if ([6543].indexOf(id) >= 0) {
-        text = text.replace('$k1', '151').replace('$387303s1', '4')
+        text = text.replace('$k1', '151').replace('$387303s1', '4');
     }
     // Legs: int + mana T3
     else if ([6544].indexOf(id) >= 0) {
-        text = text.replace('$k1', '177').replace('$387306s1', '5')
+        text = text.replace('$k1', '177').replace('$387306s1', '5');
     }
     // Ring T2
     else if ([6549, 6555, 6561, 6567].indexOf(id) >= 0) {
-        text = text.replace('$k1', '73')
+        text = text.replace('$k1', '73');
     }
     // Ring T3
     else if ([6550, 6556, 6562, 6568].indexOf(id) >= 0) {
-        text = text.replace('$k1', '82')
-    }
-    else if (id === 7052) {
-        return text.replace(/^\|cnITEM_EPIC_COLOR:(.+)\|r$/, '$1')
+        text = text.replace('$k1', '82');
+    } else if (id === 7052) {
+        return text.replace(/^\|cnITEM_EPIC_COLOR:(.+)\|r$/, '$1');
     }
 
-    return text
+    return text;
 }
