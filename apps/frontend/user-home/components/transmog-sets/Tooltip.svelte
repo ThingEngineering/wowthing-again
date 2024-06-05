e984c447d89e326ffa70be75968cbadd074dbab7
Freddie <freddie@wafflemonster.org>
2024-06-04 18:03:15 -0700

    Add the ability to hold Shift to see missing items on Sets tooltips

--- apps/frontend/user-home/components/transmog-sets/Tooltip.svelte
+++ apps/frontend/user-home/components/transmog-sets/Tooltip.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { typeOrder } from '@/data/inventory-type'
     import { staticStore } from '@/shared/stores/static'
+    import getPercentClass from '@/utils/get-percent-class';
     import type { TransmogSlotData } from '@/stores/lazy/transmog'
     import type { ManualDataTransmogGroupData } from '@/types/data/manual'
 
@@ -14,11 +15,20 @@
     export let subType: string
 
     let setName: string
+    let shiftPressed: boolean
+
     $: setName = set.transmogSetId
         ? $staticStore.transmogSets[set.transmogSetId].name
         : set.name
     
     $: weapons = Object.keys(slotHave).filter((key) => parseInt(key) >= 100)
+
+    function keyDown(event: KeyboardEvent) {
+        shiftPressed = event.shiftKey
+    }
+    function keyUp(event: KeyboardEvent) {
+        shiftPressed = event.shiftKey
+    }
 </script>
 
 <style lang="scss">
@@ -30,6 +40,10 @@
     }
     .type {
         text-align: left;
+    }
+    .slot-count {
+        font-size: 0.9rem;
+        word-spacing: -0.2ch;
     }
     .items {
         text-align: left;
@@ -43,6 +57,8 @@
         }
     }
 </style>
+
+<svelte:window on:keydown={keyDown} on:keyup={keyUp}/>
 
 <div class="wowthing-tooltip">
     <h4>{setName}</h4>
@@ -58,24 +74,42 @@
             {:else}
                 {#each typeOrder as type}
                     {#if slotHave[type] !== undefined}
+                        {@const [slotCollected, slotItems] = slotHave[type]}
+                        {@const have = slotItems.filter(([data,]) => data).length}
                         <tr>
                             <td class="have">
-                                <YesNoIcon state={slotHave[type][0]} useStatusColors={true} />
+                                <YesNoIcon state={slotCollected} useStatusColors={true} />
                             </td>
                             <td class="type">
                                 {$staticStore.inventoryTypes[type]}
-                                {#if slotHave[type][1]?.length > 2}
-                                    <div>+{slotHave[type][1].length - 2}</div>
+                                {#if slotItems?.length > 2}
+                                    <div class="slot-count {getPercentClass(have / slotItems.length * 100)}">
+                                        {have} / {slotItems.length}
+                                    </div>
                                 {/if}
                             </td>
-                            {#if slotHave[type][1]?.length > 0}
+                            {#if shiftPressed}
                                 <td class="items">
-                                    <TooltipItems items={slotHave[type][1]} />
+                                    <TooltipItems
+                                        dedupe={false}
+                                        items={slotItems.filter(([itemCollected,]) => !itemCollected)}
+                                    />
                                 </td>
+                            {:else}
+                                {#if slotHave[type][1]?.length > 0}
+                                    <td class="items">
+                                        <TooltipItems items={slotItems.slice(0, 2)} />
+                                    </td>
+                                {/if}
                             {/if}
                         </tr>
                     {/if}
                 {/each}
+            {/if}
+            {#if !shiftPressed}
+                <tr>
+                    <td colspan="100">Hold Shift to see missing items!</td>
+                </tr>
             {/if}
         </tbody>
     </table>
