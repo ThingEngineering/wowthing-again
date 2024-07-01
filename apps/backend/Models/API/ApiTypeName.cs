71db1354f7fd7c24a0c8629bd0daf0c289e5ca2e
Freddie <freddie@wafflemonster.org>
2024-07-01 16:53:52 -0700

    Fix ApiTypeName parsing things like MAIN_HAND

--- apps/backend/Models/API/ApiTypeName.cs
+++ apps/backend/Models/API/ApiTypeName.cs
@@ -8,6 +8,6 @@ public class ApiTypeName
     public TEnum EnumParse<TEnum>()
         where TEnum : struct, Enum
     {
-        return Enum.Parse<TEnum>(Type, true);
+        return Enum.Parse<TEnum>(Type.Replace("_", ""), true);
     }
 }
