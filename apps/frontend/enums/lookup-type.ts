61067b50c8ca24bbebd813a218119e0d7504713e
Freddie <freddie@wafflemonster.org>
2024-07-14 20:43:18 -0700

    Add some more lookup types

--- apps/frontend/enums/lookup-type.ts
+++ apps/frontend/enums/lookup-type.ts
@@ -1,10 +1,13 @@
 export enum LookupType {
     None,
+    Achievement,
+    Currency,
     Illusion,
     Mount,
     Pet,
     ProfessionAbility,
     Quest,
+    Reputation,
     Toy,
     Transmog,
     TransmogSet,
