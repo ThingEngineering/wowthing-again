67e8988ecf6d223b2eb7bc79cc60f65746fdd27c
Freddie <freddie@wafflemonster.org>
2024-08-08 19:41:16 -0700

    Consistently use the new cancellation token source in worker loop

--- apps/backend/Services/WorkerService.cs
+++ apps/backend/Services/WorkerService.cs
@@ -93,13 +93,13 @@ public class WorkerService : BackgroundService
         int backoffDelay = InitialBackoff;
         while (!cancellationToken.IsCancellationRequested)
         {
+            using var jobTokenSource = new CancellationTokenSource();
+
             while (_stateService.AccessToken?.Valid != true)
             {
                 _logger.Warning("Waiting for auth service to be ready");
-                await Task.Delay(1000, cancellationToken);
+                await Task.Delay(1000, jobTokenSource.Token);
             }
-
-            using var jobTokenSource = new CancellationTokenSource();
 
             // Something is crashing workers, the outer try/catch is to work out what on earth is going on
             try
@@ -108,7 +108,7 @@ public class WorkerService : BackgroundService
                 if (queuedJob == null)
                 {
                     backoffDelay = Math.Min(MaxBackoff, backoffDelay * 2);
-                    await Task.Delay(backoffDelay, cancellationToken);
+                    await Task.Delay(backoffDelay, jobTokenSource.Token);
                     continue;
                 }
 
@@ -128,7 +128,7 @@ public class WorkerService : BackgroundService
                         job.Setup(data);
                         await job.Run(data);
 
-                        await using var context = await _contextFactory.CreateDbContextAsync(cancellationToken);
+                        await using var context = await _contextFactory.CreateDbContextAsync(jobTokenSource.Token);
                         await context.QueuedJob
                             .Where(qj => qj.Id == queuedJob.Id)
                             .ExecuteDeleteAsync(jobTokenSource.Token);
@@ -137,7 +137,7 @@ public class WorkerService : BackgroundService
                     {
                         _logger.Error(ex, "Job failed");
 
-                        await using var context = await _contextFactory.CreateDbContextAsync(cancellationToken);
+                        await using var context = await _contextFactory.CreateDbContextAsync(jobTokenSource.Token);
                         if (queuedJob.Failures >= 2)
                         {
                             await context.QueuedJob
