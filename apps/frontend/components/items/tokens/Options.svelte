0063b0780ce45463a7529d12434db4823b1d6485
Freddie <freddie@wafflemonster.org>
2024-08-17 21:38:22 -0700

    Add Collecting Settings to tokens options

--- apps/frontend/components/items/tokens/Options.svelte
+++ apps/frontend/components/items/tokens/Options.svelte
@@ -2,10 +2,30 @@
     import { browserStore } from '@/shared/stores/browser';
 
     import Checkbox from '@/shared/components/forms/CheckboxInput.svelte'
+    import SidebarCollectingSettings from '@/components/common/SidebarCollectingSettings.svelte';
 </script>
 
+<style lang="scss">
+    .options-container {
+        align-items: start;
+
+        :global(.collecting-settings) {
+            border-radius: $border-radius;
+            width: 17rem;
+        }
+        :global(.collecting-settings button.expand) {
+            border-width: 0 !important;
+        }
+        :global(.collecting-settings > *:nth-child(n+4)) {
+            display: none;
+        }
+    }
+</style>
+
 <div class="options-container">
-    <button>
+    <SidebarCollectingSettings />
+
+    <button style="margin-left: 1rem">
         <Checkbox
             name="highlight_missing"
             bind:value={$browserStore.tokens.highlightMissing}
