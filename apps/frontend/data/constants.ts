9d7b6a02bef73f21cced6ca080a08f49b82c91db
Freddie <freddie@wafflemonster.org>
2024-08-23 14:29:54 -0700

    Bump max level/expansion

--- apps/frontend/data/constants.ts
+++ apps/frontend/data/constants.ts
@@ -1,6 +1,6 @@
 export abstract class Constants {
-    static readonly characterMaxLevel: number = 70;
-    static readonly expansion: number = 9;
+    static readonly characterMaxLevel: number = 80;
+    static readonly expansion: number = 10;
     static readonly maxRenown: number = 80;
     static readonly restedDuration: number = 10 * 24 * 60 * 60; // 10 days
 
