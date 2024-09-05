a304d171703f79ffdde5e7be0f89631088c0582a
Freddie <freddie@wafflemonster.org>
2024-09-05 16:26:57 -0700

    Working dbStore search

--- apps/frontend/shared/stores/db/types/store.ts
+++ apps/frontend/shared/stores/db/types/store.ts
@@ -1,17 +1,18 @@
-import type { DbDataThing, DbDataThingArray } from './thing'
-
+import type { DbDataThing, DbDataThingArray } from './thing';
 
 export interface DbData {
-    mapsById: Record<number, string>
-    requirementsById: Record<number, string>
-    tagsById: Record<number, string>
+    mapsById: Record<number, string>;
+    requirementsById: Record<number, string>;
+    tagsById: Record<number, string>;
 
-    rawThings: DbDataThingArray[]
-    things: DbDataThing[]
+    rawThings: DbDataThingArray[];
+    things: DbDataThing[];
 
-    tagsByString: Record<string, number>
-    thingsByContentTypeAndId: Record<number, Record<number, DbDataThing[]>>
-    thingsByMapId: Record<number, DbDataThing[]>
-    thingsByRequirementId: Record<number, DbDataThing[]>
-    thingsByTagId: Record<number, DbDataThing[]>
+    thingsByContentTypeAndId: Record<number, Record<number, DbDataThing[]>>;
+    thingsByMapId: Record<number, DbDataThing[]>;
+    thingsByRequirementId: Record<number, DbDataThing[]>;
+    thingsByTagId: Record<number, DbDataThing[]>;
+
+    mapsByName: Record<string, number>;
+    tagsByName: Record<string, number>;
 }
