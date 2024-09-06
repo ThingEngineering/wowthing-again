119c89e630b3a03ef9ee42379ee22e132f2fd1a2
Freddie <freddie@wafflemonster.org>
2024-09-05 21:34:28 -0700

    Add Professions>TWW Knowledge
    - Add relevant profession knowledge to the expansion page
    - Remove Professions>Knowledge

--- apps/frontend/components/professions/ProfessionsView.svelte
+++ apps/frontend/components/professions/ProfessionsView.svelte
@@ -2,8 +2,8 @@
     import type { MultiSlugParams } from '@/types'
 
     import Equipment from './equipment/Equipment.svelte'
-    import DragonflightKnowledge from './dragonflight-knowledge/DragonflightKnowledge.svelte'
-    import Knowledge from './knowledge/Knowledge.svelte';
+    import DragonflightKnowledge from './knowledge/DragonflightKnowledge.svelte'
+    import KnowledgeV2 from './knowledge/KnowledgeV2.svelte';
     import Overview from './overview/ProfessionsOverview.svelte'
     import Recipes from './recipes/ProfessionsRecipes.svelte'
 
@@ -13,10 +13,10 @@
 <div class="view">
     {#if params.slug1 === 'dragonflight-knowledge'}
         <DragonflightKnowledge />
+    {:else if params.slug1.endsWith('-knowledge')}
+        <KnowledgeV2 slug={params.slug1.replace('-knowledge', '')} />
     {:else if params.slug1 === 'equipment'}
         <Equipment slug={params.slug2} />
-    {:else if params.slug1 === 'knowledge'}
-        <Knowledge />
     {:else if params.slug1 === 'overview'}
         <Overview slug={params.slug2} />
     {:else if params.slug1 === 'recipes'}
---
+++ apps/frontend/components/professions/knowledge/V2Row.svelte
@@ -0,0 +1,160 @@
+<script lang="ts">
+    import orderBy from 'lodash/orderBy'
+
+    import { warWithinProfessions } from '@/data/professions'
+    import { warWithinZones } from '@/data/zones';
+    import { Profession } from '@/enums/profession'
+    import { dbStore } from '@/shared/stores/db'
+    import { staticStore } from '@/shared/stores/static';
+    import { componentTooltip } from '@/shared/utils/tooltips'
+    import { itemStore, userQuestStore } from '@/stores'
+    import type {  Character } from '@/types'
+
+    import Tooltip from '@/components/tooltips/profession-knowledge/TooltipProfessionKnowledge.svelte'
+
+    export let character: Character
+
+    type ZoneData = {
+        have: number
+        status?: string
+        total: number
+        items: ZoneItem[]
+    }
+    type ZoneItem  = {
+        have: boolean
+        itemId: number
+        profession: Profession
+        source?: string
+    }
+
+    let data: ZoneData[]
+    $: {
+        data = []
+
+        // Zones
+        for (const zone of warWithinZones) {
+            if (zone === null) {
+                data.push(null)
+                continue
+            }
+
+            const zoneData: ZoneData = {
+                have: 0,
+                total: 0,
+                items: [],
+            }
+
+            for (const profData of warWithinProfessions) {
+                if (!character.professions?.[profData.id]) {
+                    continue
+                }
+                
+                const things = dbStore.search({
+                    maps: [
+                        zone.map,
+                    ],
+                    tags: [
+                        `expansion:10`,
+                        `profession:${$staticStore.professions[profData.id].slug}`,
+                        'treasure:profession'
+                    ],
+                })
+
+                things.sort((a, b) => $itemStore.items[a.contents[0].id].name.localeCompare($itemStore.items[b.contents[0].id].name))
+
+                for (const thing of things) {
+                    zoneData.items.push({
+                        have: userQuestStore.hasAny(character.id, thing.trackingQuestId),
+                        itemId: thing.contents[0].id,
+                        profession: profData.id,
+                        source: zone.shortName,
+                    })
+                }
+
+                if (zone.name.endsWith('Books')) {
+                    // const bookQuests = (profData.bookQuests || []).filter((bq) =>
+                    //     (['LN', 'ZCB'].indexOf(bq.source) >= 0 && zone.shortName === 'ZC') ||
+                    //     (bq.source.startsWith(`${zone.shortName} `))
+                    // )
+
+                    // const characterRenown = zone.reputationId
+                    //     ? Math.floor((character.reputations?.[zone.reputationId] ?? 0) / 2500)
+                    //     : 0
+
+                    // for (const bookQuest of orderBy(bookQuests, (bq) => $itemStore.items[bq.itemId].name)) {
+                    //     const bookData = {
+                    //         have: userQuestStore.hasAny(character.id, bookQuest.questId),
+                    //         itemId: bookQuest.itemId,
+                    //         profession: profData.id,
+                    //         source: bookQuest.source,
+                    //     }
+
+                    //     const requiredRenown = parseInt(bookQuest.source.split(' ')[1])
+                    //     if (!bookData.have) {
+                    //         if (!isNaN(requiredRenown) && requiredRenown <= characterRenown) {
+                    //             zoneData.status = 'fail'
+                    //         }
+                    //         else if (bookData.source.startsWith('AC ') || bookData.source === 'LN') {
+                    //             zoneData.status = 'fail'
+                    //         }
+                    //     }
+
+                    //     zoneData.items.push(bookData)
+                    // }
+                }
+            }
+
+            zoneData.have = zoneData.items.filter((item) => item.have).length
+            zoneData.total = zoneData.items.length
+            zoneData.items.sort((a, b) => Profession[a.profession].localeCompare(Profession[b.profession]))
+
+            if (zone.name.endsWith('Books')) {
+                if (!zoneData.status) {
+                    zoneData.status = zoneData.have < zoneData.total ? 'shrug' : 'success'
+                }
+            }
+            else {
+                zoneData.status = zoneData.have === 0
+                    ? 'fail'
+                    : (zoneData.have < zoneData.total ? 'shrug' : 'success')
+            }
+
+            data.push(zoneData)
+        }
+    }
+</script>
+
+<style lang="scss">
+    .profession-knowledge {
+        @include cell-width($width-progress, $maxWidth: $width-progress-max);
+
+        border-left: 1px solid $border-color;
+        text-align: center;
+    }
+    .faded {
+        color: #aaa;
+    }
+</style>
+
+<td class="spacer"></td>
+{#each warWithinZones as zone, zoneIndex}
+    {@const zoneData = data[zoneIndex]}
+    {#if zoneData.items.length > 0}
+        <td
+            class="profession-knowledge status-{zoneData.status}"
+            use:componentTooltip={{
+                component: Tooltip,
+                props: {
+                    character,
+                    reputationId: 0,
+                    zoneData,
+                    zoneName: zone.name,
+                }
+            }}
+        >
+            {zoneData.have} / {zoneData.total}
+        </td>
+    {:else}
+        <td class="profession-knowledge faded">---</td>
+    {/if}
+{/each}
