5d6c72961e8d4f41626a53a971f889a448990e73
Freddie <freddie@wafflemonster.org>
2024-09-07 12:55:47 -0700

    Add Campaign/QuestLine dump models

---
+++ apps/tool/Models/Quests/DumpCampaignXQuestLine.cs
@@ -0,0 +1,10 @@
+﻿namespace Wowthing.Tool.Models.Quests;
+
+// ReSharper disable InconsistentNaming
+public class DumpCampaignXQuestLine
+{
+    public int ID { get; set; }
+    public int CampaignID { get; set; }
+    public int OrderIndex { get; set; }
+    public int QuestLineID { get; set; }
+}
