36a923816564df93573b418141f5f73b1f57df80
Freddie <freddie@wafflemonster.org>
2024-09-07 18:18:04 -0700

    Rework quest title updating to suck a bit less

--- apps/backend/Jobs/Data/DataQuestsStartJob.cs
+++ apps/backend/Jobs/Data/DataQuestsStartJob.cs
@@ -1,4 +1,7 @@
-﻿using Wowthing.Lib.Jobs;
+﻿using Wowthing.Lib.Enums;
+using Wowthing.Lib.Jobs;
+using Wowthing.Lib.Models;
+using Wowthing.Lib.Utilities;
 
 namespace Wowthing.Backend.Jobs.Data;
 
@@ -8,28 +11,64 @@ public class DataQuestsStartJob : JobBase, IScheduledJob
     {
         Type = JobType.DataQuestsStart,
         Priority = JobPriority.High,
-        Interval = TimeSpan.FromMinutes(5),
+        Interval = TimeSpan.FromMinutes(1),
         Version = 1,
     };
 
     public override async Task Run(string[] data)
     {
-        var questIds = await Context
-            .WowQuest
-            .Where(wq => wq.LastApiCheck < DateTime.UtcNow.AddDays(-30))
-            .Select(wq => wq.Id)
+        var timer = new JankTimer();
+
+        var oneMonthAgo = DateTime.UtcNow.AddMonths(-1);
+        var quests = await Context.WowQuest
+            .FromSqlInterpolated($@"
+WITH quest_ids AS (
+    SELECT  wq.id
+    FROM    wow_quest wq
+    WHERE   wq.last_api_check < {oneMonthAgo}
+            AND NOT EXISTS (
+                SELECT  1
+                FROM    language_string ls
+                WHERE   ls.language = {Language.enUS}
+                        AND ls.type = {StringType.WowQuestName}
+                        AND ls.id = wq.id
+            )
+    ORDER BY wq.id
+    FOR UPDATE SKIP LOCKED
+    LIMIT   1000
+)
+UPDATE  wow_quest
+SET     last_api_check = CURRENT_TIMESTAMP
+WHERE   id = ANY(SELECT id FROM quest_ids)
+RETURNING *
+")
             .ToArrayAsync(CancellationToken);
 
-        foreach (var questId in questIds)
+        timer.AddPoint("Query");
+
+        foreach (var quest in quests)
         {
-            await JobRepository.AddJobAsync(JobPriority.Low, JobType.DataQuest, questId.ToString());
+            Context.LanguageString.Add(new LanguageString
+            {
+                Language = Language.enUS,
+                Type = StringType.WowQuestName,
+                Id = quest.Id,
+                String = "~PLACEHOLDER~",
+            });
+            await JobRepository.AddJobAsync(
+                JobPriority.Bulk,
+                JobType.DataQuest,
+                ((int)WowRegion.US).ToString(),
+                quest.Id.ToString()
+            );
         }
 
-        await Context.WowQuest
-            .Where(wq => questIds.Contains(wq.Id))
-            .ExecuteUpdateAsync(s => s
-                .SetProperty(wq => wq.LastApiCheck, wq => DateTime.UtcNow),
-                CancellationToken
-            );
+        timer.AddPoint("Iterate");
+
+        await Context.SaveChangesAsync();
+
+        timer.AddPoint("Save", true);
+
+        Logger.Information(timer.ToString());
     }
 }
