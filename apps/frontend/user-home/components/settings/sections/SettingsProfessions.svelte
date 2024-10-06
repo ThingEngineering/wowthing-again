12cfa7f0610b9c9f8f4acffc39cc01c2815a0ae7
Freddie <freddie@wafflemonster.org>
2024-10-06 10:59:50 -0700

    Clean up Settings>Professions layout

--- apps/frontend/user-home/components/settings/sections/SettingsProfessions.svelte
+++ apps/frontend/user-home/components/settings/sections/SettingsProfessions.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
-    import groupBy from 'lodash/groupBy'
     import sortBy from 'lodash/sortBy'
 
-    import { professionCooldowns, professionWorkOrders } from '@/data/professions/cooldowns'
     import { staticStore } from '@/shared/stores/static'
     import { settingsStore } from '@/shared/stores/settings'
 
     import CheckboxInput from '@/shared/components/forms/CheckboxInput.svelte'
     import Collecting from './SettingsProfessionsCollecting.svelte'
-    import ProfessionIcon from '@/shared/components/images/ProfessionIcon.svelte'
+    import Cooldowns from './SettingsProfessionsCooldowns.svelte'
 
-    const groupedCooldowns = groupBy(
-        [...professionWorkOrders, ...professionCooldowns],
-        (cd) => cd.profession
-    )
     const sortedProfessions = sortBy(
         Object.values($staticStore.professions),
         (prof) => [prof.type, prof.name]
@@ -22,90 +16,54 @@
     $: $settingsStore.professions.collectingCharacters ||= {}
 </script>
 
-<style lang="scss">
-    .many-boxes {
-        --image-border-width: 1px;
-        --image-margin-top: -4px;
+<div>
+    <Collecting {sortedProfessions} />
 
-        display: flex;
-        flex-wrap: wrap;
-        gap: 0 1rem;
+    <div class="settings-block">
+        <h3>Profession Weeklies</h3>
 
-        :global(fieldset) {
-            min-width: 0;
-            width: 14rem;
-        }
+        <CheckboxInput
+            bind:value={$settingsStore.professions.dragonflightCountCraftingDrops}
+            name="professions_dragonflightCountCraftingDrops"
+        >
+            Count incomplete crafting drops in Profession Weeklies.
+        </CheckboxInput>
 
-        + .many-boxes {
-            border-top: 1px dotted $border-color;
-        }
-    }
-</style>
+        <CheckboxInput
+            bind:value={$settingsStore.professions.dragonflightCountGathering}
+            name="professions_dragonflightCountGathering"
+        >
+            Count incomplete gathering tasks in Profession Weeklies.
+        </CheckboxInput>
 
-<Collecting {sortedProfessions} />
+        <CheckboxInput
+            bind:value={$settingsStore.professions.dragonflightCountTasks}
+            name="professions_dragonflightCountTasks"
+        >
+            Count incomplete "Task" tasks in Profession Weeklies.
+        </CheckboxInput>
 
-<div class="settings-block">
-    <h3>Cooldowns</h3>
+        <CheckboxInput
+            bind:value={$settingsStore.professions.dragonflightTreatises}
+            name="professions_dragonflightTreatises"
+        >
+            Show Treatises in Profession Weeklies.
+        </CheckboxInput>
 
-    {#each sortedProfessions as profession}
-        {#if groupedCooldowns[profession.id]}
-            <div class="many-boxes">
-                {#each groupedCooldowns[profession.id] as cooldown}
-                    <CheckboxInput
-                        name="professions_{cooldown.key}"
-                        bind:value={$settingsStore.professions.cooldowns[cooldown.key]}
-                    >
-                        <ProfessionIcon id={cooldown.profession} border={1} />
-                        {cooldown.name.replace('[DF] ', '')}
-                    </CheckboxInput>
-                {/each}
-            </div>
-        {/if}
-    {/each}
+        <CheckboxInput
+            bind:value={$settingsStore.professions.fullConcentrationIsBad}
+            name="professions_fullConcentrationIsBad"
+        >
+            Full Concentration is a bad thing.
+        </CheckboxInput>
+
+        <CheckboxInput
+            bind:value={$settingsStore.professions.ignoreTasksWhenDoneWithTraits}
+            name="professions_ignoreTasksWhenDoneWithTraits"
+        >
+            Ignore tasks when character has 100% of traits.
+        </CheckboxInput>
+    </div>
 </div>
 
-<div class="settings-block">
-    <h3>Profession Weeklies</h3>
-
-    <CheckboxInput
-        bind:value={$settingsStore.professions.dragonflightCountCraftingDrops}
-        name="professions_dragonflightCountCraftingDrops"
-    >
-        Count incomplete crafting drops in Profession Weeklies.
-    </CheckboxInput>
-
-    <CheckboxInput
-        bind:value={$settingsStore.professions.dragonflightCountGathering}
-        name="professions_dragonflightCountGathering"
-    >
-        Count incomplete gathering tasks in Profession Weeklies.
-    </CheckboxInput>
-
-    <CheckboxInput
-        bind:value={$settingsStore.professions.dragonflightCountTasks}
-        name="professions_dragonflightCountTasks"
-    >
-        Count incomplete "Task" tasks in Profession Weeklies.
-    </CheckboxInput>
-
-    <CheckboxInput
-        bind:value={$settingsStore.professions.dragonflightTreatises}
-        name="professions_dragonflightTreatises"
-    >
-        Show Treatises in Profession Weeklies.
-    </CheckboxInput>
-
-    <CheckboxInput
-        bind:value={$settingsStore.professions.fullConcentrationIsBad}
-        name="professions_fullConcentrationIsBad"
-    >
-        Full Concentration is a bad thing.
-    </CheckboxInput>
-
-    <CheckboxInput
-        bind:value={$settingsStore.professions.ignoreTasksWhenDoneWithTraits}
-        name="professions_ignoreTasksWhenDoneWithTraits"
-    >
-        Ignore tasks when character has 100% of traits.
-    </CheckboxInput>
-</div>
+<Cooldowns {sortedProfessions} />
