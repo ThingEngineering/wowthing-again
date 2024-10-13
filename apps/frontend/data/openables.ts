469a7af1a0915ed49c3ec11f2ae5732ad0f8a7fe
Freddie <freddie@wafflemonster.org>
2024-10-13 13:06:17 -0700

    Add openable items to statuses

---
+++ apps/frontend/data/openables.ts
@@ -0,0 +1,75 @@
+export const openables = new Set<number>([
+    // The War Within
+    217012, // Novice Actor's Chest
+    217013, // Expert Actor's Chest
+    222929, // Carved Coin Coffer
+    222954, // Runed Coin Coffer
+    224556, // Glorious Contender's Strongbox
+    224557, // Field Medic's Hazard Payout
+    224573, // Crystal Satchel of Cooperation
+    224650, // Wax-Sealed Pouch
+    224721, // Wax-Sealed Box
+    224722, // Waxy Bundle of Resonance Crystals
+    224723, // Waxy Bundle of Leather
+    224724, // Waxy Bundle of Dust
+    224725, // Waxy Bundle of Herbs
+    224726, // Waxy Box of Rocks
+    224784, // Pinnacle Cache
+    225239, // Overflowing Council of Dornogal Trove
+    225245, // Overflowing Trove of the Deeps
+    225246, // Overflowing Hallowfall Trove
+    224247, // Overflowing Severed Threads Trove
+    225493, // Medal of Conquest
+    225571, // The Weaver's Gratuity
+    225572, // The General's War Chest
+    225573, // The Vizier's Capital
+    226045, // The General's Trove
+    226100, // The Vizier's Trove
+    226103, // The Weaver's Trove
+    226193, // Cache of Nerubian Treasures [N]
+    226194, // Cache of Nerubian Treasures [H]
+    226256, // Token of the Remembrancers
+    226258, // Delver's Pouch of Reagents
+    226263, // Theater Troupe's Trove
+    226264, // Radiant Cache
+    226273, // Awakened Mechanical Cache
+    226339, // Nerubian Mining Supplies
+    226392, // Careless Dasher's Treasure
+    226814, // Chest of Gold
+    227450, // Sky Racer's Purse
+    227675, // Satchel of Surplus Herbs
+    227676, // Satchel of Surplus Ore
+    227681, // Satchel of Surplus Leather
+    227682, // Satchel of Surplus Cloth
+    227713, // Artisan's Consortium Payout
+    227792, // Everyday Cache
+    228220, // Waxy Bundle
+    228337, // Satchel of Surplus Dust
+    228361, // Seasoned Adventurer's Cache
+    228610, // Artisan's Consortium Payout
+    228611, // Artisan's Consortium Payout
+    228612, // Artisan's Consortium Payout
+    228741, // Lamplighter Supply Satchel
+    229005, // Cache of Earthen Treasures
+    229006, // Cache of Earthen Treasures
+    229129, // Cache of Delver's Spoils
+    229130, // Cache of Delver's Spoils
+    229354, // Algari Adventurer's Cache
+    232602, // Forged Gladiator's Coin Pouch
+
+    220773, // Celebratory Pack of Runed Harbinger Crests
+    220776, // Glorious Cluster of Gilded Harbinger Crests
+    220767, // Triumphant Satchel of Carved Harbinger Crests
+    221268, // Pouch of Weathered Harbinger Crests
+    221373, // Satchel of Carved Harbinger Crests
+    221375, // Pack of Runed Harbinger Crests
+
+    227668, // Delver's Bounty T1
+    227778, // Delver's Bounty T2
+    227779, // Delver's Bounty T3
+    227780, // Delver's Bounty T4
+    227781, // Delver's Bounty T5
+    227782, // Delver's Bounty T6
+    227783, // Delver's Bounty T7
+    227784, // Delver's Bounty T8
+]);
