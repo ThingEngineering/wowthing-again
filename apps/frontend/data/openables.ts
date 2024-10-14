18c847c3cc4c573dfc5c4cc1d96c7316e60d3240
Freddie <freddie@wafflemonster.org>
2024-10-13 19:29:43 -0700

    Add more openables

--- apps/frontend/data/openables.ts
+++ apps/frontend/data/openables.ts
@@ -18,7 +18,7 @@ export const openables = new Set<number>([
     225239, // Overflowing Council of Dornogal Trove
     225245, // Overflowing Trove of the Deeps
     225246, // Overflowing Hallowfall Trove
-    224247, // Overflowing Severed Threads Trove
+    225247, // Overflowing Severed Threads Trove
     225493, // Medal of Conquest
     225571, // The Weaver's Gratuity
     225572, // The General's War Chest
@@ -57,6 +57,9 @@ export const openables = new Set<number>([
     229354, // Algari Adventurer's Cache
     232602, // Forged Gladiator's Coin Pouch
 
+    221269, // Crimson Valorstone
+    225896, // Void-Touched Valorstone
+    226813, // Golden Valorstone
     220773, // Celebratory Pack of Runed Harbinger Crests
     220776, // Glorious Cluster of Gilded Harbinger Crests
     220767, // Triumphant Satchel of Carved Harbinger Crests
@@ -72,4 +75,45 @@ export const openables = new Set<number>([
     227782, // Delver's Bounty T6
     227783, // Delver's Bounty T7
     227784, // Delver's Bounty T8
+
+    // TWW Raid Tokens
+    225614, // Dreadful Blasphemer's Effigy
+    225622, // Dreadful Conniver's Badge
+    225630, // Dreadful Obscenity's Idol
+    225626, // Dreadful Slayer's Icon
+    225618, // Dreadful Stalwart's Emblem
+    225615, // Mystic Blasphemer's Effigy
+    225623, // Mystic Conniver's Badge
+    225631, // Mystic Obscenity's Idol
+    225627, // Mystic Slayer's Icon
+    225619, // Mystic Stalwart's Emblem
+    225616, // Venerated Blasphemer's Effigy
+    225624, // Venerated Conniver's Badge
+    225632, // Venerated Obscenity's Idol
+    225628, // Venerated Slayer's Icon
+    225620, // Venerated Stalwart's Emblem
+    225617, // Zenith Blasphemer's Effigy
+    225625, // Zenith Conniver's Badge
+    225633, // Zenith Obscenity's Idol
+    225629, // Zenith Slayer's Icon
+    225621, // Zenith Stalwart's Emblem
+    226206, // Mark of the Spelunker Supreme
+
+    // TWW Professions
+    228724, // Flicker of Alchemy Knowledge
+    228726, // Flicker of Blacksmithing Knowledge
+    228728, // Flicker of Enchanting Knowledge
+    228730, // Flicker of Engineering Knowledge
+    228732, // Flicker of Inscription Knowledge
+    228734, // Flicker of Jewelcrafting Knowledge
+    228736, // Flicker of Leatherworking Knowledge
+    228738, // Flicker of Tailoring Knowledge
+    228725, // Glimmer of Alchemy Knowledge
+    228727, // Glimmer of Blacksmithing Knowledge
+    228729, // Glimmer of Enchanting Knowledge
+    228731, // Glimmer of Engineering Knowledge
+    228733, // Glimmer of Inscription Knowledge
+    228735, // Glimmer of Jewelcrafting Knowledge
+    228737, // Glimmer of Leatherworking Knowledge
+    228739, // Glimmer of Tailoring Knowledge
 ]);
