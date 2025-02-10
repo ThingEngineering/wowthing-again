6cbf09cb3f92bf13bc19a39a3c79d04c69a7ebbf
Freddie <freddie@wafflemonster.org>
2025-02-09 20:18:57 -0800

    Handle db location factions

--- apps/tool/Models/Db/OutDbThingLocation.cs
+++ apps/tool/Models/Db/OutDbThingLocation.cs
@@ -12,10 +12,20 @@ public class OutDbThingLocation
         MapId = mapId;
 
         // Convert x/y locations (eg 12.34 56.78) to an 8-digit integer (eg 12345678)
-        decimal[] parts = locationString
-            .Split()
+        string[] parts = locationString.Split();
+        decimal[] coords = parts[..2]
             .Select(s => decimal.Parse(s, CultureInfo.InvariantCulture))
             .ToArray();
-        PackedLocation = (int)(parts[0] * 100 * 10000) + (int)(parts[1] * 100);
+        PackedLocation = (int)(coords[0] * 100 * 10000) + (int)(coords[1] * 100);
+
+        // Add alliance/horde flag
+        if (parts.Length > 2 && parts[2] == "alliance")
+        {
+            PackedLocation += 100000000;
+        }
+        else if (parts.Length > 2 && parts[2] == "horde")
+        {
+            PackedLocation += 200000000;
+        }
     }
 }
