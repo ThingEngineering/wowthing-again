066cb9bf676380e09340d3bf841b98d60aa78c2f
Freddie <freddie@wafflemonster.org>
2025-02-18 23:05:20 -0800

    Don't show search items with 0 results

--- apps/frontend/components/items/ItemsSearchItemTable.svelte
+++ apps/frontend/components/items/ItemsSearchItemTable.svelte
@@ -34,49 +34,51 @@
 
 {#each response as item}
     {@const { characterItems, itemCount } = getCombinedItems(item)}
-    <table class="table table-striped search-table">
-        <thead>
-            <tr class="item-row">
-                <th
-                    class="item quality{$itemStore.items[item.itemId].quality} text-overflow"
-                    colspan="{userStore.useAccountTags ? 4 : 3}"
-                >
-                    <WowthingImage name="item/{item.itemId}" size={20} border={1} />
-                    {item.itemName}
-                </th>
-                <th
-                    class="count"
-                    use:basicTooltip={itemCount.toLocaleString()}
-                >
-                    {toNiceNumber(itemCount)}
-                </th>
-                <th class="item-level">ILvl</th>
-            </tr>
-        </thead>
+    {#if itemCount > 0}
+        <table class="table table-striped search-table">
+            <thead>
+                <tr class="item-row">
+                    <th
+                        class="item quality{$itemStore.items[item.itemId].quality} text-overflow"
+                        colspan="{userStore.useAccountTags ? 4 : 3}"
+                    >
+                        <WowthingImage name="item/{item.itemId}" size={20} border={1} />
+                        {item.itemName}
+                    </th>
+                    <th
+                        class="count"
+                        use:basicTooltip={itemCount.toLocaleString()}
+                    >
+                        {toNiceNumber(itemCount)}
+                    </th>
+                    <th class="item-level">ILvl</th>
+                </tr>
+            </thead>
 
-        <tbody>
-            {#each characterItems as characterItem}
-                <Row
-                    itemId={item.itemId}
-                    {characterItem}
-                />
-            {/each}
+            <tbody>
+                {#each characterItems as characterItem}
+                    <Row
+                        itemId={item.itemId}
+                        {characterItem}
+                    />
+                {/each}
 
-            {#each (item.guildBanks || []) as guildBankItem}
-                <Row
-                    itemId={item.itemId}
-                    {guildBankItem}
-                />
-            {/each}
+                {#each (item.guildBanks || []) as guildBankItem}
+                    <Row
+                        itemId={item.itemId}
+                        {guildBankItem}
+                    />
+                {/each}
 
-            {#each (item.warbank || []) as warbankItem}
-                <Row
-                    itemId={item.itemId}
-                    {warbankItem}
-                />
-            {/each}
-        </tbody>
-    </table>
+                {#each (item.warbank || []) as warbankItem}
+                    <Row
+                        itemId={item.itemId}
+                        {warbankItem}
+                    />
+                {/each}
+            </tbody>
+        </table>
+    {/if}
 {:else}
     <table class="table table-striped search-table">
         <tr>
