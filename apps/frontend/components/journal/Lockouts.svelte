87d0415def188556b2c4c2152c436ed822d2f680
Freddie <freddie@wafflemonster.org>
2025-02-27 19:03:59 -0800

    Reformat lockouts file

--- apps/frontend/components/journal/Lockouts.svelte
+++ apps/frontend/components/journal/Lockouts.svelte
@@ -3,17 +3,19 @@
 
     import { classOrderMap } from '@/data/character-class';
     import { uiIcons } from '@/shared/icons';
-    import { componentTooltip } from '@/shared/utils/tooltips'
+    import { componentTooltip } from '@/shared/utils/tooltips';
     import { userStore } from '@/stores';
     import type { JournalDataInstance } from '@/types/data';
 
     import IconifyIcon from '@/shared/components/images/IconifyIcon.svelte';
-    import TooltipLockout from '@/components/tooltips/lockout/TooltipLockout.svelte'
+    import TooltipLockout from '@/components/tooltips/lockout/TooltipLockout.svelte';
 
-    export let instance: JournalDataInstance
+    export let instance: JournalDataInstance;
 
     $: instanceId = instance.id === 1278 ? 110001 : instance.id;
-    $: instanceLockouts = $userStore.allLockouts.filter((lockout) => lockout.instanceId === instanceId);
+    $: instanceLockouts = $userStore.allLockouts.filter(
+        (lockout) => lockout.instanceId === instanceId,
+    );
 </script>
 
 <style lang="scss">
@@ -37,12 +39,15 @@
         {#each instanceLockouts as instanceLockout}
             <div class="lockout">
                 <span>{instanceLockout.difficulty.shortName}:</span>
-                {#each sortBy(instanceLockout.characters, ([char]) => [classOrderMap[char.classId], char.name]) as [character, lockout]}
-                    {@const per = lockout.defeatedBosses / lockout.maxBosses * 100}
-                    {@const status = per === 0 ? 0 : (per < 100 ? 1 : 2)}
+                {#each sortBy( instanceLockout.characters, ([char]) => [classOrderMap[char.classId], char.name], ) as [character, lockout]}
+                    {@const per = (lockout.defeatedBosses / lockout.maxBosses) * 100}
+                    {@const status = per === 0 ? 0 : per < 100 ? 1 : 2}
                     <span
                         class="character class-{character.classId}"
-                        use:componentTooltip={{component: TooltipLockout, props: {character, lockout}}}
+                        use:componentTooltip={{
+                            component: TooltipLockout,
+                            props: { character, lockout },
+                        }}
                     >
                         <IconifyIcon
                             extraClass="status-{['fail', 'shrug', 'success'][status]}"
