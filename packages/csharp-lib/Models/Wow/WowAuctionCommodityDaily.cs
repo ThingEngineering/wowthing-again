638ef34621a9880a1f2a53aafb1b6bcbc1511afe
Freddie <freddie@wafflemonster.org>
2025-03-02 19:55:54 -0800

    Add MaintenanceAggregateHourlyAuctionDataJob

--- packages/csharp-lib/Models/Wow/WowAuctionCommodityDaily.cs
+++ packages/csharp-lib/Models/Wow/WowAuctionCommodityDaily.cs
@@ -1,4 +1,6 @@
-﻿namespace Wowthing.Lib.Models.Wow;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Wowthing.Lib.Models.Wow;
 
 public class WowAuctionCommodityDaily
 {
@@ -7,7 +9,18 @@ public class WowAuctionCommodityDaily
     public int ListedMin { get; set; }
     public int ListedMax { get; set; }
     public short Region { get; set; }
-    public List<int> AvgData { get; set; }
-    public List<int> MaxData { get; set; }
-    public List<int> MinData { get; set; }
+    public List<int> AvgData { get; set; } = [];
+    public List<int> MaxData { get; set; } = [];
+    public List<int> MinData { get; set; } = [];
+
+    [NotMapped]
+    public List<DailyDataPoint> DataPoints { get; set; }
+
+    public class DailyDataPoint
+    {
+        public int Max = int.MinValue;
+        public int Min = int.MaxValue;
+        public long Listed;
+        public long TotalPrice;
+    }
 }
