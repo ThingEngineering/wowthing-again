8c11848eddbdd99ae0a9a93f1eaaae1802b7a7e6
Freddie <freddie@wafflemonster.org>
2025-03-15 18:00:53 -0700

    Add collecting settings to zone maps sidebar

--- apps/frontend/components/zone-maps/ZoneMapsSidebar.svelte
+++ apps/frontend/components/zone-maps/ZoneMapsSidebar.svelte
@@ -1,47 +1,41 @@
 <script lang="ts">
-    import { lazyStore, manualStore } from '@/stores'
-    import type { SidebarItem } from '@/shared/components/sub-sidebar/types'
-    import type { UserCount } from '@/types'
+    import { lazyStore, manualStore } from '@/stores';
+    import type { SidebarItem } from '@/shared/components/sub-sidebar/types';
+    import type { UserCount } from '@/types';
 
-    import ProgressBar from '@/components/common/ProgressBar.svelte'
-    import Sidebar from '@/shared/components/sub-sidebar/SubSidebar.svelte'
+    import ProgressBar from '@/components/common/ProgressBar.svelte';
+    import Settings from '@/components/common/SidebarCollectingSettings.svelte';
+    import Sidebar from '@/shared/components/sub-sidebar/SubSidebar.svelte';
 
-    let categories: SidebarItem[]
-    let overall: UserCount
+    let categories: SidebarItem[];
+    let overall: UserCount;
     $: {
-        categories = $manualStore.zoneMaps.sets.map((set) => set === null ? null : ({
-            children: set.slice(1),
-            ...set[0],
-        }))
-        overall = $lazyStore.zoneMaps.counts['OVERALL']
+        categories = $manualStore.zoneMaps.sets.map((set) =>
+            set === null
+                ? null
+                : {
+                      children: set.slice(1),
+                      ...set[0],
+                  },
+        );
+        overall = $lazyStore.zoneMaps.counts['OVERALL'];
     }
 
-    const percentFunc = function(entry: SidebarItem, parentEntries?: SidebarItem[]) {
-        const slug = [...parentEntries, entry].slice(-2)
+    const percentFunc = function (entry: SidebarItem, parentEntries?: SidebarItem[]) {
+        const slug = [...parentEntries, entry]
+            .slice(-2)
             .map((entry) => entry.slug)
-            .join('--')
-        return $lazyStore.zoneMaps.counts[slug].percent
-    }
+            .join('--');
+        return $lazyStore.zoneMaps.counts[slug].percent;
+    };
 </script>
 
-<style lang="scss">
-    div {
-        margin-bottom: 0.75rem;
-    }
-</style>
+<Sidebar baseUrl="/zone-maps" items={categories} width="16rem" noVisitRoot={true} {percentFunc}>
+    <svelte:fragment slot="before">
+        <div>
+            <ProgressBar title="Overall" have={overall.have} total={overall.total} />
+        </div>
 
-<Sidebar
-    baseUrl="/zone-maps"
-    items={categories}
-    width="16rem"
-    noVisitRoot={true}
-    {percentFunc}
->
-    <div slot="before">
-        <ProgressBar
-            title="Overall"
-            have={overall.have}
-            total={overall.total}
-        />
-    </div>
+        <Settings />
+    </svelte:fragment>
 </Sidebar>
