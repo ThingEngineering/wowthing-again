a6a3b92c4cfdd29c1a081c56b0e33f7522c6d522
Freddie <freddie@wafflemonster.org>
2025-03-19 22:12:54 -0700

    Add TWW S2 crafting bonus IDs

--- apps/frontend/data/crafted-gear.ts
+++ apps/frontend/data/crafted-gear.ts
@@ -1,5 +1,7 @@
+// BonusIDs - season (ItemNameDescription) -> upgrade tiers (IncreaseItemLevel)
 export const craftedTiers: Record<number, number[]> = {
     9498: [null, 9402, 9403, 9404, 9405], // Dragonflight Season 3
     10249: [null, 9402, 9403, 9404, 9405], // Dragonflight Season 4
     10222: [null, 9624, 9625, 9626, 9627], // War Within Season 1
+    12040: [null, 9624, 9625, 9626, 9627], // War Within Season 2
 };
