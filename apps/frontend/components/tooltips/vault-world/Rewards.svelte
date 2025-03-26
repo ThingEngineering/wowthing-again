8b96cd73447ed7c4dd4d80602b9f67b813f769e3
Freddie <freddie@wafflemonster.org>
2025-03-25 19:47:44 -0700

    Show slot icons on vault rewards

--- apps/frontend/components/tooltips/vault-world/Rewards.svelte
+++ apps/frontend/components/tooltips/vault-world/Rewards.svelte
@@ -1,37 +1,42 @@
 <script lang="ts">
+    import IconifyIcon from '@/shared/components/images/IconifyIcon.svelte';
+    import { inventoryTypeIcons } from '@/shared/icons/mappings';
     import { itemStore } from '@/stores/item';
-    import type { Character, CharacterItem } from '@/types'
+    import type { Character, CharacterItem } from '@/types';
 
-    export let character: Character
+    export let character: Character;
 
-    let rewards: [CharacterItem, CalculatedItemData][]
+    let rewards: [CharacterItem, CalculatedItemData][];
     $: rewards = [
         character.weekly.vault.raidProgress,
         character.weekly.vault.dungeonProgress,
         character.weekly.vault.worldProgress,
-    ].flatMap((progress) => progress.flatMap((tier) => tier.rewards))
+    ]
+        .flatMap((progress) => progress.flatMap((tier) => tier.rewards))
         .filter((reward) => !!reward)
-        .map((reward) => [reward, applyBonusIds(reward)])
+        .map((reward) => [reward, applyBonusIds(reward)]);
 
     type CalculatedItemData = {
         itemLevel: number;
         quality: number;
-    }
+    };
 
     function applyBonusIds(rewardItem: CharacterItem): CalculatedItemData {
         const ret = {
             itemLevel: rewardItem.itemLevel,
             quality: rewardItem.quality,
-        }
+        };
 
         if (ret.itemLevel === 0) {
             const item = $itemStore.items[rewardItem.itemId];
             ret.itemLevel = item.itemLevel;
-            for (const bonusId of (rewardItem.bonusIds || [])) {
+            for (const bonusId of rewardItem.bonusIds || []) {
                 const itemBonus = $itemStore.itemBonuses[bonusId];
-                if (!itemBonus) { continue; }
+                if (!itemBonus) {
+                    continue;
+                }
 
-                for (const bonus of (itemBonus.bonuses || [])) {
+                for (const bonus of itemBonus.bonuses || []) {
                     if (bonus[0] === 1) {
                         ret.itemLevel += bonus[1];
                     } else if (bonus[0] === 3) {
@@ -55,10 +60,14 @@
     tbody tr:first-child td {
         border-top: 1px solid $border-color;
     }
+    .icon {
+        text-align: center;
+        width: 2rem;
+    }
     .item-level {
-        @include cell-width(2.0rem);
-        
-        text-align: right;
+        @include cell-width(2rem);
+
+        text-align: center;
     }
     .item-name {
         padding-left: 0.3rem;
@@ -70,13 +79,16 @@
 <table class="table-striped">
     <thead>
         <tr>
-            <td colspan="2">Available rewards:</td>
+            <td colspan="3">Available rewards:</td>
         </tr>
     </thead>
     <tbody>
         {#each rewards as [reward, calculated]}
             {@const item = $itemStore.items[reward.itemId]}
             <tr>
+                <td class="icon">
+                    <IconifyIcon icon={inventoryTypeIcons[item.inventoryType]} />
+                </td>
                 <td class="item-level">
                     {calculated.itemLevel}
                 </td>
