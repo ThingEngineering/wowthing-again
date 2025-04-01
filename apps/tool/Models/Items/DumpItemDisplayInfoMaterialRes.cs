9e1835a3451821ac827fe9df7b74cd6ca3b217f1
Freddie <freddie@wafflemonster.org>
2025-03-31 22:00:37 -0700

    Add some ItemDisplayInfo dump models

---
+++ apps/tool/Models/Items/DumpItemDisplayInfoMaterialRes.cs
@@ -0,0 +1,11 @@
+﻿namespace Wowthing.Tool.Models.Items;
+
+// ReSharper disable InconsistentNaming
+public class DumpItemDisplayInfoMaterialRes
+{
+    public int ID { get; set; }
+
+    public short ComponentSection { get; set; }
+    public int ItemDisplayInfoID { get; set; }
+    public int MaterialResourcesID { get; set; }
+}
---
+++ apps/tool/Models/Items/DumpItemDisplayInfoModelMatRes.cs
@@ -0,0 +1,12 @@
+﻿namespace Wowthing.Tool.Models.Items;
+
+// ReSharper disable InconsistentNaming
+public class DumpItemDisplayInfoModelMatRes
+{
+    public int ID { get; set; }
+
+    public int ItemDisplayInfoID { get; set; }
+    public int MaterialResourcesID { get; set; }
+    public short ModelIndex { get; set; }
+    public short TextureType { get; set; }
+}
