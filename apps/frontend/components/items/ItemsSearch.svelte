4a7774e0166f4700e04e386bda7be53d7ad5c5c0
Freddie <freddie@wafflemonster.org>
2025-04-12 10:29:42 -0700

    Fix item search not actually filtering by location

--- apps/frontend/components/items/ItemsSearch.svelte
+++ apps/frontend/components/items/ItemsSearch.svelte
@@ -1,48 +1,47 @@
 <script lang="ts">
-    import sortBy from 'lodash/sortBy'
-    import { afterUpdate, onMount } from 'svelte'
-    import { replace } from 'svelte-spa-router'
+    import sortBy from 'lodash/sortBy';
+    import { afterUpdate, onMount } from 'svelte';
+    import { replace } from 'svelte-spa-router';
 
-    import { ItemLocation } from '@/enums/item-location'
-    import { ItemQuality } from '@/enums/item-quality'
-    import { itemSearchState, userStore } from '@/stores'
-    import { getColumnResizer } from '@/utils/get-column-resizer'
-    import type { ItemSearchResponseItem } from '@/types/items'
+    import { ItemLocation } from '@/enums/item-location';
+    import { ItemQuality } from '@/enums/item-quality';
+    import { itemSearchState, userStore } from '@/stores';
+    import { getColumnResizer } from '@/utils/get-column-resizer';
+    import type { ItemSearchResponseItem } from '@/types/items';
 
-    import CharacterTable from './ItemsSearchCharacterTable.svelte'
-    import Checkbox from '@/shared/components/forms/CheckboxInput.svelte'
-    import ItemTable from './ItemsSearchItemTable.svelte'
-    import RadioGroup from '@/shared/components/forms/RadioGroup.svelte'
-    import Select from '@/shared/components/forms/Select.svelte'
-    import TextInput from '@/shared/components/forms/TextInput.svelte'
+    import CharacterTable from './ItemsSearchCharacterTable.svelte';
+    import Checkbox from '@/shared/components/forms/CheckboxInput.svelte';
+    import ItemTable from './ItemsSearchItemTable.svelte';
+    import RadioGroup from '@/shared/components/forms/RadioGroup.svelte';
+    import Select from '@/shared/components/forms/Select.svelte';
+    import TextInput from '@/shared/components/forms/TextInput.svelte';
 
-    let response: ItemSearchResponseItem[]
+    let response: ItemSearchResponseItem[];
 
-    $: formValid = $itemSearchState.isValid
-    $: if ($userStore.public) { replace('/items/bags') }
+    $: formValid = $itemSearchState.isValid;
+    $: if ($userStore.public) {
+        replace('/items/bags');
+    }
 
     onMount(() => {
         if (formValid) {
-            document.getElementById('item-search-submit').click()
+            document.getElementById('item-search-submit').click();
         }
-    })
+    });
 
-    const onSubmit = async function() {
-        response = await $itemSearchState.search()
+    const onSubmit = async function () {
+        response = await $itemSearchState.search();
         for (const item of response) {
-            item.characters = sortBy(
-                item.characters,
-                (char) => [
-                    1000000000 - char.count,
-                    $userStore.characterMap[char.characterId].name
-                ]
-            )
+            item.characters = sortBy(item.characters, (char) => [
+                1000000000 - char.count,
+                $userStore.characterMap[char.characterId].name,
+            ]);
         }
-    }
+    };
 
-    let containerElement: HTMLElement
-    let resizeableElement: HTMLElement
-    let debouncedResize: () => void
+    let containerElement: HTMLElement;
+    let resizeableElement: HTMLElement;
+    let debouncedResize: () => void;
     $: {
         if (resizeableElement) {
             debouncedResize = getColumnResizer(
@@ -53,17 +52,16 @@
                     columnCount: '--column-count',
                     gap: 20,
                     minColumns: 2,
-                    padding: '0.75rem'
-                }
-            )
-            debouncedResize()
-        }
-        else {
-            debouncedResize = null
+                    padding: '0.75rem',
+                },
+            );
+            debouncedResize();
+        } else {
+            debouncedResize = null;
         }
     }
 
-    afterUpdate(() => debouncedResize?.())
+    afterUpdate(() => debouncedResize?.());
 </script>
 
 <style lang="scss">
@@ -74,7 +72,12 @@
     form {
         align-items: center;
         display: flex;
-        gap: 0.5rem;
+        gap: 0.8rem;
+    }
+    .option {
+        align-items: center;
+        display: flex;
+        gap: 0.2rem;
     }
     .results-container {
         column-count: max(2, var(--column-count, 1));
@@ -107,9 +110,7 @@
 
 <div class="wrapper-column" bind:this={containerElement}>
     <div class="thing-container" bind:this={resizeableElement}>
-        <form
-            on:submit|preventDefault={onSubmit}
-        >
+        <form on:submit|preventDefault={onSubmit}>
             <TextInput
                 name="terms"
                 maxlength={20}
@@ -117,48 +118,55 @@
                 bind:value={$itemSearchState.searchTerms}
             />
 
-            <span>in</span>
+            <div class="option">
+                <span>Location:</span>
+                <Select
+                    name="location"
+                    width="9rem"
+                    bind:selected={$itemSearchState.location}
+                    options={[
+                        [ItemLocation.Any, '-Any-'],
+                        [ItemLocation.Bags, 'Bags'],
+                        [ItemLocation.Bank, 'Bank'],
+                        [ItemLocation.Reagent, 'Reagent Bank'],
+                        [ItemLocation.WarbandBank, 'Warband Bank'],
+                        [ItemLocation.GuildBank, 'Guild Bank'],
+                    ]}
+                />
+            </div>
 
-            <Select
-                name="location"
-                bind:selected={$itemSearchState.location}
-                options={[
-                    [ItemLocation.Any, '-Any-'],
-                    [ItemLocation.Bags, 'Bags'],
-                    [ItemLocation.Bank, 'Bank'],
-                    [ItemLocation.Reagent, 'Reagent Bank'],
-                    [ItemLocation.GuildBank, 'Guild Bank'],
-                ]}
-            />
+            <div class="option">
+                <span>Group by:</span>
+                <RadioGroup
+                    bind:value={$itemSearchState.groupBy}
+                    name="sort_by"
+                    options={[
+                        ['character', 'Character'],
+                        ['item', 'Item'],
+                    ]}
+                />
+            </div>
 
-            <span>group by</span>
+            <Checkbox name="include_equipped" bind:value={$itemSearchState.includeEquipped}
+                >Include equipped</Checkbox
+            >
 
-            <RadioGroup
-                bind:value={$itemSearchState.groupBy}
-                name="sort_by"
-                options={[
-                    ['character', 'Character'],
-                    ['item', 'Item'],
-                ]}
-            />
-
-            <Checkbox
-                    name="include_equipped"
-                    bind:value={$itemSearchState.includeEquipped}
-            >Include equipped</Checkbox>
-
-            <Select
-                name="minimum_quality"
-                bind:selected={$itemSearchState.minimumQuality}
-                options={[
-                    [ItemQuality.Poor, 'Poor'],
-                    [ItemQuality.Common, 'Common'],
-                    [ItemQuality.Uncommon, 'Uncommon'],
-                    [ItemQuality.Rare, 'Rare'],
-                    [ItemQuality.Epic, 'Epic'],
-                    [ItemQuality.Legendary, 'Legendary'],
-               ]}
-            />
+            <div class="option">
+                <span>Min quality:</span>
+                <Select
+                    name="minimum_quality"
+                    width="8rem"
+                    bind:selected={$itemSearchState.minimumQuality}
+                    options={[
+                        [ItemQuality.Poor, 'Poor'],
+                        [ItemQuality.Common, 'Common'],
+                        [ItemQuality.Uncommon, 'Uncommon'],
+                        [ItemQuality.Rare, 'Rare'],
+                        [ItemQuality.Epic, 'Epic'],
+                        [ItemQuality.Legendary, 'Legendary'],
+                    ]}
+                />
+            </div>
 
             <button
                 id="item-search-submit"
@@ -166,8 +174,8 @@
                 class:state-valid={formValid}
                 class:state-invalid={!formValid}
                 disabled={!formValid}
-                type="submit"
-            >Search!</button>
+                type="submit">Search!</button
+            >
         </form>
 
         {#if response !== undefined}
--- packages/csharp-lib/Enums/ItemLocation.cs
+++ packages/csharp-lib/Enums/ItemLocation.cs
@@ -2,7 +2,8 @@
 
 public enum ItemLocation : short
 {
-    Unknown = 0,
+    Unknown = -1,
+    Any = 0,
     Bags = 1,
     Bank = 2,
     ReagentBank = 3,
