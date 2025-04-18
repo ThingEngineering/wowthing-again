e158a8c9f4f1955084dc76c11bc45c5a3864ca4f
Freddie <freddie@wafflemonster.org>
2025-04-17 23:05:01 -0700

    Fix lint issues

--- apps/frontend/user-home/components/settings/sections/views/Views.svelte
+++ apps/frontend/user-home/components/settings/sections/views/Views.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
-    import { writable } from 'svelte/store'
+    import { writable } from 'svelte/store';
 
-    import { uiIcons } from '@/shared/icons'
-    import { browserStore } from '@/shared/stores/browser'
-    import { activeView, settingsStore } from '@/shared/stores/settings'
-    import { settingsState } from '@/stores/local-storage'
-    import type { SettingsView } from '@/shared/stores/settings/types'
+    import { uiIcons } from '@/shared/icons';
+    import { browserStore } from '@/shared/stores/browser';
+    import { activeView, settingsStore } from '@/shared/stores/settings';
+    import { settingsState } from '@/stores/local-storage';
+    import type { SettingsView } from '@/shared/stores/settings/types';
 
-    import IconifyIcon from '@/shared/components/images/IconifyIcon.svelte'
+    import IconifyIcon from '@/shared/components/images/IconifyIcon.svelte';
 
-    const deleting = writable<string>(null)
+    const deleting = writable<string>(null);
 
     const newView = () => {
         const view: SettingsView = {
             id: crypto.randomUUID(),
             name: 'NEW',
             characterFilter: '',
+            showCompletedUntrackedChores: false,
             groups: ['groupBy'],
             groupBy: [],
             sortBy: [],
@@ -26,41 +27,41 @@
             homeLockouts: [],
             homeTasks: [],
             disabledChores: {},
-        }
+        };
 
-        const newCustomViews = ($settingsStore.views || []).slice()
-        newCustomViews.push(view)
+        const newCustomViews = ($settingsStore.views || []).slice();
+        newCustomViews.push(view);
 
-        $settingsStore.views = newCustomViews
-        $settingsState.selectedView = view.id
-    }
+        $settingsStore.views = newCustomViews;
+        $settingsState.selectedView = view.id;
+    };
 
     const moveUpClick = (index: number) => {
-        const newViews = $settingsStore.views.slice()
-        const temp = newViews[index - 1]
-        newViews[index - 1] = newViews[index]
-        newViews[index] = temp
-        $settingsStore.views = newViews
-        $deleting = null
-    }
+        const newViews = $settingsStore.views.slice();
+        const temp = newViews[index - 1];
+        newViews[index - 1] = newViews[index];
+        newViews[index] = temp;
+        $settingsStore.views = newViews;
+        $deleting = null;
+    };
 
     const moveDownClick = (index: number) => {
-        const newViews = $settingsStore.views.slice()
-        const temp = newViews[index + 1]
-        newViews[index + 1] = newViews[index]
-        newViews[index] = temp
-        $settingsStore.views = newViews
-        $deleting = null
-    }
+        const newViews = $settingsStore.views.slice();
+        const temp = newViews[index + 1];
+        newViews[index + 1] = newViews[index];
+        newViews[index] = temp;
+        $settingsStore.views = newViews;
+        $deleting = null;
+    };
 
     const deleteConfirmClick = (viewId: string) => {
-        $deleting = null
-        $settingsStore.views = $settingsStore.views.filter((view) => view.id !== viewId)
+        $deleting = null;
+        $settingsStore.views = $settingsStore.views.filter((view) => view.id !== viewId);
 
         if ($activeView.id === viewId) {
-            $browserStore.home.activeView = $settingsStore.views[0].id
+            $browserStore.home.activeView = $settingsStore.views[0].id;
         }
-    }
+    };
 </script>
 
 <style lang="scss">
@@ -106,8 +107,8 @@
     <h3>Views</h3>
 
     <p>
-        The first View will be used as your default grouping/sorting on pages other than Home,
-        don't set it to anything that will annoy you.
+        The first View will be used as your default grouping/sorting on pages other than Home, don't
+        set it to anything that will annoy you.
     </p>
 
     <table class="table table-striped">
@@ -128,7 +129,7 @@
                         {/if}
                     </td>
                     <td class="icon">
-                        {#if viewIndex < ($settingsStore.views.length - 1)}
+                        {#if viewIndex < $settingsStore.views.length - 1}
                             <IconifyIcon
                                 icon={uiIcons.chevronDown}
                                 scale={'1.2'}
@@ -137,16 +138,16 @@
                             />
                         {/if}
                     </td>
-                    <td
-                        class="icon"
-                        class:border-right={$deleting === view.id}
-                    >
+                    <td class="icon" class:border-right={$deleting === view.id}>
                         {#if viewIndex > 0}
                             <IconifyIcon
                                 extraClass={'status-fail'}
                                 icon={uiIcons.no}
                                 tooltip={'Delete'}
-                                on:click={() => deleting.update((current) => current === view.id ? null : view.id)}
+                                on:click={() =>
+                                    deleting.update((current) =>
+                                        current === view.id ? null : view.id,
+                                    )}
                             />
                         {/if}
                     </td>
@@ -167,11 +168,6 @@
     </table>
 
     {#if $settingsStore.views.length < 10}
-        <button
-            class="group-entry"
-            on:click={newView}
-        >
-            New View
-        </button>
+        <button class="group-entry" on:click={newView}> New View </button>
     {/if}
 </div>
