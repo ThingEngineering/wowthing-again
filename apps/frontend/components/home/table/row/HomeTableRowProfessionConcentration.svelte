fd91d72d5f84dfe9c95fc8e7a043692c4ab9f8a0
Freddie <freddie@wafflemonster.org>
2025-05-03 11:31:11 -0700

    Use warn color for almost full concentration

--- apps/frontend/components/home/table/row/HomeTableRowProfessionConcentration.svelte
+++ apps/frontend/components/home/table/row/HomeTableRowProfessionConcentration.svelte
@@ -1,29 +1,32 @@
 <script lang="ts">
-    import { imageStrings } from '@/data/icons'
+    import { imageStrings } from '@/data/icons';
     import { professionConcentration } from '@/data/professions/cooldowns';
     import { staticStore } from '@/shared/stores/static';
     import { timeStore } from '@/shared/stores/time';
-    import { basicTooltip } from '@/shared/utils/tooltips'
+    import { basicTooltip } from '@/shared/utils/tooltips';
     import { itemStore, userStore } from '@/stores';
     import { getCurrencyData } from '@/utils/characters/get-currency-data';
     import { getProfessionSortKey } from '@/utils/professions';
-    import type { Character } from '@/types'
+    import type { Character } from '@/types';
 
-    import WowthingImage from '@/shared/components/images/sources/WowthingImage.svelte'
+    import WowthingImage from '@/shared/components/images/sources/WowthingImage.svelte';
     import { settingsStore } from '@/shared/stores/settings';
 
-    export let character: Character
+    export let character: Character;
 
-    $: professions = Object.values($staticStore.professions)
-            .filter((profession) => professionConcentration[profession.id] &&
-                character.professions?.[profession.id]);
-    $: professions.sort((a, b) => getProfessionSortKey(a).localeCompare(getProfessionSortKey(b)))
+    $: professions = Object.values($staticStore.professions).filter(
+        (profession) =>
+            professionConcentration[profession.id] && character.professions?.[profession.id],
+    );
+    $: professions.sort((a, b) => getProfessionSortKey(a).localeCompare(getProfessionSortKey(b)));
 
     function statusClass(fullIsBad: boolean, percent: number) {
-        if (percent >= 75) {
+        if (percent >= 100) {
             return fullIsBad ? 'status-fail' : 'status-success';
+        } else if (percent >= 75) {
+            return fullIsBad ? 'status-warn' : 'status-shrug';
         } else if (percent > 25 && percent < 75) {
-            return 'status-shrug';
+            return fullIsBad ? 'status-shrug' : 'status-warn';
         } else {
             return fullIsBad ? 'status-success' : 'status-fail';
         }
@@ -48,7 +51,7 @@
         align-items: center;
         display: flex;
         justify-content: space-between;
-    
+
         span {
             display: block;
             text-align: right;
@@ -61,19 +64,23 @@
     <div class="flex-wrapper">
         {#each professions as profession}
             {@const { amount, percent, tooltip } = getCurrencyData(
-                $itemStore, $timeStore, userStore, character, $staticStore.currencies[professionConcentration[profession.id]])}
+                $itemStore,
+                $timeStore,
+                userStore,
+                character,
+                $staticStore.currencies[professionConcentration[profession.id]],
+            )}
             <div
-                class="concentration {statusClass($settingsStore.professions.fullConcentrationIsBad, percent)}"
+                class="concentration {statusClass(
+                    $settingsStore.professions.fullConcentrationIsBad,
+                    percent,
+                )}"
                 use:basicTooltip={{
                     allowHTML: true,
-                    content: tooltip
+                    content: tooltip,
                 }}
             >
-                <WowthingImage
-                    name="{imageStrings[profession.slug]}"
-                    size={20}
-                    border={1}
-                />
+                <WowthingImage name={imageStrings[profession.slug]} size={20} border={1} />
                 <span>{amount}</span>
             </div>
         {/each}
