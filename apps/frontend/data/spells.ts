22a989a20d5f63e9c6a91c7c05cc957b858bc7bf
Freddie <freddie@wafflemonster.org>
2025-05-19 20:54:37 -0700

    Add some more spell buffs

--- apps/frontend/data/spells.ts
+++ apps/frontend/data/spells.ts
@@ -1,20 +1,29 @@
-export const durationAuras: [number, string][] = [
+// spellId, spellName, expiresWhileOffline
+export const durationAuras: [number, string, boolean?][] = [
     [24705, 'Grim Visage'],
-    [26013, 'Deserter'],
+    [26013, 'Deserter', true],
     [29175, 'Ribbon Dance'],
     [42138, 'Brewfest Enthusiast'],
     [46668, 'WHEE!'],
-    [71041, 'Dungeon Deserter'],
+    [71041, 'Dungeon Deserter', true],
     [95987, 'Unburdened'],
-    [136583, 'Darkmoon Top Hat'],
+    [136583, 'Darkmoon Top Hat', true],
     [289982, 'Draught of Ten Lands'],
-    [392960, 'Waygate Travel'],
+    [392960, 'Waygate Travel', true],
     [423860, 'Knowledge of Timeways'],
     [423861, 'Mastery of Timeways'],
     [455050, 'Blessings of the Bronze Dragonflight'],
     [465631, 'Blessings of the Bronze Dragonflight'],
     [471543, 'Knowledge of Timeways'],
     [471544, 'Mastery of Timeways'],
+    [1221184, 'Bottle of Mysterious Wisdom'],
+    // flasks
+    [431971, 'Flask of Tempered Aggression'],
+    [431972, 'Flask of Tempered Swiftness'],
+    [431973, 'Flask of Tempered Versatility'],
+    [431974, 'Flask of Tempered Mastery'],
+    [432021, 'Flask of Alchemical Chaos'],
+    [432473, 'Flask of Saving Graces'],
 ];
 
 export const staticAuras: [number, string][] = [[473218, "Delver's Bounty"]];
