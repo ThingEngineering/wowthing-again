029258d817495062b1fd944581985aa0154537c9
Freddie <freddie@wafflemonster.org>
2025-06-01 23:59:43 -0700

    Prettier config file rename

--- apps/frontend/prettier.config.js
+++ apps/frontend/prettier.config.js
@@ -1,6 +1,5 @@
 const config = {
     plugins: ['prettier-plugin-svelte'],
-    overrides: [{ "files": "*.svelte", "options": { "parser": "svelte" } }],
     singleQuote: true,
     tabWidth: 4,
     trailingComma: 'es5',
