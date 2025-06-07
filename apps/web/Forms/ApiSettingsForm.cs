70d12e819be377cd7b8d09655bf9872f4f7012cf
Freddie <freddie@wafflemonster.org>
2025-06-07 11:18:58 -0700

    Fix saved account settings not persisting correctly

--- apps/web/Forms/ApiSettingsForm.cs
+++ apps/web/Forms/ApiSettingsForm.cs
@@ -5,6 +5,5 @@ namespace Wowthing.Web.Forms;
 
 public class ApiSettingsForm
 {
-    public Dictionary<int, PlayerAccount> Accounts { get; set; }
     public ApplicationUserSettings Settings { get; set; }
 }
