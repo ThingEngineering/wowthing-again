c1d359b444cbbfbaf6ccbce24c294316016b3238
Freddie <freddie@wafflemonster.org>
2025-06-13 17:18:34 -0700

    Add a wrapper function to handle "log errors" since $derived.by is useless

--- apps/frontend/user-home/state/lazy/lazyState.svelte.ts
+++ apps/frontend/user-home/state/lazy/lazyState.svelte.ts
@@ -1,3 +1,5 @@
+import { logErrors } from '@/utils/log-errors';
+
 import { doAchievements } from './achievements.svelte';
 import { doAppearances } from './appearances.svelte';
 import { doConvertible } from './convertible.svelte';
@@ -8,14 +10,14 @@ import { doVendors } from './vendors.svelte';
 import { doZoneMaps } from './zoneMaps.svelte';
 
 class LazyState {
-    public achievements = $derived.by(() => doAchievements());
-    public appearances = $derived.by(() => doAppearances());
-    public convertible = $derived.by(() => doConvertible());
-    public customizations = $derived.by(() => doCustomizations());
-    public journal = $derived.by(() => doJournal());
-    public transmog = $derived.by(() => doTransmog());
-    public vendors = $derived.by(() => doVendors());
-    public zoneMaps = $derived.by(() => doZoneMaps());
+    public achievements = $derived.by(() => logErrors(doAchievements));
+    public appearances = $derived.by(() => logErrors(doAppearances));
+    public convertible = $derived.by(() => logErrors(doConvertible));
+    public customizations = $derived.by(() => logErrors(doCustomizations));
+    public journal = $derived.by(() => logErrors(doJournal));
+    public transmog = $derived.by(() => logErrors(doTransmog));
+    public vendors = $derived.by(() => logErrors(doVendors));
+    public zoneMaps = $derived.by(() => logErrors(doZoneMaps));
 }
 
 export const lazyState = new LazyState();
