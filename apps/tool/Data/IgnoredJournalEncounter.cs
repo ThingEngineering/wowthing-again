18f42eaaa66308fbaa5f5a5d80a5cb6611261139
Freddie <freddie@wafflemonster.org>
2025-07-12 22:27:24 -0700

    Un-ignore some journal encounters

--- apps/tool/Data/IgnoredJournalEncounter.cs
+++ apps/tool/Data/IgnoredJournalEncounter.cs
@@ -5,16 +5,11 @@ public static partial class Hardcoded
     public static readonly HashSet<int> IgnoredJournalEncounter = new()
     {
         485, // Classic/Zul'Farrak/Theka the Martyr [drops nothing useful]
-        634, // WotLk/ToC/Grand Champions [shows raid difficulties]
         748, // Classic/Uldaman/Obsidian Sentinel [drops nothing useful]
         749, // Classic/Stratholme/Commander Malor [drops nothing useful]
-        833, // WotLK/The Nexus/Commander Kolurg [raid difficulties, dupes Commander Stoutbeard]
         858, // Yu'lon
         859, // Niu'zao
         860, // Xuen
-        1764, // TBC/Karazhan/Chess Event [dupe]
         1872, // Legion/NH/Grand Magistrix Elisande [dupe with only LFR loot]
-        2340, // Grong, the Revenant [dupe]
-        2344, // Champion of the Light [dupe]
     };
 }
