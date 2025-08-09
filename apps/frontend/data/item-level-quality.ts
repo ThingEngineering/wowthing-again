edf66cf8bc01cf9a4c3f5086ac7d8a6357161dc3
Freddie <freddie@wafflemonster.org>
2025-08-08 18:50:09 -0700

    Use convertible tiers for item level quality

--- apps/frontend/data/item-level-quality.ts
+++ apps/frontend/data/item-level-quality.ts
@@ -1,7 +1,9 @@
+import { convertibleCategories } from '@/components/items/convertible/data';
+
 export const itemLevelQuality: number[][] = [
-    [662, 5], // Mythic raid = 662-671, +10 vault = 662
-    [649, 4], // Heroic raid = 649-658, + 2 vault = 649
-    [636, 3], // Normal raid = 636-645
-    [623, 2], // LFR raid    = 623-632,   H vault = 632
-    [610, 1], //
+    [convertibleCategories[0].tiers[0].itemLevel, 5], // Mythic raid = 662-671, +10 vault = 662
+    [convertibleCategories[0].tiers[1].itemLevel, 4], // Heroic raid = 649-658, + 2 vault = 649
+    [convertibleCategories[0].tiers[2].itemLevel, 3], // Normal raid = 636-645
+    [convertibleCategories[0].tiers[3].itemLevel, 2], // LFR raid    = 623-632,   H vault = 632
+    [convertibleCategories[0].tiers[3].itemLevel - 13, 1], //
 ];
