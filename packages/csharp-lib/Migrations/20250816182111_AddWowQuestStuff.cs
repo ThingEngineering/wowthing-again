131f95db3e6794a4dc58d5015238db2cca217f61
Freddie <freddie@wafflemonster.org>
2025-08-16 11:23:41 -0700

    Add more data to WowQuest

---
+++ packages/csharp-lib/Migrations/20250816182111_AddWowQuestStuff.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wowthing.Lib.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWowQuestStuff : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "area_id",
+                table: "wow_quest",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "category_id",
+                table: "wow_quest",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<short>(
+                name: "maximum_level",
+                table: "wow_quest",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<short>(
+                name: "minimum_level",
+                table: "wow_quest",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "area_id",
+                table: "wow_quest");
+
+            migrationBuilder.DropColumn(
+                name: "category_id",
+                table: "wow_quest");
+
+            migrationBuilder.DropColumn(
+                name: "maximum_level",
+                table: "wow_quest");
+
+            migrationBuilder.DropColumn(
+                name: "minimum_level",
+                table: "wow_quest");
+        }
+    }
+}
--- packages/csharp-lib/Models/Wow/WowQuest.cs
+++ packages/csharp-lib/Models/Wow/WowQuest.cs
@@ -7,6 +7,10 @@ public class WowQuest
 {
     [Key]
     public int Id { get; set; }
+    public int AreaId { get; set; }
+    public int CategoryId { get; set; }
+    public short MinimumLevel { get; set; }
+    public short MaximumLevel { get; set; }
 
     public DateTime LastApiCheck { get; set; } = MiscConstants.DefaultDateTime;
 
