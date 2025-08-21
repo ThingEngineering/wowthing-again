84d5001360cf7aff1a689528657e6ee2480f2f81
Freddie <freddie@wafflemonster.org>
2025-08-20 18:57:06 -0700

    Add PlayerCharacterMythicPlusSeason.Rating and populate it

--- apps/backend/Models/API/Character/ApiCharacterMythicKeystoneProfileSeason.cs
+++ apps/backend/Models/API/Character/ApiCharacterMythicKeystoneProfileSeason.cs
@@ -4,4 +4,13 @@ public class ApiCharacterMythicKeystoneProfileSeason
 {
     [JsonPropertyName("best_runs")]
     public List<ApiCharacterMythicKeystoneBestRun> BestRuns { get; set; }
+
+    [JsonPropertyName("mythic_rating")]
+    public ApiCharacterMythicKeystoneProfileRating MythicRating { get; set; }
 }
+
+public class ApiCharacterMythicKeystoneProfileRating
+{
+    public double Rating { get; set; }
+}
+
---
+++ packages/csharp-lib/Migrations/20250821014415_Add_PlayerCharacterMythicPlusSeason_Rating.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wowthing.Lib.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_PlayerCharacterMythicPlusSeason_Rating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "rating",
+                table: "player_character_mythic_plus_season",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "rating",
+                table: "player_character_mythic_plus_season");
+        }
+    }
+}
