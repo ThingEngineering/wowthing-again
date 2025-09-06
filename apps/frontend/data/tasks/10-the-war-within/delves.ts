e0ff17bce3852e3cefcb7cc82847428161232493
Freddie <freddie@wafflemonster.org>
2025-09-06 15:53:12 -0700

    Add twwDelves task

---
+++ apps/frontend/data/tasks/10-the-war-within/delves.ts
@@ -0,0 +1,120 @@
+import { DbResetType } from '@/shared/stores/db/enums';
+import type { Task } from '@/types/tasks';
+
+export const twwDelves: Task = {
+    key: 'twwDelves',
+    name: '[TWW] Delves',
+    shortName: 'Delve',
+    minimumLevel: 80,
+    showSeparate: true,
+    chores: [
+        {
+            key: 'keys',
+            name: 'Keys',
+            minimumLevel: 80,
+            subChores: [91175, 91176, 91177, 91178].map((questId, index) => ({
+                key: `key${index}`,
+                name: '{currency:3028}', // Restored Coffer Key
+                noProgress: true,
+                questIds: [questId],
+                questReset: DbResetType.Weekly,
+            })),
+        },
+        {
+            key: 'gilded',
+            name: 'Gilded Stash',
+            minimumLevel: 80,
+            subChores: [1, 2, 3].map((index) => ({
+                key: `stash${index}`,
+                name: '{currency:3290}', // Gilded Ethereal Crest
+                noProgress: true,
+                questReset: DbResetType.Weekly,
+                progressFunc: (char) => {
+                    const have = char.weekly?.delveGilded || 0;
+                    return { have: have >= index ? 1 : 0, need: 1 };
+                },
+            })),
+        },
+        {
+            key: 'map',
+            name: 'Map Drop',
+            minimumLevel: 80,
+            noProgress: true,
+            questIds: [86371],
+            questReset: DbResetType.Weekly,
+        },
+        null,
+        {
+            key: 'archaicCipher',
+            name: 'Archaic Cipher',
+            minimumLevel: 70,
+            noProgress: true,
+            accountWide: true,
+            questIds: [84370],
+            questReset: DbResetType.Weekly,
+        },
+        {
+            key: 'repCouncil',
+            name: 'Rep: Council of Dornogal',
+            minimumLevel: 80,
+            noProgress: true,
+            accountWide: true,
+            questIds: [83317],
+            questReset: DbResetType.Weekly,
+        },
+        {
+            key: 'repAssembly',
+            name: 'Rep: Assembly of the Deeps',
+            minimumLevel: 80,
+            noProgress: true,
+            accountWide: true,
+            questIds: [83318],
+            questReset: DbResetType.Weekly,
+        },
+        {
+            key: 'repSevered',
+            name: 'Rep: Severed Threads',
+            minimumLevel: 80,
+            noProgress: true,
+            accountWide: true,
+            questIds: [83319],
+            questReset: DbResetType.Weekly,
+        },
+        {
+            key: 'repHallowfall',
+            name: 'Rep: Hallowfall Arathi',
+            minimumLevel: 80,
+            noProgress: true,
+            accountWide: true,
+            questIds: [83320],
+            questReset: DbResetType.Weekly,
+        },
+        {
+            key: 'repCartels',
+            name: 'Rep: Cartels of Undermine',
+            minimumLevel: 80,
+            noProgress: true,
+            accountWide: true,
+            questIds: [87407],
+            questReset: DbResetType.Weekly,
+        },
+        {
+            key: 'repKaresh',
+            name: "Rep: The K'aresh Trust",
+            minimumLevel: 80,
+            noProgress: true,
+            accountWide: true,
+            questIds: [91453],
+            questReset: DbResetType.Weekly,
+        },
+
+        // {
+        //     key: 'twwDelveKyveza',
+        //     name: "Ky'veza Invasion",
+        //     minimumLevel: 80,
+        //     noProgress: true,
+        //     questIds: [], // ??
+        //     questReset: DbResetType.Weekly,
+        // },
+    ],
+};
--- apps/frontend/data/tasks/10-the-war-within/index.ts
+++ apps/frontend/data/tasks/10-the-war-within/index.ts
@@ -3,11 +3,13 @@ import { twwChores11_1_0 } from './11-1-0';
 import { twwChores11_1_5 } from './11-1-5';
 import { twwChores11_2_0 } from './11-2-0';
 import { twwChoresChett } from './chett';
+import { twwDelves } from './delves';
 import { twwHorrificVisions } from './horrific-visions';
 import { twwProfessions } from './professions';
 
 export const twwTasks = [
     twwProfessions,
+    twwDelves,
     twwChores11_0_0,
     twwChores11_1_0,
     twwChoresChett,
