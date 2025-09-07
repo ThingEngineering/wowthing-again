226c17485487209ef7810ac1894c4af035c04832
Freddie <freddie@wafflemonster.org>
2025-09-06 23:11:16 -0700

    Dunno why these were commented out, guess Blizzard fixed it at some point

--- apps/tool/Data/SkipInstances.cs
+++ apps/tool/Data/SkipInstances.cs
@@ -5,11 +5,11 @@ public static partial class Hardcoded
     public static readonly HashSet<int> SkipInstances = new()
     {
         // Reused map IDs for world boss sections? Sigh
-        322, // Pandaria
-        557, // Draenor
-        822, // Broken Isles
-        959, // Invasion Points
-        1028, // Azeroth
+        // 322, // Pandaria
+        // 557, // Draenor
+        // 822, // Broken Isles
+        // 959, // Invasion Points
+        // 1028, // Azeroth
 
         // I dunno what to do with this junk
         1301, // Blackrock Depths Raid
