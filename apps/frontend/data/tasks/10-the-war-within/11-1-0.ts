473292bd1a2ada0edf24e3c585b1b997a8b06632
Freddie <freddie@wafflemonster.org>
2025-09-18 21:15:06 -0700

    Undermine task work

--- apps/frontend/data/tasks/10-the-war-within/11-1-0.ts
+++ apps/frontend/data/tasks/10-the-war-within/11-1-0.ts
@@ -13,21 +13,23 @@ export const twwChores11_1_0: Task = {
             key: 'twwUndermineWorldBossFirst',
             name: '[Um] World Boss 1st Kill',
             accountWide: true,
-            questIds: [89401],
             questReset: DbResetType.Weekly,
+            questResetForced: true,
+            questIds: [89401],
         },
         {
             key: 'twwUndermineCartel',
             name: '[Um] Choose Cartel',
             accountWide: true,
             icon: iconLibrary.mdiListStatus,
+            questReset: DbResetType.Weekly,
+            questResetForced: true,
             questIds: [
                 84951, // Bilgewater Cartel Weekly Contract
                 84954, // Blackwater Cartel Weekly Contract
                 84952, // Steamwheedle Cartel Weekly Contract
                 84953, // Venture Co. Weekly Contract
             ],
-            questReset: DbResetType.Weekly,
         },
         {
             key: 'twwUndermineManyJobs',
@@ -39,39 +41,55 @@ export const twwChores11_1_0: Task = {
             key: 'twwUndermineReduce',
             name: '[Um] 3x S.C.R.A.P. Jobs',
             icon: iconLibrary.hisTrash,
-            questIds: [85879], // Reduce, Resuse, Resell
             questReset: DbResetType.Weekly,
+            questIds: [85879], // Reduce, Resuse, Resell
         },
         {
             key: 'twwUndermineSurge',
             name: '[Um] Surge Pricing',
-            questIds: [86775], // Urge to Surge
             questReset: DbResetType.Weekly,
+            questIds: [86775], // Urge to Surge
         },
         {
             key: 'twwUndermineSpecial',
             name: '[Um] Special Assignment',
             noProgress: true,
-            questIds: [
-                85487, // Boom! Headshot!
-                85488, // Security Detail
-            ],
             questReset: DbResetType.Weekly,
+            subChores: [
+                {
+                    key: 'unlock',
+                    name: 'World Quests',
+                    questIds: [
+                        85489, // Boom! Headshot! Unlock
+                        85490, // Security Detail Unlock
+                    ],
+                },
+                {
+                    key: 'assignment',
+                    name: 'Assignment',
+                    noProgress: true,
+                    questIds: [
+                        85487, // Boom! Headshot!
+                        85488, // Security Detail
+                    ],
+                },
+            ],
         },
+        // TODO: support something like ChoreTracker "pick n" (N/whatever quests)
         {
             key: 'twwSideGig',
             name: '[Um] Side Gig',
             showQuestName: true,
-            questIds: [],
             questReset: DbResetType.Weekly,
+            questIds: [],
         },
         {
             key: 'twwRaidPull',
             name: '[Raid] Rune Dispenser',
             icon: iconLibrary.mdiSlotMachineOutline,
             noProgress: true,
-            questIds: [89350],
             questReset: DbResetType.Weekly,
+            questIds: [89350],
         },
     ],
 };
