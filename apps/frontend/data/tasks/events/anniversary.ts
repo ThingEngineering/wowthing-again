2aee91171c5036a95861bdcf9620e6e8cab9cbb2
Freddie <freddie@wafflemonster.org>
2025-11-16 21:13:38 -0800

    Add anniversary tasks

---
+++ apps/frontend/data/tasks/events/anniversary.ts
@@ -0,0 +1,42 @@
+import { Holiday } from '@/enums/holiday';
+import { DbResetType } from '@/shared/stores/db/enums';
+import type { Task } from '@/types/tasks';
+
+export const eventAnniversary: Task = {
+    key: 'eventAnniversary',
+    name: '[Event] Anniversary',
+    requiredHolidays: [Holiday.Anniversary],
+    shortName: 'Anni',
+    minimumLevel: 10,
+    showSeparate: true,
+    chores: [
+        {
+            key: 'trivia',
+            name: 'Trivia',
+            minimumLevel: 10,
+            questReset: DbResetType.Daily,
+            questResetForced: true,
+            questIds: [
+                43323, // A
+                43461, // H
+            ],
+        },
+        {
+            key: 'codex',
+            name: "Chromie's Codex",
+            minimumLevel: 10,
+            questReset: DbResetType.Weekly,
+            questIds: [82783],
+        },
+        {
+            key: 'worldBosses',
+            name: 'World Bosses',
+            minimumLevel: 30,
+            questReset: DbResetType.Weekly,
+            questIds: [
+                47254, // The Originals
+                60215, // Timely Gate Crashers
+            ],
+        },
+    ],
+};
--- apps/frontend/data/tasks/events/index.ts
+++ apps/frontend/data/tasks/events/index.ts
@@ -1,3 +1,4 @@
+import { eventAnniversary } from './anniversary';
 import { eventBonus } from './bonus';
 import { eventBrewfest } from './brewfest';
 import { eventDarkmoonFaire } from './darkmoon-faire';
@@ -5,6 +6,7 @@ import { eventRemixLegion } from './remix';
 import { eventTimewalking } from './timewalking';
 
 export const eventTasks = [
+    eventAnniversary,
     eventBonus,
     eventBrewfest,
     eventDarkmoonFaire,
