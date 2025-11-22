a26952639ee2c552cf8a1f13e8754772e0db6378
Freddie <freddie@wafflemonster.org>
2025-11-21 19:11:35 -0800

    Reorder status spells

--- apps/frontend/data/spells.ts
+++ apps/frontend/data/spells.ts
@@ -2,6 +2,19 @@ import { Constants } from './constants';
 
 // spellId, spellName, expiresWhileOffline
 export const durationAuras: [number, string, boolean?][] = [
+    // flasks
+    [431971, 'Flask of Tempered Aggression'],
+    [431972, 'Flask of Tempered Swiftness'],
+    [431973, 'Flask of Tempered Versatility'],
+    [431974, 'Flask of Tempered Mastery'],
+    [432021, 'Flask of Alchemical Chaos'],
+    [432403, 'Vicious Flask of Classical Spirits'],
+    [432430, 'Vicious Flask of Honor'],
+    [432452, 'Vicious Flask of the Wrecking Ball'],
+    [432473, 'Flask of Saving Graces'],
+    // misc consumables
+    [1221184, 'Bottle of Mysterious Wisdom'],
+    // ??
     [24705, 'Grim Visage'],
     [26013, 'Deserter', true],
     [29175, 'Ribbon Dance'],
@@ -14,7 +27,6 @@ export const durationAuras: [number, string, boolean?][] = [
     [392960, 'Waygate Travel', true],
     [455050, 'Blessings of the Bronze Dragonflight'],
     [465631, 'Blessings of the Bronze Dragonflight'],
-    [1221184, 'Bottle of Mysterious Wisdom'],
     // timewalking
     [423860, 'Knowledge of Timeways I'],
     [423861, 'Mastery of Timeways I'],
@@ -24,16 +36,6 @@ export const durationAuras: [number, string, boolean?][] = [
     [1229050, 'Mastery of Timeways III'],
     [1229052, 'Knowledge of Timeways III'],
     [1258528, 'Mastery of Timeways IV'],
-    // flasks
-    [431971, 'Flask of Tempered Aggression'],
-    [431972, 'Flask of Tempered Swiftness'],
-    [431973, 'Flask of Tempered Versatility'],
-    [431974, 'Flask of Tempered Mastery'],
-    [432021, 'Flask of Alchemical Chaos'],
-    [432403, 'Vicious Flask of Classical Spirits'],
-    [432430, 'Vicious Flask of Honor'],
-    [432452, 'Vicious Flask of the Wrecking Ball'],
-    [432473, 'Flask of Saving Graces'],
 ];
 
 export const staticAuras: [number, string][] = [
