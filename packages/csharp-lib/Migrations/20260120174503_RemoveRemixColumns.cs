b9b5d754e12872b660609c225e89337dc4b46472
Freddie <freddie@thing.engineering>
2026-01-20 09:46:45 -0800

    Add migration for removing remix columns

---
+++ packages/csharp-lib/Migrations/20260120174503_RemoveRemixColumns.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wowthing.Lib.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveRemixColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "remix_artifact_trait",
+                table: "player_character_addon_data");
+
+            migrationBuilder.DropColumn(
+                name: "remix_research_have",
+                table: "player_character_addon_data");
+
+            migrationBuilder.DropColumn(
+                name: "remix_research_total",
+                table: "player_character_addon_data");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<short>(
+                name: "remix_artifact_trait",
+                table: "player_character_addon_data",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<short>(
+                name: "remix_research_have",
+                table: "player_character_addon_data",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<short>(
+                name: "remix_research_total",
+                table: "player_character_addon_data",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+        }
+    }
+}
