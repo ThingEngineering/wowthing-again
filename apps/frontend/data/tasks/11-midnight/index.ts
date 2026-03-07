b34241d1cf5e2fabafdf3d3cb8a52f1e144295f8
Freddie <freddie@thing.engineering>
2026-03-06 17:55:38 -0800

    Add Prey task

--- apps/frontend/data/tasks/11-midnight/index.ts
+++ apps/frontend/data/tasks/11-midnight/index.ts
@@ -1,4 +1,5 @@
 import { midChores12_0_0 } from './12-0-0';
+import { midPrey } from './prey';
 import { midProfessions } from './professions';
 
-export const midTasks = [midChores12_0_0, midProfessions];
+export const midTasks = [midChores12_0_0, midPrey, midProfessions];
