c295d98a77a43b49282b5c95cdc9688a4cab17e5
Freddie <freddie@thing.engineering>
2026-03-13 17:59:14 -0700

    Add "Unknown recipes" filter to patron orders

--- apps/frontend/components/professions/patron-orders/Sidebar.svelte
+++ apps/frontend/components/professions/patron-orders/Sidebar.svelte
@@ -3,6 +3,8 @@
     import type { StaticDataProfession } from '@/shared/stores/static/types';
 
     import Sidebar from '@/shared/components/sub-sidebar/SubSidebar.svelte';
+    import CheckboxInput from '@/shared/components/forms/CheckboxInput.svelte';
+    import { browserState } from '@/shared/state/browser.svelte';
 
     let { sortedProfessions }: { sortedProfessions: StaticDataProfession[] } = $props();
 
@@ -23,4 +25,13 @@
     ] as SidebarItem[]);
 </script>
 
-<Sidebar baseUrl="/professions/patron-orders" items={categories} width="10rem"></Sidebar>
+<Sidebar baseUrl="/professions/patron-orders" items={categories} width="10rem">
+    <svelt:fragment slot="after">
+        <CheckboxInput
+            name="patron_unknown"
+            bind:value={browserState.current.professions.patronOrdersUnknown}
+        >
+            Unknown recipes
+        </CheckboxInput>
+    </svelt:fragment>
+</Sidebar>
