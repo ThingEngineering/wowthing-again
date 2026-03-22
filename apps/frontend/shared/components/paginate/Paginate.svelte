c66e89ac93f8481b5df146d9fcb2819c567f9163
Freddie <freddie@thing.engineering>
2026-03-21 23:13:33 -0700

    Revert Paginate changes

--- apps/frontend/shared/components/paginate/Paginate.svelte
+++ apps/frontend/shared/components/paginate/Paginate.svelte
@@ -2,27 +2,31 @@
     import { router } from 'svelte-spa-router';
 
     import PaginateBar from './PaginateBar.svelte';
-    import type _ from 'lodash';
 
-    type Props = {
-        items: TData[];
-        page: number;
-        perPage: number;
-    };
-    let { items, page, perPage }: Props = $props();
+    // Update this to Svelte 5 at your own peril
+    export let items: TData[];
+    export let page: number;
+    export let perPage: number;
+    export let pageItems: TData[] = [];
 
-    let pages = $derived(Math.ceil(items.length / perPage));
-    let derivedPage = $derived(Math.max(1, Math.min(pages, page)));
-    let start = $derived((page - 1) * perPage);
-    let end = $derived(start + perPage);
+    let end: number;
+    let pages: number;
+    let start: number;
+    let url: string;
+    $: {
+        pages = Math.ceil(items.length / perPage);
+        page = Math.max(1, Math.min(pages, page));
+        start = (page - 1) * perPage;
+        end = start + perPage;
 
-    let url = $derived('#' + router.location.replace(/\/?\d+$/, ''));
+        url = '#' + router.location.replace(/\/?\d+$/, '');
 
-    let pageItems = $derived(items.slice(start, end));
+        pageItems = items.slice(start, end);
+    }
 </script>
 
 {#if items.length > 0}
-    <PaginateBar total={items.length} page={derivedPage} {pages} {perPage} {url}>
+    <PaginateBar total={items.length} {page} {pages} {perPage} {url}>
         <slot name="bar-end" slot="bar-end"></slot>
     </PaginateBar>
 {/if}
@@ -30,5 +34,5 @@
 <slot paginated={pageItems} />
 
 {#if items.length > 0}
-    <PaginateBar total={items.length} page={derivedPage} {pages} {perPage} {url} />
+    <PaginateBar total={items.length} {page} {pages} {perPage} {url} />
 {/if}
