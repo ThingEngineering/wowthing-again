fc32d9b9528c87ddefe052cfc316161e7785ef90
Freddie <freddie@thing.engineering>
2026-03-23 22:24:16 -0700

    Add some delve chores

--- apps/frontend/data/tasks/11-midnight/delves.ts
+++ apps/frontend/data/tasks/11-midnight/delves.ts
@@ -8,26 +8,60 @@ export const midDelves: Task = {
     shortName: 'Delve',
     minimumLevel: 80,
     showSeparate: true,
+    sumChores: true,
     chores: [
-        // {
-        //     key: 'map',
-        //     name: '{item:252415}',
-        //     icon: iconLibrary.gameTreasureMap,
-        //     minimumLevel: 80,
-        //     alwaysStarted: true,
-        //     questReset: DbResetType.Weekly,
-        //     questIds: [],
-        // },
-        // null,
         {
             key: 'arcaneRemnant',
-            name: '{item:262586}',
+            name: '{item:262586}', // Primeval Arcane Remnant
             icon: iconLibrary.gameUnstableOrb,
             minimumLevel: 80,
             accountWide: true,
             alwaysStarted: true,
             questReset: DbResetType.Weekly,
             questIds: [93784],
+        },
+        null,
+        {
+            key: 'bounty',
+            name: '{item:252415}', // Trovehunter's Bounty [Season 1]
+            icon: iconLibrary.gameTreasureMap,
+            minimumLevel: 90,
+            alwaysStarted: true,
+            subChoresAnyOrder: true,
+            questReset: DbResetType.Weekly,
+            questResetForced: true,
+            subChores: [
+                {
+                    key: 'get',
+                    name: 'Get Bounty',
+                    questIds: [86371],
+                },
+                {
+                    key: 'use',
+                    name: 'Use Bounty',
+                    questIds: [92887],
+                },
+            ],
+        },
+        {
+            key: 'gilded',
+            name: 'Gilded Stash',
+            icon: iconLibrary.gameCutDiamond,
+            minimumLevel: 90,
+            alwaysStarted: true,
+            questReset: DbResetType.Weekly,
+            questResetForced: true,
+            showQuestName: true,
+            subChores: [
+                {
+                    key: 'stashes',
+                    name: '{currency:3290}',
+                    progressFunc: (char) => ({
+                        have: char.weekly?.delveGilded || 0,
+                        need: 4,
+                    }),
+                },
+            ],
         },
         // {
         //     key: 'nullaeus',
