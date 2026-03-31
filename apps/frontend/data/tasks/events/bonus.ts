9fc144bdb52cb20c885ee349b746b5d649301afa
Freddie <freddie@thing.engineering>
2026-03-31 16:53:46 -0700

    Update a bunch of chore quest IDs

--- apps/frontend/data/tasks/events/bonus.ts
+++ apps/frontend/data/tasks/events/bonus.ts
@@ -21,34 +21,34 @@ export const eventBonus: Task = {
         {
             key: 'dungeons',
             name: 'Dungeons',
-            minimumLevel: 10,
+            minimumLevel: 90,
             requiredHolidays: [Holiday.BonusDungeon],
             questReset: DbResetType.Weekly,
             questResetForced: true,
-            questIds: [83347], // Emissary of War
+            questIds: [93598], // Emissary of War
         },
         {
             key: 'worldQuests',
             name: 'World Quests',
-            minimumLevel: 10,
+            minimumLevel: 90,
             requiredHolidays: [Holiday.BonusWorldQuest],
             questReset: DbResetType.Weekly,
             questResetForced: true,
-            questIds: [83366], // The World Awaits
+            questIds: [93605], // The World Awaits
         },
         {
             key: 'arenaSkirmish',
             name: '[PvP] Arena Skirmishes',
-            minimumLevel: 10,
+            minimumLevel: 90,
             requiredHolidays: [Holiday.BonusArenaSkirmish],
             questReset: DbResetType.Weekly,
             questResetForced: true,
-            questIds: [83358], // The Arena Calls
+            questIds: [93600], // The Arena Calls
         },
         {
             key: 'battleground',
             name: '[PvP] Battlegrounds',
-            minimumLevel: 10,
+            minimumLevel: 90,
             requiredHolidays: [Holiday.BonusBattleground],
             questReset: DbResetType.Weekly,
             questResetForced: true,
@@ -57,11 +57,11 @@ export const eventBonus: Task = {
         {
             key: 'petBattles',
             name: '[PvP] Pet Battles',
-            minimumLevel: 10,
+            minimumLevel: 90,
             requiredHolidays: [Holiday.BonusPetBattle],
             questReset: DbResetType.Weekly,
             questResetForced: true,
-            questIds: [83357], // The Very Best
+            questIds: [93599], // The Very Best
         },
     ],
 };
