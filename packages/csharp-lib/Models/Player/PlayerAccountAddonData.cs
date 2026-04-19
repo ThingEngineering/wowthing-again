2eb541290fe3238c99f909e171f794e734c8b965
Freddie <freddie@thing.engineering>
2026-04-19 16:45:33 -0700

    Add PlayerAccountAddonData.Reputations and .ReputationsScannedAt

--- packages/csharp-lib/Models/Player/PlayerAccountAddonData.cs
+++ packages/csharp-lib/Models/Player/PlayerAccountAddonData.cs
@@ -15,6 +15,10 @@ public class PlayerAccountAddonData
     public short HonorMax { get; set; }
 
     public DateTime DecorScannedAt { get; set; } = MiscConstants.DefaultDateTime;
+    public DateTime ReputationsScannedAt { get; set; } = MiscConstants.DefaultDateTime;
+
+    public List<int> Illusions { get; set; }
+    public List<int> Quests { get; set; }
 
     [Column(TypeName = "jsonb")]
     public Dictionary<int, List<int>> Decor { get; set; }
@@ -22,6 +26,6 @@ public class PlayerAccountAddonData
     [Column(TypeName = "jsonb")]
     public Dictionary<int, short> Heirlooms { get; set; }
 
-    public List<int> Illusions { get; set; }
-    public List<int> Quests { get; set; }
+    [Column(TypeName = "jsonb")]
+    public Dictionary<int, int> Reputations { get; set; }
 }
