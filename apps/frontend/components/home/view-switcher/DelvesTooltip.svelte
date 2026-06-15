2f30666d712c9bd094fcbacddd2cbe98971f74b5
Freddie <freddie@thing.engineering>
2026-06-14 19:34:15 -0700

    Add active delves to view switcher

---
+++ apps/frontend/components/home/view-switcher/DelvesTooltip.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import { delveMap } from '@/data/delve';
+
+    let { delves }: { delves: { poiId: number; story: string }[] } = $props();
+</script>
+
+<style lang="scss">
+    table {
+        width: 24rem;
+    }
+    td {
+        text-align: left;
+    }
+    .name {
+        width: 12rem;
+    }
+    .story {
+        width: 12rem;
+    }
+</style>
+
+<div class="wowthing-tooltip">
+    <h4>Bountiful Delves</h4>
+    <table class="table table-striped">
+        <tbody>
+            {#each delves as { poiId, story } (poiId)}
+                {@const delve = delveMap[poiId]}
+                {#if delve}
+                    <tr>
+                        <td class="name text-overflow">{delve.name}</td>
+                        <td class="story quality{delve.storyRanks[story] ?? 3}">{story}</td>
+                    </tr>
+                {/if}
+            {/each}
+        </tbody>
+    </table>
+</div>
