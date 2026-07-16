519f08856cff1d9ea9dc93f1d6903dc6844a3449
Freddie <freddie@thing.engineering>
2026-07-16 14:09:26 -0700

    Add questline questId blocking and block some unimplemented stuff

---
+++ apps/tool/Data/IgnoredQuestLineEntries.cs
@@ -0,0 +1,11 @@
+namespace Wowthing.Tool.Data;
+
+public static partial class Hardcoded
+{
+    public static readonly Dictionary<int, int[]> IgnoredQuestLineEntries = new()
+    {
+        // Dragonflight
+        { 5468, [77488] }, // Excavation: Back to Eadweard!
+        { 5469, [77485] }, // Technoscrying: Back to Eadweard!
+    };
+}
