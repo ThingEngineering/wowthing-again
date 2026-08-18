938f45375046d0a19915f6075458c9434d4c8781
Freddie <freddie@thing.engineering>
2026-08-18 11:14:49 -0700

    Add skeleton of explore>modifier trees

---
+++ apps/frontend/components/explore/ModifierTrees.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { browserState } from '@/shared/state/browser.svelte';
+
+    import NumberInput from '@/shared/components/forms/NumberInput.svelte';
+
+    // meow
+</script>
+
+<style lang="scss">
+</style>
+
+<div class="thing-container border">
+    <NumberInput
+        name="modifier_tree_id"
+        minValue={0}
+        maxValue={999999}
+        bind:value={browserState.current.explore.modifierTreeId}
+    />
+</div>
