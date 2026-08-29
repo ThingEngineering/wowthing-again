62f00897c5f0ffc6f57e322a82a529e51665edf9
Freddie <freddie@thing.engineering>
2026-08-29 12:10:53 -0700

    Fix some TypeScript weirdness
    `private` is useless. Change property to an optional hash property so that it is a) actually private, and b) not required in other places

--- apps/frontend/types/character/equipped-item.ts
+++ apps/frontend/types/character/equipped-item.ts
@@ -1,8 +1,7 @@
-import { wowthingData } from '@/shared/stores/data';
 import { applyBonusIds } from '@/utils/items/apply-bonus-ids';
 
 export class CharacterEquippedItem {
-    private _itemLevel: number;
+    #itemLevel?: number;
 
     constructor(
         public context: number,
@@ -14,7 +13,7 @@ export class CharacterEquippedItem {
         public enchantmentIds: number[],
         public gemIds: number[]
     ) {
-        this._itemLevel = itemLevel;
+        this.#itemLevel = itemLevel;
 
         this.bonusIds.sort((a, b) => a - b);
     }
@@ -25,14 +24,14 @@ export class CharacterEquippedItem {
 
     get itemLevel(): number {
         // the addon API is returning 0 sometimes for random equipped items 🙄
-        if (this._itemLevel === 0) {
+        if (this.#itemLevel === 0) {
             const calculatedItemLevel = applyBonusIds(this.bonusIds, {
                 quality: this.quality,
             });
-            this._itemLevel = calculatedItemLevel.itemLevel;
+            this.#itemLevel = calculatedItemLevel.itemLevel;
         }
 
-        return this._itemLevel;
+        return this.#itemLevel;
     }
 }
 export type CharacterEquippedItemArray = ConstructorParameters<typeof CharacterEquippedItem>;
